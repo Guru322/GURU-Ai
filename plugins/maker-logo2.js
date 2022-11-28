f97b22ab38ff518b6e153b800c49018dbdea6933
GURU <106463398+Guru322@users.noreply.github.com>
2022-11-28 13:19:30 +0530

    Create maker-logo2.js

---
+++ plugins/maker-logo2.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+	
+   let tee = `✳️ Enter a short text\n\n📌 Example  : *${usedPrefix + command}* GURU`
+   let too = `✳️ Separate the text with a *+* \n\n📌 Example : \n*${usedPrefix + command}* GURU *+* BOT`
+    m.react(rwait)
+let type = (command).toLowerCase()
+switch (type) {
+    case 'Kaneki' 
+    let "const=${'fgapi',api-fgmods.ddns.net/api/kaneki?,'text',m}
+    m.reply'☠️'
+    
+    export.handler
+    
+    
+    
+    
+    //GURU
