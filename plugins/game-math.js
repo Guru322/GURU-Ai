6d518a267a1d9b05d07acf6a86ae2d12cbd302b7
GURU <106463398+Guru322@users.noreply.github.com>
2022-12-06 17:26:40 +0530

    Update game-math.js

--- plugins/game-math.js
+++ plugins/game-math.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     conn.math = conn.math ? conn.math : {}
     
     if (args.length < 1) throw `
-  🧮 Dificultades disponibles : 
+  🧮 available difficulties : 
   
 ${Object.keys(modes).join(' | ')} 
 
-_📌Ejemplo : ${usedPrefix+command} normal_
+_📌Example : ${usedPrefix+command} normal_
 `.trim()
   let mode = args[0].toLowerCase()
   if (!(mode in modes)) throw `
@@ -14,22 +14,22 @@ _📌Ejemplo : ${usedPrefix+command} normal_
   
  ${Object.keys(modes).join(' | ')}
 
-_📌Ejemplo : ${usedPrefix+command} normal_
+_📌Example : ${usedPrefix+command} normal_
 `.trim()
     
   let id = m.chat
-    if (id in conn.math) return conn.reply(m.chat, '⚠️ Todavía hay preguntas sin respuesta en este chat', conn.math[id][0])
+    if (id in conn.math) return conn.reply(m.chat, '⚠️ There are still unanswered questions in this chat', conn.math[id][0])
     let math = genMath(mode)
     conn.math[id] = [
-        await conn.reply(m.chat, `▢ CUANTO ES *${math.str}*=\n\n_Tiempo:_ ${(math.time / 1000).toFixed(2)} segundos\n\n🎁 Recompensa : ${math.bonus} XP`, m),
+        await conn.reply(m.chat, `▢ HOW MUCH IS IT *${math.str}*=\n\n_Time:_ ${(math.time / 1000).toFixed(2)} seconds\n\n🎁 Reward : ${math.bonus} XP`, m),
         math, 4,
         setTimeout(() => {
-            if (conn.math[id]) conn.reply(m.chat, `⏳ Se acabó el tiempo!\nLa respuesta es : *${math.result}*`, conn.math[id][0])
+            if (conn.math[id]) conn.reply(m.chat, `⏳ Time is over!\nThe answer is : *${math.result}*`, conn.math[id][0])
       delete conn.math[id]
         }, math.time)
     ]
 }
-handler.help = ['Mates <modo>']
+handler.help = ['Maths <modes>']
 handler.tags = ['game']
 handler.command = ['mates', 'mate', 'matemáticas', 'math'] 
 
