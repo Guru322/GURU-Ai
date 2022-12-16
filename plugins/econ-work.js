d6834a4f8d465d3c9f27daa2b3e293c04329c006
GURU <106463398+Guru322@users.noreply.github.com>
2022-12-16 18:56:39 +0530

    Update econ-work.js

--- plugins/econ-work.js
+++ plugins/econ-work.js
@@ -1,20 +1,19 @@
-
 import fetch from 'node-fetch'
 import axios from 'axios'
 let handler = async (m, { conn, usedPrefix, command }) => {
 
   let hasil = Math.floor(Math.random() * 2000)
   let time = global.db.data.users[m.sender].lastwork + 3600000
-  if (new Date - global.db.data.users[m.sender].lastwork < 3600000) throw `*🧘🏻‍♂️ You are tired* and therefore you have to wait *${msToTime(time - new Date())}* to get back to work!`
+  if (new Date - global.db.data.users[m.sender].lastwork < 3600000) throw `*🧘🏻‍♂️ Estas cansado* y por lo tanto hay que esperar *${msToTime(time - new Date())}* para volver a trabajar!`
 
     /*let w = await axios.get(global.API('fgmods', '/api/work', { }, 'apikey'))
     let res = w.data.result*/
-    let anu = (await axios.get('https://raw.githubusercontent.com/FG98F/team-fg/main/games/work.json')).data
+    let anu = (await axios.get('https://raw.githubusercontent.com/fgmods/fg-team/main/games/work.json')).data
     let res = pickRandom(anu)
  global.db.data.users[m.sender].exp += hasil
 
   m.reply(`
-‣ U need more *${hasil} XP to use this feature*
+‣ ${res.fgwork} *${hasil} XP*
 `)
   global.db.data.users[m.sender].lastwork = new Date * 1
 }
@@ -34,7 +33,7 @@ function msToTime(duration) {
   minutes = (minutes < 10) ? "0" + minutes : minutes
   seconds = (seconds < 10) ? "0" + seconds : seconds
 
-  return minutes + " minute(s) " + seconds + " second(s)" 
+  return minutes + " minuto(s) " + seconds + " segundo(s)" 
 }
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
