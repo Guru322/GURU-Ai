ded7a5255c41edc4de4e0f240d45b97f61ffe059
GURU <106463398+Guru322@users.noreply.github.com>
2023-01-09 20:21:35 +0530

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -4,17 +4,12 @@ import fetch from 'node-fetch'
 let handler = async function (m, { conn, text, usedPrefix }) {
   
 let m2 = `
-≡ _Use these commands without the prefix_
+≡ _Use the commands with the prefix . _
 
 ▢ If you have more audio   
 send me by t.me/i_want_to_be_isekaied *audio + command* with which you will respond
 
-┌─⊷ *AUDIOS* 
-▢ Bot
-▢ HELLO
-▢ Good afternoon!
-▢ Goodnight
-└──────────────
+Bot has more than 100 commands and i am lazy to write all
 `
 //const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
     let pp = './src/fg_logo.jpg' 
@@ -27,6 +22,6 @@ send me by t.me/i_want_to_be_isekaied *audio + command* with which you will resp
 
 handler.help = ['audios']
 handler.tags = ['main']
-handler.command = ['menu2', 'audios'] 
+handler.command = ['menu', 'help'] 
 
 export default handler
