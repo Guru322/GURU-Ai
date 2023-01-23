ca5d040ee89d33cebdb10692b770a4e753b2eef4
GURU <106463398+Guru322@users.noreply.github.com>
2023-01-23 08:08:23 +0530

    Guru

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -4,24 +4,234 @@ import fetch from 'node-fetch'
 let handler = async function (m, { conn, text, usedPrefix }) {
   
 let m2 = `
-≡ _Use the commands with the prefix . _
+≡ _Use the commands with the prefix . 
 
-▢ If you have more audio   
-send me by t.me/i_want_to_be_isekaied *audio + command* with which you will respond
+▢ ‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‎
+Ⓟ = Premium
+ⓓ = Diamonds
+-----  -----  -----  -----  -----
+  ≡ *ALL MENU LIST*
 
-Bot has more than 100 commands and i am lazy to write all
+┌─⊷ *ABOUT*
+▢ .blocklist
+▢ .info
+▢ .owner
+▢ .donate
+▢ .gpguru
+▢ .listprem
+▢ .help
+▢ .audios
+▢ .ping
+▢ .runtime
+└───────────
+
+┌─⊷ *GAMES*
+▢ .dado
+▢ .delttt
+▢ .Math <mode>
+▢ .ppt <stone/paper/scissors>
+▢ .slot <gamble>
+▢ .tictactoe <tag Someone>
+└───────────
+
+┌─⊷ *LEVELS AND ECO*
+▢ .adddi <@user>
+▢ .addxp <@user>
+▢ .balance
+▢ .daily
+▢ .leaderboard
+▢ .levelup
+▢ .mine
+▢ .rob
+▢ .buy
+▢ .buyall
+▢ .transfer [tip] [amount] [@tag]
+▢ .work
+└───────────
+
+┌─⊷ *REGISTER*
+▢ .reg <name.age>
+▢ .mysn
+▢ .unreg <Num Serie>
+└───────────
+
+┌─⊷ *STICKER*
+▢ .attp <text>
+▢ .emojimix <emoji+emoji> (ⓓ)
+▢ .getsticker (ⓓ)
+▢ .smaker (ⓓ)
+▢ .sticker
+▢ .toimg <sticker>
+▢ .tovid
+▢ .trigger <@user>
+▢ .ttp <text>
+▢ .take <name>|<author>
+└───────────
+
+┌─⊷ *IMAGE*
+▢ .tvid (ⓓ)
+▢ .imagen (ⓓ)
+▢ .girl (ⓓ)
+▢ .meme (ⓓ)
+▢ .pinterest
+▢ .wallpaper (ⓓ)
+└───────────
+
+┌─⊷ *MAKER*
+▢ .logololi (ⓓ)
+▢ .neon (ⓓ)
+▢ .devil (ⓓ)
+▢ .wolf (ⓓ)
+▢ .phlogo (ⓓ)
+└───────────
+
+┌─⊷ *PREMIUM*
+▢ .gdrive (ⓓ) (Ⓟ)
+▢ .mediafire <url> (ⓓ)
+▢ .xvid (ⓓ) (Ⓟ)
+▢ .xnxx (ⓓ) (Ⓟ)
+└───────────
+
+┌─⊷ *GROUP*
+▢ .add
+▢ .delete
+▢ .delwarn @user
+▢ .demote (@tag)
+▢ .infogp
+▢ .hidetag
+▢ .invite <917xxx>
+▢ .kick @user
+▢ .link
+▢ .poll <Guru|piro|xd>
+▢ .perfil
+▢ .promote
+▢ .resetlink
+▢ .setbye <text>
+▢ .group *open/close*
+▢ .setwelcome <text>
+▢ .simulate <event> @user
+▢ .staff
+▢ .tagall
+▢ .totag
+▢ .warn @user
+▢ .warns
+▢ .checkexpired
+└───────────
+
+┌─⊷ *EN/DISABLE OPTIONS*
+▢ .enable <option>
+▢ .disable <option>
+└───────────
+
+┌─⊷ *ANIME*
+▢ .waifu (ⓓ)
+▢ .neko (ⓓ)
+▢ .megumin (ⓓ)
+▢ .loli (ⓓ)
+└───────────
+
+┌─⊷ *ANIME REACTIONS*
+▢ .kill @tag (ⓓ)
+▢ .kiss @tag (ⓓ)
+▢ .pat @tag (ⓓ)
+▢ .slap @tag (ⓓ)
+└───────────
+
+┌─⊷ *DOWNLOADERS*
+▢ .facebook <url> (ⓓ)
+▢ .gdrive (ⓓ) (Ⓟ)
+▢ .gitclone <url> (ⓓ)
+▢ .igstalk
+▢ .instagram <link ig> (ⓓ)
+▢ .mediafire <url> (ⓓ)
+▢ .play
+▢ .play2
+▢ .tiktok (ⓓ)
+▢ .tiktokstalk
+▢ .twitter <url> (ⓓ)
+▢ .ytmp3 <url> (ⓓ)
+▢ .ytsearch
+▢ .ytmp4 <link yt> (ⓓ)
+└───────────
+
+┌─⊷ *TOOLS*
+▢ .cal <equation>
+▢ .google (ⓓ)
+▢ .lyrics
+▢ .readmore <text1>|<text2>
+▢ .ssweb <url> (ⓓ)
+▢ .tourl
+▢ .trad <leng> <text>
+▢ .tts <lang> <teks>
+▢ .wikipedia
+└───────────
+
+┌─⊷ *FUN*
+▢ .afk <razon>
+▢ .tomp3
+▢ .toav
+▢ .gay @user
+▢ .pregunta
+▢ .bot
+└───────────
+
+┌─⊷ *DATABASE*
+▢ .listcmd
+▢ .setcmd <txt>
+└───────────
+
+┌─⊷ *NSFW +18*
+▢ .xwaifu (ⓓ)
+▢ .xneko (ⓓ)
+▢ .blowjob (ⓓ)
+▢ .ass (ⓓ)
+▢ .boobs (ⓓ)
+▢ .lesbian (ⓓ)
+▢ .pussy (ⓓ)
+▢ .pack (ⓓ)
+▢ .chut (ⓓ)
+▢ .gand (ⓓ)
+└───────────
+
+┌─⊷ *OWNER*
+▢ .expired <days>
+▢ .addprem <@tag>
+▢ .banchat
+▢ .listban
+▢ .ban @user
+▢ .tx
+▢ .cleartmp
+▢ .delexpired
+▢ .delprem @user
+▢ .getplugin <name file>
+▢ .getfile <name file>
+▢ .join <chat.whatsapp.com> <dias>
+▢ .reset <54xxx>
+▢ .restart
+▢ .unbanchat
+▢ .unban @user
+▢ .update
+└───────────
+
+┌─⊷ *ADVANCED*
+▢ >
+▢ =>
+▢ $
+└───────────  
+
+Bot has more than 300 commands and i am lazy to write all
 `
 //const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
     let pp = './src/fg_logo.jpg' 
     conn.sendButton(m.chat, m2, '▢ ᴳᵁᴿᵁ  ┃ ᴮᴼᵀ\n▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n', pp, [
       ['⏍ Info', `${usedPrefix}botinfo`],
-      ['⌬ Groups', `${usedPrefix}gpdylux`]
+      ['⌬ Groups', `${usedPrefix}gpguru`]
     ],m, rpyt)
    
 }
 
 handler.help = ['audios']
 handler.tags = ['main']
-handler.command = ['menu', 'help'] 
+handler.command = ['menu', 'help', 'h'] 
 
 export default handler
