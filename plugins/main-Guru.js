4211f071e0b5c6ecf3bca11b0ce0659370156dc8
GURU <106463398+Guru322@users.noreply.github.com>
2023-01-23 07:57:45 +0530

    GURU

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -3,20 +3,20 @@ let handler = async m => m.reply(`
 ≡  *GURU  ┃ ᴮᴼᵀ*   GRUPOS
 
 ─────────────
-▢ Group 1
-https://chat.whatsapp.com/FYvu6iqA7UA3sGePjPK636
+▢ Join public bot group and support
+https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f
 
 ▢ Group 2
-https://chat.whatsapp.com/EdpAQTof9BW8Mn4CnBFhWY
+https://chat.whatsapp.com/LwLQab5mZVW6WWRbBPnDGC
 
 ▢ Group 3
-https://chat.whatsapp.com/I1eAuFDBp6a1568QD0CeCk
+https://chat.whatsapp.com/Jn9qVerYN6Q1L0vlVFe2XL
 
 ─────────────
 ≡ Disabled links? enter here! 
 
 ▢ Group WhatsApp 
- https://chat.whatsapp.com/FYvu6iqA7UA3sGePjPK636
+ https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f
 ─────────────
 ▢ *Owner Telegram*
  https://t.me/i_want_to_be_isekaied
@@ -28,6 +28,6 @@ https://chat.whatsapp.com/I1eAuFDBp6a1568QD0CeCk
 `.trim())
 handler.help = ['gpdylux']
 handler.tags = ['main']
-handler.command = ['groups', 'groupdylux', 'dxgp', 'dygp', 'gpdylux'] 
+handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru'] 
 
 export default handler
