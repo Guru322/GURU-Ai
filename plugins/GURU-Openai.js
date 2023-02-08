caf9f0c908b9fdf0214c36ec6443e6ab56e3cd7b
GURU <106463398+Guru322@users.noreply.github.com>
2023-02-08 18:25:34 +0530

    GURU

---
+++ plugins/GURU-Openai.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch'
+let handler = async (m, { text, usedPrefix, command }) => {
+if (!text) throw *enter a request or an order to use the chatgpt function*\n\n*—◉ 𝙴xample*\n*◉ ${usedPrefix + command} Reflexion  series 2022  netflix*\n*◉ ${usedPrefix + command} write a js code*
+try {
+m.reply('*wait sometime*')
+let tiores = await fetch(https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id)
+let hasil = await tiores.json()
+m.reply(${hasil.result}.trim())
+} catch {
+throw *𝙴𝚁𝚁𝙾𝚁*
+}}
+handler.command = ['bro', 'chatgpt', 'ai', 'siri']
+export default handler
