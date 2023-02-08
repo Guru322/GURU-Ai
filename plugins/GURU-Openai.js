1f6f543eb4c2409bfe82352b4b64ded1cd677bc4
GURU <106463398+Guru322@users.noreply.github.com>
2023-02-08 18:34:33 +0530

    🗿🗿
    Always remember you have to use , 
    Or u r fucked up

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
-if (!text) throw *enter a request or an order to use the chatgpt*\n\n*—◉ 𝙴xample*\n*◉ ${usedPrefix + command}series 2022  netflix*\n*◉ ${usedPrefix + command} write a js code*
+if (!text) throw '*enter a request or an order to use the chatgpt*\n\n*—◉ 𝙴xample*\n*◉ ${usedPrefix + command}series 2022  netflix*\n*◉ ${usedPrefix + command} write a js code*'
 try {
 m.reply('*wait sometime*')
-let tiores = await fetch(https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id)
+let tiores = await fetch('https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id')
 let hasil = await tiores.json()
-m.reply(${hasil.result}.trim())
+m.reply('${hasil.result}'.trim())
 } catch {
-throw *𝙴𝚁𝚁𝙾𝚁*
+throw '*𝙴𝚁𝚁𝙾𝚁*'
 }}
 handler.command = ['bro', 'chatgpt', 'ai', 'siri']
 export default handler
