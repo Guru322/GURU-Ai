67024276d7526c18f9635097a96052a75f5d9108
GURU <106463398+Guru322@users.noreply.github.com>
2023-02-08 18:18:08 +0530

    GURU

---
+++ plugins/prefix.js
@@ -0,0 +1,11 @@
+let handler = async(m, { conn, text }) => {
+  if (!text) throw `No symbol detected ...`
+  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+  await m.reply(`the prefix has been changed to  *${text}*`)
+}
+handler.help = ['setprefix'].map(v => v + ' [prefix]')
+handler.tags = ['owner']
+handler.command = /^(setprefix)$/i
+handler.rowner = true
+
+export default handler
