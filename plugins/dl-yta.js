ec68f1a1c34c3ec8bf43d49c479bf961586f0f4c
GURU <106463398+Guru322@users.noreply.github.com>
2023-02-08 10:16:26 +0530

    GURU

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -1,32 +1,24 @@
-
-import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
-let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `✳️ Example :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
-  if (!args[0].match(/youtu/gi)) throw `❎ Verify that the YouTube link`
-   m.react(rwait)
- let chat = global.db.data.chats[m.chat]
-  try {
-		let q = '128kbps'
-		let v = args[0]
-		const yt = await youtubedl(v).catch(async () => await youtubedlv2(v)).catch(async () => await youtubedlv3(v))
-		const dl_url = await yt.audio[q].download()
-		const title = await yt.title
-		const size = await yt.audio[q].fileSizeH
-		conn.sendFile(m.chat, dl_url, title + '.mp3', `
- ≡  *GURU YTDL*
-  
-▢ *📌Title* : ${title}
-▢ *⚖️Size* : ${size}
-`.trim(), m, false, { mimetype: 'audio/mpeg', asDocument: chat.useDocument })
-		m.react(done)
-        } catch {
-			await m.reply(`❎ Error: Could not download audio`)
-} 
-
-}
-handler.help = ['ytmp3 <url>']
-handler.tags = ['dl']
-handler.command = ['ytmp3', 'fgmp3'] 
-handler.diamond = true
-
+import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] SEARCH YOUTUBE / YOUTUBE 𝙻𝙸𝙽𝙺*'
+await m.reply(`*_⏳processing ᴀᴜᴅɪᴏ...⏳_*\n\n*◉  command #playdoc ᴏ #play.2 ᴏ #ytmp4doc ◉*`)
+try {
+let q = '128kbps'
+let v = args[0]
+const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v))
+const dl_url = await yt.audio[q].download()
+const ttl = await yt.title
+const size = await yt.audio[q].fileSizeH
+await conn.sendFile(m.chat, dl_url, ttl + '.mp3', null, m, false, { mimetype: 'audio/mp4' })
+} catch {
+try {
+let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${lolkeysapi}&url=${args[0]}`)    
+let lolh = await lolhuman.json()
+let n = lolh.result.title || 'error'
+await conn.sendMessage(m.chat, { audio: { url: lolh.result.link }, fileName: `${n}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })   
+} catch {
+await conn.reply(m.chat, '*[❗]AUDIO ERROR*', m)}
+}}
+handler.command = /^fgmp3|dlmp3|getaud|yt(a|mp3)$/i
 export default handler
