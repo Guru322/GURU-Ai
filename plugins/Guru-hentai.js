499d0185ce8d0b91d7682fa11ec295f1c6c3fe02
GURU <106463398+Guru322@users.noreply.github.com>
2023-02-09 16:22:32 +0530

    GURU

---
+++ plugins/Guru-hentai.js
@@ -0,0 +1,43 @@
+import fetch from 'node-fetch'
+import PDFDocument from "pdfkit"
+import { extractImageThumb } from "@adiwajshing/baileys"
+let handler = async (m, { conn, text, usedPrefix, command, args }) => {
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*NOT A NSFW GROUP TELL ADMIN TO USE #enable nsfw*'
+if (!text) throw `*ENTER CHARACTER, 𝙴XAMPLE: ${usedPrefix + command} miku*`
+try {
+m.reply(global.wait)
+let res = await fetch(`https://api.lolhuman.xyz/api/nhentaisearch?apikey=${lolkeysapi}&query=${text}`)    
+let json = await res.json()
+let aa = json.result[0].id
+let data = await nhentaiScraper(aa)
+let pages = []
+let thumb = `https://external-content.duckduckgo.com/iu/?u=https://t.nhentai.net/galleries/${data.media_id}/thumb.jpg`	
+data.images.pages.map((v, i) => {
+let ext = new URL(v.t).pathname.split('.')[1]
+pages.push(`https://external-content.duckduckgo.com/iu/?u=https://i7.nhentai.net/galleries/${data.media_id}/${i + 1}.${ext}`)})
+let buffer = await (await fetch(thumb)).buffer()		
+let jpegThumbnail = await extractImageThumb(buffer)		
+let imagepdf = await toPDF(pages)		
+await conn.sendMessage(m.chat, { document: imagepdf, jpegThumbnail, fileName: data.title.english + '.pdf', mimetype: 'application/pdf' }, { quoted: m })
+} catch {
+throw `*[❗] 𝙴𝚁𝚁𝙾𝚁, NOT FOUND*`
+}}
+handler.command = /^(hentai)$/i
+export default handler
+async function nhentaiScraper(id) {
+let uri = id ? `https://cin.guru/v/${+id}/` : 'https://cin.guru/'
+let html = (await axios.get(uri)).data
+return JSON.parse(html.split('<script id="__NEXT_DATA__" type="application/json">')[1].split('</script>')[0]).props.pageProps.data}
+function toPDF(images, opt = {}) {
+return new Promise(async (resolve, reject) => {
+if (!Array.isArray(images)) images = [images]
+let buffs = [], doc = new PDFDocument({ margin: 0, size: 'A4' })
+for (let x = 0; x < images.length; x++) {
+if (/.webp|.gif/.test(images[x])) continue
+let data = (await axios.get(images[x], { responseType: 'arraybuffer', ...opt })).data
+doc.image(data, 0, 0, { fit: [595.28, 841.89], align: 'center', valign: 'center' })
+if (images.length != x + 1) doc.addPage()}
+doc.on('data', (chunk) => buffs.push(chunk))
+doc.on('end', () => resolve(Buffer.concat(buffs)))
+doc.on('error', (err) => reject(err))
+doc.end()})}
