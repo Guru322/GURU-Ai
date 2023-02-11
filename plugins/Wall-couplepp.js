2c9fa16c447f0da1df3993f57c304da41472c47d
GURU <106463398+Guru322@users.noreply.github.com>
2023-02-11 13:14:47 +0530

    Guru [star the repo u guys plz]

--- plugins/Wall-couplepp.js
+++ plugins/Wall-couplepp.js
@@ -4,8 +4,8 @@ let res = await fetch(`https://api.lolhuman.xyz/api/random/ppcouple?apikey=${lol
 if (res.status != 200) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
-conn.sendButton(m.chat, 'KAWAI', wm, json.result.female, [['🔄 NEXT 🔄', `/${command}`]], m)
-conn.sendButton(m.chat, '𝙲𝚄𝚃𝙴', wm, json.result.male, [['🔄 NEXT 🔄', `/${command}`]], m)
+conn.sendButton(m.chat, 'KAWAI', json.result.female, [['🔄 NEXT 🔄', `/${command}`]], m)
+conn.sendButton(m.chat, '𝙲𝚄𝚃𝙴', json.result.male, [['🔄 NEXT 🔄', `/${command}`]], m)
 }
 handler.help = ['ppcouple']
 handler.tags = ['wall']
