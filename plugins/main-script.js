9fe6d74f9693a8ff352cf0b3c49bdcbd116d87e6
GURU <106463398+Guru322@users.noreply.github.com>
2023-02-14 14:45:59 +0530

    GURU

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -1,17 +1,31 @@
-import { promises } from 'fs'
-import { join } from 'path'
-
-let handler = async function (m, { conn, __dirname }) {
-let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-  
-m.reply(`
-*≡ SCRIPT*
-
-▢ Git : https://github.com/Guru322/GURU-BOT-V1`    
-}
-
-handler.help = ['script']
-handler.tags = ['main']
-handler.command = ['sc', 'git', 'script'] 
-
+let handler = async (m, { conn, usedPrefix }) => {
+var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
+var document = doc[Math.floor(Math.random() * doc.length)]    
+let text1 = `*https://github.com/Guru322/GURU-BOT*`
+let pp = './Guru.jpg'
+let buttonMessage= {
+'document': { url: `https://github.com/Guru322/GURU-BOT` },
+'mimetype': `application/${document}`,
+'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
+'fileLength': 99999999999999,
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': 'https://github.com/Guru322/GURU-BOT',
+'mediaType': 2,
+'previewType': 'pdf',
+'title': 'ʙᴏᴛ  ᴡʜᴀᴛsᴀᴘᴘ',
+'body': author,
+'thumbnail': pp,
+'sourceUrl': 'https://youtube.com/@Asliguru' }},
+'caption': text1,
+'footer': author,
+'buttons':[
+{buttonId: `${usedPrefix}menu`, buttonText: {displayText: 'MENU'}, type: 1}, 
+{buttonId: `${usedPrefix}info`, buttonText: {displayText: 'INFO'}, type: 1}],
+'headerType': 6 }
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
+handler.command = ['sc','script']
 export default handler
