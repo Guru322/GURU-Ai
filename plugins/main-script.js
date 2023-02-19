813066648cde3b448e35cc0ac38ec269422fd901
GURU <106463398+Guru322@users.noreply.github.com>
2023-02-19 15:20:46 +0530

    GURU

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -1,31 +1,21 @@
-let handler = async (m, { conn, usedPrefix }) => {
-var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
-var document = doc[Math.floor(Math.random() * doc.length)]    
-let text1 = `*https://github.com/Guru322/GURU-BOT*`
-let pp = './Guru.jpg'
-let buttonMessage= {
-'document': { url: `https://github.com/Guru322/GURU-BOT` },
-'mimetype': `application/${document}`,
-'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
-'fileLength': 99999999999999,
-'pageCount': 200,
-'contextInfo': {
-'forwardingScore': 200,
-'isForwarded': true,
-'externalAdReply': {
-'mediaUrl': 'https://github.com/Guru322/GURU-BOT',
-'mediaType': 2,
-'previewType': 'pdf',
-'title': 'ʙᴏᴛ  ᴡʜᴀᴛsᴀᴘᴘ',
-'body': author,
-'thumbnail': pp,
-'sourceUrl': 'https://youtube.com/@Asliguru' }},
-'caption': text1,
-'footer': author,
-'buttons':[
-{buttonId: `${usedPrefix}menu`, buttonText: {displayText: 'MENU'}, type: 1}, 
-{buttonId: `${usedPrefix}info`, buttonText: {displayText: 'INFO'}, type: 1}],
-'headerType': 6 }
-conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
-handler.command = ['sc','script']
+import fetch from 'node-fetch'
+
+let handler = async function (m, { conn, text, usedPrefix }) {
+  
+let m2 = `
+ᴛʜᴇ ɢᴜʀᴜ ʙᴏᴛ ꜱᴄʀɪᴘᴛ
+
+https://github.com/Guru322/GURU-BOT
+
+ᴅᴏ ꜱᴛᴀʀ ᴛʜᴇ ʀᴇᴘᴏ ᴀɴᴅ ꜰᴏʀᴋ
+`
+    let pp = './Guru.jpg' 
+    conn.sendButton(m.chat, m2, '▢ ᴳᵁᴿᵁ  ┃ ᴮᴼᵀ\n▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n', pp,m, rpyt)
+   
+}
+
+handler.help = ['audios']
+handler.tags = ['main']
+handler.command = ['script', 'sc', 'repo'] 
+
 export default handler
