d6873499bc72e872d8a362b01cc99d36a4ac7520
GURU <106463398+Guru322@users.noreply.github.com>
2023-02-23 07:22:21 +0530

    GURU

---
+++ replit.nix
@@ -0,0 +1,20 @@
+{ pkgs }: {
+    deps = [
+        pkgs.nodejs
+        pkgs.nodePackages.typescript
+        pkgs.ffmpeg
+        pkgs.imagemagick
+        pkgs.git
+        pkgs.neofetch
+        pkgs.libwebp
+        pkgs.speedtest-cli
+        pkgs.wget
+        pkgs.yarn
+        pkgs.libuuid
+    ];
+    env = {
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.libuuid
+        ];
+    };
+}
