f3ad1e00a86de88993d7f2e378996a363d14dd76
GURU <106463398+Guru322@users.noreply.github.com>
2023-02-26 17:21:08 +0530

    GURU

--- plugins/econ-shop.js
+++ plugins/econ-shop.js
@@ -1,24 +1,99 @@
 //import db from '../lib/database.js'
-
-const xpperdiamond = 350 
-let handler = async (m, { conn, command, args }) => {
-  let count = command.replace(/^buy/i, '')
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperdiamond) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
-  count = Math.max(1, count)
-  if (global.db.data.users[m.sender].exp >= xpperdiamond * count) {
-    global.db.data.users[m.sender].exp -= xpperdiamond * count
-    global.db.data.users[m.sender].diamond += count
-    conn.reply(m.chat, `
-┌─「 *PAYMENT NOTE* 」
-‣ *nominal purchase* : + ${count}💎 
-‣ *Spent* : -${xpperdiamond * count} XP
-└──────────────`, m)
-  } else conn.reply(m.chat, `❎ Sorry, you don't have enough *XP* to buy *${count}* Diamonds💎\n\n You can get *XP* using the commands from the *games and economy menu*`, m)
+const items = {
+    buy: {
+        limit: {
+            exp: 999
+        },
+        potion: {
+            money: 1250,
+        },
+        trash: {
+            money: 4,
+        },
+        wood: {
+            money: 700
+        },
+        rock: {
+            money: 850
+        },
+        string: {
+            money: 400
+        },
+        iron: { 
+        	money: 3000
+        }
+    },
+    sell: {
+        potion: {
+            money: 125,
+        },
+        trash: {
+            money: 2
+        },
+        wood: {
+            money: 600
+        },
+        rock: {
+            money: 750
+        },
+        string: {
+            money: 300
+        },
+        iron: {
+            money: 2500
+        },
+        gold: {
+            money: 4700
+        },
+        diamond: {
+            money: 9000
+        },
+        emerald: {
+            money: 15000
+        }
+    }
 }
-handler.help = ['buy', 'buyall']
-handler.tags = ['econ']
-handler.command = ['buy', 'buyall'] 
+
+let handler = async (m, { command, usedPrefix, args }) => {
+    let user = global.db.data.users[m.sender]
+    const listItems = Object.fromEntries(Object.entries(items[command.toLowerCase()]).filter(([v]) => v && v in user))
+    const info = `
+Use Format *${usedPrefix}${command} [crate] [count]*
+Usage example: *${usedPrefix}${command} potion 10*
+    
+📍Items list: 
+${Object.keys(listItems).map((v) => {
+        let paymentMethod = Object.keys(listItems[v]).find(v => v in user)
+        return `${global.rpg.emoticon(v)}${v} | ${listItems[v][paymentMethod]} ${global.rpg.emoticon(paymentMethod)}${paymentMethod}`.trim()
+    }).join('\n')}
+`.trim()
+    const item = (args[0] || '').toLowerCase()
+    const total = Math.floor(isNumber(args[1]) ? Math.min(Math.max(parseInt(args[1]), 1), Number.MAX_SAFE_INTEGER) : 1) * 1
+    if (!listItems[item]) return m.reply(info)
+    if (command.toLowerCase() == 'buy') {
+        let paymentMethod = Object.keys(listItems[item]).find(v => v in user)
+        if (user[paymentMethod] < listItems[item][paymentMethod] * total) return m.reply(`You don't have enough ${global.rpg.emoticon(paymentMethod)}${paymentMethod} to buy *${total}* ${global.rpg.emoticon(item)}${item}. You need *${(listItems[item][paymentMethod] * total) - user[paymentMethod]}* more ${paymentMethod} to be able to buy`)
+        user[paymentMethod] -= listItems[item][paymentMethod] * total
+        user[item] += total
+        return m.reply(`You bought *${total}* ${global.rpg.emoticon(item)}${item}`)
+    } else {
+        if (user[item] < total) return m.reply(`You don't have enough *${global.rpg.emoticon(item)}${item}* to sell, you only have ${user[item]} items`)
+        user[item] -= total
+        user.money += listItems[item].money * total
+        return m.reply(`You sold *${total}* ${global.rpg.emoticon(item)}${item}`)
+    }
+}
+
+handler.help = ['buy', 'sell'].map(v => v + ' [item] [count]')
+handler.tags = ['rpg']
+handler.command = /^(buy|sell)$/i
 
 handler.disabled = false
 
 export default handler
+
+function isNumber(number) {
+    if (!number) return number
+    number = parseInt(number)
+    return typeof number == 'number' && !isNaN(number)
+}
