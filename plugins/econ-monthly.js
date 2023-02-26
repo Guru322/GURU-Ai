99bc5623383986f900f3c669640979d1d3817e5e
GURU <106463398+Guru322@users.noreply.github.com>
2023-02-26 09:37:30 +0530

    GURU

---
+++ plugins/econ-monthly.js
@@ -0,0 +1,27 @@
+const rewards = {
+    exp: 50000,
+    money: 49999,
+    potion: 10,
+    mythic: 3,
+    legendary: 1
+}
+
+const cooldown = 2592000000
+let handler = async (m) => {
+    let user = global.db.data.users[m.sender]
+    if (new Date - user.lastmonthly < cooldown) throw `You have already claimed this monthly claim, wait for *${((user.lastmonthly + cooldown) - new Date()).toTimeString()}*`
+    let text = ''
+    for (let reward of Object.keys(rewards)) if (reward in user) {
+        user[reward] += rewards[reward]
+        text += `*+${rewards[reward]}* ${rpg.emoticon(reward)}${reward}\n`
+    }
+    conn.sendButton(m.chat,'*––––––『 MONTHLY 』––––––*', text.trim(), null, [['Inventory', '.inv'], ['Menu', '.menu']],m)
+    user.lastmonthly = new Date * 1
+}
+handler.help = ['monthly']
+handler.tags = ['rpg']
+handler.command = /^(monthly)$/i
+
+handler.cooldown = cooldown
+
+export default handler
