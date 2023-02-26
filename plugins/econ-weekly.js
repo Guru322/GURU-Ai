2d9478386fe67e1fd5b46c45d64c7a9baccafbc5
GURU <106463398+Guru322@users.noreply.github.com>
2023-02-26 09:36:31 +0530

    GURU

---
+++ plugins/econ-weekly.js
@@ -0,0 +1,24 @@
+ exp: 15000,
+    money: 35999,
+    potion: 9,
+  }
+  const cooldown = 604800000
+  let handler = async (m) => {
+    let user = global.db.data.users[m.sender]
+    if (new Date - user.lastweekly < cooldown) throw `You have already claimed this daily claim!, wait for *${((user.lastweekly + cooldown) - new Date()).toTimeString()}*`
+    let text = ''
+    for (let reward of Object.keys(rewards)) {
+      if (!(reward in user)) continue
+      user[reward] += rewards[reward]
+      text += `*+${rewards[reward]}* ${global.rpg.emoticon(reward)}${reward}\n`
+    }
+    conn.sendButton(m.chat,'*––––––『 WEEKLY 』––––––*', text.trim(), null, [['Inventory', '.inv'], ['Monthly', '.monthly']],m)
+    user.lastweekly = new Date * 1
+  }
+  handler.help = ['weekly']
+  handler.tags = ['rpg']
+  handler.command = /^(weekly)$/i
+  
+  handler.cooldown = cooldown
+  
+  export default handler
