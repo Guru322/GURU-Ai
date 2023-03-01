33be47cf87ba83e78141feefcdb9db80f75acc47
GURU <107632293+guru65476@users.noreply.github.com>
2023-03-01 20:01:01 +0530

    Delete node_modules/canvas directory

--- node_modules/canvas/build/Release/.deps/Release/canvas.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/canvas.node := ln -f "Release/obj.target/canvas.node" "Release/canvas.node" 2>/dev/null || (rm -rf "Release/canvas.node" && cp -af "Release/obj.target/canvas.node" "Release/canvas.node")
