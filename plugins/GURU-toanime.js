e8369b989c804944006b397d174a61923d1bb287
GURU <106463398+Guru322@users.noreply.github.com>
2023-03-11 20:57:34 +0530

    ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ

--- plugins/GURU-toanime.js
+++ plugins/GURU-toanime.js
@@ -22,6 +22,6 @@ throw '*Error check if the persons face is visible*'
 }}}}
 handler.help = ["toanime"]
 handler.tags = ["tools"]
-handler.diamond = true
+handler.diamond = ${premium}
 handler.command = /^(imganime|toanime)$/i
 export default handler
