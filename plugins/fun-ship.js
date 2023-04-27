7daf67fb3d6b55418144b0bb5b207d81fe0d2807
GURU <106463398+Guru322@users.noreply.github.com>
2023-04-27 10:13:09 +0000

    GURU

---
+++ plugins/fun-ship.js
@@ -0,0 +1,18 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+    let ps = groupMetadata.participants.map(v => v.id)
+    let a = m.sender
+    let b
+    do b = ps.getRandom()
+    while (b === a)
+    m.reply(`${toM(a)} ❤️ ${toM(b)}\nCongratulations 💖🍻`, null, {
+        mentions: [a, b]
+    })
+}
+handler.help = ['ship']
+handler.tags = ['fun']
+handler.command = ['ship']
+
+handler.group = true
+
+export default handler  
---
+++ plugins/fun-truth.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+let handler  = async (m, { conn }) => {
+    let shizokeys = 'shizo'	
+  let res = await fetch(`https://api.shizo-devs.repl.co/api/texts/truth?apikey=${shizokeys}`)
+  if (!res.ok) throw await res.text()
+	    let json = await res.json()
+
+  let guru = `${json.result}`
+  conn.sendMessage(m.chat, { text: guru, mentions: [m.sender] }, { quoted: m })
+}
+handler.help = ['truth']
+handler.tags = ['fun']
+handler.command = /^(truth)$/i
+
+export default handler
+
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
