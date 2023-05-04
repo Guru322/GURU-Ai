22b71aad627594dea318170663df2db421d1e63e
GURU <106463398+Guru322@users.noreply.github.com>
2023-05-04 19:15:09 +0530

    Update nsfw-nsfw.js

--- plugins/nsfw-nsfw.js
+++ plugins/nsfw-nsfw.js
@@ -13,29 +13,29 @@ switch (type) {
 
 case 'ass':
 case 'gand':
-    let as = await (await fetch(global.API('fgmods', '/api/nsfw/ass', { }, 'apikey'))).json() 
-    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, as.result, [['▷▷ NEXT', `${usedPrefix + command}`]], m)
+    let as = await conn.getFile(global.API('fgmods', '/api/nsfw/ass', { }, 'apikey'))).json() 
+    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, as.data, [['▷▷ NEXT', `${usedPrefix + command}`]], m)
    m.react(xmoji) 
 break
 
 case 'boobs':
 case 'boobies':
-   let xb = await (await fetch(global.API('fgmods', '/api/nsfw/boobs', { }, 'apikey'))).json()
-    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, xb.result, [['▷▷ NEXT', `${usedPrefix + command}`]], m)
+   let xb = await conn.getFile(global.API('fgmods', '/api/nsfw/boobs', { }, 'apikey'))).json()
+    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, xb.data, [['▷▷ NEXT', `${usedPrefix + command}`]], m)
    m.react(xmoji) 
 break
 
 case 'pussy':
 case 'chut':		
-   let xp = await (await fetch(global.API('fgmods', '/api/nsfw/pussy', { }, 'apikey'))).json()
-    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, xp.result, [['▷▷ NEXT', `${usedPrefix + command}`]], m)
+   let xp = await conn.getFile(global.API('fgmods', '/api/nsfw/pussy', { }, 'apikey'))).json()
+    conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, xp.data, [['▷▷ NEXT', `${usedPrefix + command}`]], m)
    m.react(xmoji) 
 break
 
 case 'lesbians':
 case 'lesbian':
-   let les = await (await fetch(global.API('fgmods', '/api/nsfw/lesbian', { }, 'apikey'))).json()
-   conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, les.result, [['▷▷ NEXT', `${usedPrefix + command}`]], m)
+   let les = await conn.getFile(global.API('fgmods', '/api/nsfw/lesbian', { }, 'apikey'))).json()
+   conn.sendButton(m.chat, `✅ Random *${command}*`, fgyt, les.data, [['▷▷ NEXT', `${usedPrefix + command}`]], m)
    m.react(xmoji) 
 break
 
