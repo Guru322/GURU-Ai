ea179f424e6702d9d49baf86d902348b2cdf973d
GURU <106463398+Guru322@users.noreply.github.com>
2023-05-12 20:28:50 +0530

    remving buttons

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -6,8 +6,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 ONLY WANT YOUR LOVE
 SUPPORT ME BY MAKING 69 MILLION FOLLOWERS ON MY INSTAGRAM`
 let img = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
-conn.sendHydrated2(m.chat, don, '▢ MAY LORD JAGANNATH BLESS YOUR DAY\n▢ ᴳᵁᴿᵁ  ┃ ᴮᴼᵀ\n▢ Subscribe  Youtube\nhttps://youtube.com/@Asliguru\n', img,'https://www.instagram.com/asli_guru69', 'Instagram', 'https://t.me/i_want_to_be_isekaied', 'TELEGRAM', [
-    ], m)
+conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 
 handler.help = ['donate']
