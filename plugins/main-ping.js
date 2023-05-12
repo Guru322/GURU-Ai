24ed97d03882a12d4645fde51c25f64dfa417197
MR Katussa <108258060+Mr-Katussa@users.noreply.github.com>
2023-05-12 13:16:57 +0530

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn }) => {
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-          m.reply(`${ssd}🟢 *pong* : ${latensi.toFixed(4)} _ms_`);
+          m.reply(`${ssd}🟢 *S P E E D* : ${latensi.toFixed(4)} _ms_`);
             });
 }
 handler.help = ['ping']
