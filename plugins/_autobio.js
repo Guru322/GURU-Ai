14129934227deef44da32abddd06746e0e125d43
MR Katussa <108258060+Mr-Katussa@users.noreply.github.com>
2023-05-12 09:58:16 +0530

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Time Active ${muptime}\n\n ┃ 💎  By GURU`
+		let bio = `\n🟢 Time Active ${muptime}\n\n ┃ 💎  By Sandaru`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
