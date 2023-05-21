4db25f4fca437470246e5c54c8a44903e329a640
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-21 22:26:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.owner = [
 
 global.mods = ['919148942515', 'Vivek'] 
 global.prems = ['919148942515', 'Vivek']
-global.developer = ['919148942515', 'Vivek'] 
+global.developer = ['Vivek'] 
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
