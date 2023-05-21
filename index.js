4eaf68dc8303ab98327ab12d412e046712423d9a
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-22 01:35:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('GURU -  BOT', {
+say('TROUBLEMAKER -  BOT', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @GURU._`, {
+say(`'${name}' By @VIVEK._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
