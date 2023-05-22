42b3d7dc6b52ccc488194bcbceccd1c51d1ad421
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-22 19:03:59 +0530

    Update img-asupan.js

--- plugins/img-asupan.js
+++ plugins/img-asupan.js
@@ -3,7 +3,6 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 	
 	let img = await conn.getFile(global.API('fgmods', '/api/asupan-la', { }, 'apikey'))
     let asupan = img.data
-    conn.sendButton(m.chat, `✅ Result`, igfg, asupan, [['▷▷ NEXT', `${usedPrefix + command}`]], m)
     m.react(dmoji)
     
 }
