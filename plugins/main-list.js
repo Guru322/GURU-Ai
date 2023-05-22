bc01a5b956e3a66a6fe16317caa3b862d9ea2a05
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-22 13:08:37 +0530

    Update main-list.js

--- plugins/main-list.js
+++ plugins/main-list.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems })  => m.reply(`
 ┏━━━━━━━━━━━━━━━━┓
-┃  *<All menu />*
+┃  *<TROUBLEMAKER menu />*
 ┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
 ┣  💟 groups
 ┣  💟 infobot
@@ -18,273 +18,273 @@ let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isP
 ┣  💟 ytcomment
 ┣  💟 whatmusic
 ┣  💟 lolicon
-┣ ඬ⃟ 💎 _${usedPrefix}kick *<@tag>*_
-┣ ඬ⃟ 💎 _${usedPrefix}promote *<@tag>*_
-┣ ඬ⃟ 💎 _${usedPrefix}demote *<@tag>*_
-┣ ඬ⃟ 💎 _${usedPrefix}demote *<@tag>*_
-┣ ඬ⃟ 💎 _${usedPrefix}infogroup_
-┣ ඬ⃟ 💎 _${usedPrefix}resetlink_
-┣ ඬ⃟ 💎 _${usedPrefix}link_
-┣ ඬ⃟ 💎 _${usedPrefix}setname *<text>*_
-┣ ඬ⃟ 💎 _${usedPrefix}setdesc *<text>*_
-┣ ඬ⃟ 💎 _${usedPrefix}invocar *<text>*_
-┣ ඬ⃟ 💎 _${usedPrefix}setwelcome *<text>*_
-┣ ඬ⃟ 💎 _${usedPrefix}setbye *<text>*_
-┣ ඬ⃟ 💎 _${usedPrefix}hidetag *<text>*_
-┣ ඬ⃟ 💎 _${usedPrefix}hidetag *<audio>*_
-┣ ඬ⃟ 💎 _${usedPrefix}hidetag *<video>*_
-┣ ඬ⃟ 💎 _${usedPrefix}hidetag *<image>*_
-┣ ඬ⃟ 💎 _${usedPrefix}warn *<@tag>*_
-┣ ඬ⃟ 💎 _${usedPrefix}unwarn *<@tag>*_
-┣ ඬ⃟ 💎 _${usedPrefix}listwarn_
-┣ ඬ⃟ 💎 _${usedPrefix}setpp *<image>*_
-┣ ඬ⃟ 💵 _${usedPrefix}balance_
-┣ ඬ⃟ 💵 _${usedPrefix}claim_
-┣ ඬ⃟ 💵 _${usedPrefix}lb_
-┣ ඬ⃟ 💵 _${usedPrefix}levelup_
-┣ ඬ⃟ 💵 _${usedPrefix}myns_
-┣ ඬ⃟ 💵 _${usedPrefix}profile_
-┣ ඬ⃟ 💵 _${usedPrefix}work_
-┣ ඬ⃟ 💵 _${usedPrefix}adventure_
-┣ ඬ⃟ 💵 _${usedPrefix}heal_
-┣ ඬ⃟ 💵 _${usedPrefix}dungeon_
-┣ ඬ⃟ 💵 _${usedPrefix}todiamond_
-┣ ඬ⃟ 💵 _${usedPrefix}tomoney_
-┣ ඬ⃟ 💵 _${usedPrefix}weekly_
-┣ ඬ⃟ 💵 _${usedPrefix}monthly_
-┣ ඬ⃟ 💵 _${usedPrefix}mine_
-┣ ඬ⃟ 💵 _${usedPrefix}mine_
-┣ ඬ⃟ 💵 _${usedPrefix}buy_
-┣ ඬ⃟ 💵 _${usedPrefix}sell_
-┣ ඬ⃟ 💵 _${usedPrefix}todiamondall_
-┣ ඬ⃟ 💵 _${usedPrefix}register_
-┣ ඬ⃟ 💵 _${usedPrefix}rob <@tag>*_
-┣ ඬ⃟ 💵 _${usedPrefix}transfer *<type> <amount> <@tag>*_
-┣ ඬ⃟ 💵 _${usedPrefix}ureg*<seriel num>*_
-┣ ඬ⃟ 👑 _${usedPrefix}setprefix *<prefix>*_
-┣ ඬ⃟ 👑 _${usedPrefix}resetprefix_
-┣ ඬ⃟ 👑 _${usedPrefix}autoadmin_
-┣ ඬ⃟ 👑 _${usedPrefix}leavegc_
-┣ ඬ⃟ 👑 _${usedPrefix}blocklist_
-┣ ඬ⃟ 👑 _${usedPrefix}block *<@tag / number>*_
-┣ ඬ⃟ 👑 _${usedPrefix}unblock *<@tag / number>*_
-┣ ඬ⃟ 👑 _${usedPrefix}enable *restrict*_
-┣ ඬ⃟ 👑 _${usedPrefix}disable *restrict*_
-┣ ඬ⃟ 👑 _${usedPrefix}enable *autoread*_
-┣ ඬ⃟ 👑 _${usedPrefix}disable *autoread*_
-┣ ඬ⃟ 👑 _${usedPrefix}enable *public*_
-┣ ඬ⃟ 👑 _${usedPrefix}disable *public*_
-┣ ඬ⃟ 👑 _${usedPrefix}enable *pconly*_
-┣ ඬ⃟ 👑 _${usedPrefix}disable *pconly*_
-┣ ඬ⃟ 👑 _${usedPrefix}enable *gconly*_
-┣ ඬ⃟ 👑 _${usedPrefix}disable *gconly*_
-┣ ඬ⃟ 👑 _${usedPrefix}msg *<text>*_
-┣ ඬ⃟ 👑 _${usedPrefix}banchat_
-┣ ඬ⃟ 👑 _${usedPrefix}unbanchat_
-┣ ඬ⃟ 👑 _${usedPrefix}banuser *<@tag>*_
-┣ ඬ⃟ 👑 _${usedPrefix}unbanuser *<@tag>*_
-┣ ඬ⃟ 👑 _${usedPrefix}banuser *<@tag>*_
-┣ ඬ⃟ 👑 _${usedPrefix}bc *<text>*_
-┣ ඬ⃟ 👑 _${usedPrefix}bcbot *<text>*_
-┣ ඬ⃟ 👑 _${usedPrefix}cleartpm_
-┣ ඬ⃟ 👑 _${usedPrefix}restart_
-┣ ඬ⃟ 👑 _${usedPrefix}update_
-┣ ඬ⃟ 👑 _${usedPrefix}banlist_
-┣ ඬ⃟ 👑 _${usedPrefix}addprem *<@tag>*_
-┣ ඬ⃟ 👑 _${usedPrefix}delprem *<@tag>*_
-┣ ඬ⃟ 👑 _${usedPrefix}listprem_
-┣ ඬ⃟ 👑 _${usedPrefix}listcmd_
-┣ ඬ⃟ 👑 _${usedPrefix}setppbot *<respondr a image>*_
-┣ ඬ⃟ 👑 _${usedPrefix}addcmd *<text> <respond a sticker/image>*_
-┣ ඬ⃟ 📥 _${usedPrefix}instagram *< / link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}mediafire *< LINK >
-┣ ඬ⃟ 📥 _${usedPrefix}instagram *< link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}gitclone *< link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}gdrive *< link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}tiktok *< link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}xnxxdl *< link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}xvideosdl *<link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}twitter *<  link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}fb *< link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}ytmp3 *< link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}ytmp4 *< link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}ytmp3doc *< link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}ytmp4doc *< link / url>*_
-┣ ඬ⃟ 📥 _${usedPrefix}play *<text>*_
-┣ ඬ⃟ 📥 _${usedPrefix}spotify *<text>*_
-┣ ඬ⃟ 🔞 _${usedPrefix}videoxxx_
-┣ ඬ⃟ 🔞 _${usedPrefix}videolesbixxx_
-┣ ඬ⃟ 🔞 _${usedPrefix}tit_
-┣ ඬ⃟ 🔞 _${usedPrefix}booty_
-┣ ඬ⃟ 🔞 _${usedPrefix}ecchi_
-┣ ඬ⃟ 🔞 _${usedPrefix}furro_
-┣ ඬ⃟ 🔞 _${usedPrefix}lesbians_
-┣ ඬ⃟ 🔞 _${usedPrefix}panties_
-┣ ඬ⃟ 🔞 _${usedPrefix}penis_
-┣ ඬ⃟ 🔞 _${usedPrefix}porn_
-┣ ඬ⃟ 🔞 _${usedPrefix}randomxxx_
-┣ ඬ⃟ 🔞 _${usedPrefix}pechos_
-┣ ඬ⃟ 🔞 _${usedPrefix}yaoi_
-┣ ඬ⃟ 🔞 _${usedPrefix}yaoi2_
-┣ ඬ⃟ 🔞 _${usedPrefix}yuri_
-┣ ඬ⃟ 🔞 _${usedPrefix}yuri2_
-┣ ඬ⃟ 🔞 _${usedPrefix}trap_
-┣ ඬ⃟ 🔞 _${usedPrefix}hentai_
-┣ ඬ⃟ 🔞 _${usedPrefix}hloli_
-┣ ඬ⃟ 🔞 _${usedPrefix}orgy_
-┣ ඬ⃟ 🔞 _${usedPrefix}foot_
-┣ ඬ⃟ 🔞 _${usedPrefix}hass_
-┣ ඬ⃟ 🔞 _${usedPrefix}bdsm_
-┣ ඬ⃟ 🔞 _${usedPrefix}cum_
-┣ ඬ⃟ 🔞 _${usedPrefix}ero_
-┣ ඬ⃟ 🔞 _${usedPrefix}femdom_
-┣ ඬ⃟ 🔞 _${usedPrefix}glass_
-┣ ඬ⃟ 🔞 _${usedPrefix}hentai*_
-┣ ඬ⃟ 🔞 _${usedPrefix}underwear_
-┣ ඬ⃟ 🔞 _${usedPrefix}spussy_
-┣ ඬ⃟ 🔞 _${usedPrefix}bunnygirl_
-┣ ඬ⃟ 🔞 _${usedPrefix}bunnyear_
-┣ ඬ⃟ 🔞 _${usedPrefix}sswimsuit_
-┣ ඬ⃟ 🔞 _${usedPrefix}chain_
-┣ ඬ⃟ 🔞 _${usedPrefix}genshin_
-┣ ඬ⃟ 🔞 _${usedPrefix}white_
-┣ ඬ⃟ 🔞 _${usedPrefix}barefoot_
-┣ ඬ⃟ 🔞 _${usedPrefix}whitehair_
-┣ ඬ⃟ 🔞 _${usedPrefix}touhou_
-┣ ඬ⃟ 🔞 _${usedPrefix}holo_
-┣ ඬ⃟ 🔞 _${usedPrefix}gamecg_
-┣ ඬ⃟ 🔞 _${usedPrefix}uncensored_
-┣ ඬ⃟ 🔞 _${usedPrefix}sunglass_
-┣ ඬ⃟ 🔞 _${usedPrefix}glass_
-┣ ඬ⃟ 🔞 _${usedPrefix}demon_
-┣ ඬ⃟ 🔞 _${usedPrefix}bondage_
-┣ ඬ⃟ 🔞 _${usedPrefix}torn cloth_
-┣ ඬ⃟ 🔞 _${usedPrefix}fingering_
-┣ ඬ⃟ 🔞 _${usedPrefix}gun_
-┣ ඬ⃟ 🔞 _${usedPrefix}vampire_
-┣ ඬ⃟ 🔞 _${usedPrefix}idol_
-┣ ඬ⃟ 🔞 _${usedPrefix}beach_
-┣ ඬ⃟ 🔞 _${usedPrefix}bra_
-┣ ඬ⃟ 🔞 _${usedPrefix}topless_
-┣ ඬ⃟ 🔞 _${usedPrefix}stokings_
-┣ ඬ⃟ 🔞 _${usedPrefix}shorts_
-┣ ඬ⃟ 🔞 _${usedPrefix}anus_
-┣ ඬ⃟ 🔞 _${usedPrefix}tie_
-┣ ඬ⃟ 🔞 _${usedPrefix}headphone_
-┣ ඬ⃟ 🔞 _${usedPrefix}pantypull_
-┣ ඬ⃟ 🔞 _${usedPrefix}wet_
-┣ ඬ⃟ 🔞 _${usedPrefix}breast_
-┣ ඬ⃟ 🔞 _${usedPrefix}twintail_
-┣ ඬ⃟ 🔞 _${usedPrefix}sex_
-┣ ඬ⃟ 🔞 _${usedPrefix}sex2_
-┣ ඬ⃟ 🔞 _${usedPrefix}sex3_
-┣ ඬ⃟ 🔞 _${usedPrefix}skirt_
-┣ ඬ⃟ 🔞 _${usedPrefix}uniform_
-┣ ඬ⃟ 🔞 _${usedPrefix}foxgirl_
-┣ ඬ⃟ 🔞 _${usedPrefix}ponytail_
-┣ ඬ⃟ 🔞 _${usedPrefix}nude_
-┣ ඬ⃟ 🔞 _${usedPrefix}bed_
-┣ ඬ⃟ 🔞 _${usedPrefix}pinkhair_
-┣ ඬ⃟ 🔞 _${usedPrefix}bikini_
-┣ ඬ⃟ 🔞 _${usedPrefix}nobra_
-┣ ඬ⃟ 🔞 _${usedPrefix}maid_
-┣ ඬ⃟ 🧧 _${usedPrefix}toimg *<sticker>*_
-┣ ඬ⃟ 🧧 _${usedPrefix}tomp3 *<video>*_
-┣ ඬ⃟ 🧧 _${usedPrefix}tovideo *<sticker>*_
-┣ ඬ⃟ 🧧 _${usedPrefix}tourl *<video / image / audio>*_
-┣ ඬ⃟ 🧧 _${usedPrefix}tts en *<text>*_
-┣ ඬ⃟ 👾 _${usedPrefix}cristianoronaldo_
-┣ ඬ⃟ 👾 _${usedPrefix}messi_
-┣ ඬ⃟ 👾 _${usedPrefix}meme_
-┣ ඬ⃟ 👾 _${usedPrefix}itzy_
-┣ ඬ⃟ 👾 _${usedPrefix}blackpink_
-┣ ඬ⃟ 👾 _${usedPrefix}lolivid_
-┣ ඬ⃟ 👾 _${usedPrefix}loli_
-┣ ඬ⃟ 👾 _${usedPrefix}navidad_
-┣ ඬ⃟ 👾 _${usedPrefix}ppcouple_
-┣ ඬ⃟ 👾 _${usedPrefix}wpmontaña_
-┣ ඬ⃟ 👾 _${usedPrefix}pubg_
-┣ ඬ⃟ 👾 _${usedPrefix}wpgaming_
-┣ ඬ⃟ 👾 _${usedPrefix}wpaesthetic_
-┣ ඬ⃟ 👾 _${usedPrefix}wpaesthetic2_
-┣ ඬ⃟ 👾 _${usedPrefix}wprandom_
-┣ ඬ⃟ 👾 _${usedPrefix}wallhp_
-┣ ඬ⃟ 👾 _${usedPrefix}wpvehiculo_
-┣ ඬ⃟ 👾 _${usedPrefix}wpmoto_
-┣ ඬ⃟ 👾 _${usedPrefix}coffee_
-┣ ඬ⃟ 👾 _${usedPrefix}pentol_
-┣ ඬ⃟ 👾 _${usedPrefix}caricatura_
-┣ ඬ⃟ 👾 _${usedPrefix}ciberespacio_
-┣ ඬ⃟ 👾 _${usedPrefix}technology_
-┣ ඬ⃟ 👾 _${usedPrefix}doraemon_
-┣ ඬ⃟ 👾 _${usedPrefix}hacker_
-┣ ඬ⃟ 👾 _${usedPrefix}planeta_
-┣ ඬ⃟ 👾 _${usedPrefix}randomprofile_
-┣ ඬ⃟ 👾 _${usedPrefix}neko_
-┣ ඬ⃟ 👾 _${usedPrefix}waifu_
-┣ ඬ⃟ 👾 _${usedPrefix}akira_
-┣ ඬ⃟ 👾 _${usedPrefix}akiyama_
-┣ ඬ⃟ 👾 _${usedPrefix}anna_
-┣ ඬ⃟ 👾 _${usedPrefix}asuna_
-┣ ඬ⃟ 👾 _${usedPrefix}ayuzawa_
-┣ ඬ⃟ 👾 _${usedPrefix}boruto_
-┣ ඬ⃟ 👾 _${usedPrefix}chiho_
-┣ ඬ⃟ 👾 _${usedPrefix}chitoge_
-┣ ඬ⃟ 👾 _${usedPrefix}deidara_
-┣ ඬ⃟ 👾 _${usedPrefix}erza_
-┣ ඬ⃟ 👾 _${usedPrefix}elaina_
-┣ ඬ⃟ 👾 _${usedPrefix}eba_
-┣ ඬ⃟ 👾 _${usedPrefix}emilia_
-┣ ඬ⃟ 👾 _${usedPrefix}hestia_
-┣ ඬ⃟ 👾 _${usedPrefix}hinata_
-┣ ඬ⃟ 👾 _${usedPrefix}inori_
-┣ ඬ⃟ 👾 _${usedPrefix}isuzu_
-┣ ඬ⃟ 👾 _${usedPrefix}itachi_
-┣ ඬ⃟ 👾 _${usedPrefix}itori_
-┣ ඬ⃟ 👾 _${usedPrefix}kaga_
-┣ ඬ⃟ 👾 _${usedPrefix}kagura_
-┣ ඬ⃟ 👾 _${usedPrefix}kaori_
-┣ ඬ⃟ 👾 _${usedPrefix}keneki_
-┣ ඬ⃟ 👾 _${usedPrefix}kotori_
-┣ ඬ⃟ 👾 _${usedPrefix}kurumi_
-┣ ඬ⃟ 👾 _${usedPrefix}madara_
-┣ ඬ⃟ 👾 _${usedPrefix}mikasa_
-┣ ඬ⃟ 👾 _${usedPrefix}miku_
-┣ ඬ⃟ 👾 _${usedPrefix}minato_
-┣ ඬ⃟ 👾 _${usedPrefix}naruto_
-┣ ඬ⃟ 👾 _${usedPrefix}nezuko_
-┣ ඬ⃟ 👾 _${usedPrefix}sagiri_
-┣ ඬ⃟ 👾 _${usedPrefix}sasuke_
-┣ ඬ⃟ 👾 _${usedPrefix}sakura_
-┣ ඬ⃟ 👾 _${usedPrefix}cosplay_
-┣ ඬ⃟ 🛠️ _${usedPrefix}chatgpt *<text>*_
-┣ ඬ⃟ 🛠️ _${usedPrefix}dall-e *<text>*_
-┣ ඬ⃟ 🛠️ _${usedPrefix}styletext *<text>*_
-┣ ඬ⃟ 👽 _${usedPrefix}sticker *<respond a image video>*_
-┣ ඬ⃟ 👽 _${usedPrefix}sticker *<link / url>*_
-┣ ඬ⃟ 👽 _${usedPrefix}s *<respond a image video>*_
-┣ ඬ⃟ 👽 _${usedPrefix}s *<link / url>*_
-┣ ඬ⃟ 👽 _${usedPrefix}sfull *<imagen o video>*_
-┣ ඬ⃟ 👽 _${usedPrefix}emojimix *<emoji 1>&<emoji 2>*_
-┣ ඬ⃟ 👽 _${usedPrefix}scircle *<image>*_
-┣ ඬ⃟ 👽 _${usedPrefix}sremovebg *<image>*_
-┣ ඬ⃟ 👽 _${usedPrefix}semoji *<tip> <emoji>*_
-┣ ඬ⃟ 👽 _${usedPrefix}attp *<text>*_
-┣ ඬ⃟ 👽 _${usedPrefix}attp2 *<text>*_
-┣ ඬ⃟ 👽 _${usedPrefix}attp3 *<text>*_
-┣ ඬ⃟ 👽 _${usedPrefix}ttp *<text>*_
-┣ ඬ⃟ 👽 _${usedPrefix}ttp2 *<text>*_
-┣ ඬ⃟ 👽 _${usedPrefix}ttp3 *<text>*_
-┣ ඬ⃟ 👽 _${usedPrefix}ttp4 *<text>*_
-┣ ඬ⃟ 👽 _${usedPrefix}ttp5 *<text>*_
-┣ ඬ⃟ 👽 _${usedPrefix}pat *<@tag>*_
-┣ ඬ⃟ 👽 _${usedPrefix}slap *<@tag>*_
-┣ ඬ⃟ 👽 _${usedPrefix}kiss *<@tag>*_
-┣ ඬ⃟ 👽 _${usedPrefix}wm *<packname> <author>*_
+┣ 💎 _${usedPrefix}kick *<@tag>*_
+┣ 💎 _${usedPrefix}promote *<@tag>*_
+┣ 💎 _${usedPrefix}demote *<@tag>*_
+┣ 💎 _${usedPrefix}demote *<@tag>*_
+┣ 💎 _${usedPrefix}infogroup_
+┣ 💎 _${usedPrefix}resetlink_
+┣ 💎 _${usedPrefix}link_
+┣ 💎 _${usedPrefix}setname *<text>*_
+┣ 💎 _${usedPrefix}setdesc *<text>*_
+┣ 💎 _${usedPrefix}invocar *<text>*_
+┣ 💎 _${usedPrefix}setwelcome *<text>*_
+┣ 💎 _${usedPrefix}setbye *<text>*_
+┣ 💎 _${usedPrefix}hidetag *<text>*_
+┣ 💎 _${usedPrefix}hidetag *<audio>*_
+┣ 💎 _${usedPrefix}hidetag *<video>*_
+┣ 💎 _${usedPrefix}hidetag *<image>*_
+┣ 💎 _${usedPrefix}warn *<@tag>*_
+┣ 💎 _${usedPrefix}unwarn *<@tag>*_
+┣ 💎 _${usedPrefix}listwarn_
+┣ 💎 _${usedPrefix}setpp *<image>*_
+┣ 💵_${usedPrefix}balance_
+┣ 💵 _${usedPrefix}claim_
+┣ 💵 _${usedPrefix}lb_
+┣ 💵 _${usedPrefix}levelup_
+┣ 💵 _${usedPrefix}myns_
+┣ 💵 _${usedPrefix}profile_
+┣ 💵 _${usedPrefix}work_
+┣ 💵 _${usedPrefix}adventure_
+┣ 💵 _${usedPrefix}heal_
+┣ 💵 _${usedPrefix}dungeon_
+┣ 💵 _${usedPrefix}todiamond_
+┣ 💵 _${usedPrefix}tomoney_
+┣ 💵 _${usedPrefix}weekly_
+┣ 💵 _${usedPrefix}monthly_
+┣ 💵 _${usedPrefix}mine_
+┣ 💵 _${usedPrefix}mine_
+┣ 💵 _${usedPrefix}buy_
+┣ 💵 _${usedPrefix}sell_
+┣ 💵 _${usedPrefix}todiamondall_
+┣ 💵 _${usedPrefix}register_
+┣ 💵 _${usedPrefix}rob <@tag>*_
+┣ 💵 _${usedPrefix}transfer *<type> <amount> <@tag>*_
+┣ 💵 _${usedPrefix}ureg*<seriel num>*_
+┣ 👑 _${usedPrefix}setprefix *<prefix>*_
+┣ 👑 _${usedPrefix}resetprefix_
+┣ 👑 _${usedPrefix}autoadmin_
+┣ 👑 _${usedPrefix}leavegc_
+┣ 👑 _${usedPrefix}blocklist_
+┣ 👑 _${usedPrefix}block *<@tag / number>*_
+┣ 👑 _${usedPrefix}unblock *<@tag / number>*_
+┣ 👑 _${usedPrefix}enable *restrict*_
+┣ 👑 _${usedPrefix}disable *restrict*_
+┣ 👑 _${usedPrefix}enable *autoread*_
+┣ 👑 _${usedPrefix}disable *autoread*_
+┣ 👑 _${usedPrefix}enable *public*_
+┣ 👑 _${usedPrefix}disable *public*_
+┣ 👑 _${usedPrefix}enable *pconly*_
+┣ 👑 _${usedPrefix}disable *pconly*_
+┣ 👑 _${usedPrefix}enable *gconly*_
+┣ 👑 _${usedPrefix}disable *gconly*_
+┣ 👑 _${usedPrefix}msg *<text>*_
+┣ 👑 _${usedPrefix}banchat_
+┣ 👑 _${usedPrefix}unbanchat_
+┣ 👑 _${usedPrefix}banuser *<@tag>*_
+┣ 👑 _${usedPrefix}unbanuser *<@tag>*_
+┣ 👑 _${usedPrefix}banuser *<@tag>*_
+┣ 👑 _${usedPrefix}bc *<text>*_
+┣ 👑 _${usedPrefix}bcbot *<text>*_
+┣ 👑 _${usedPrefix}cleartpm_
+┣ 👑 _${usedPrefix}restart_
+┣ 👑 _${usedPrefix}update_
+┣ 👑 _${usedPrefix}banlist_
+┣ 👑 _${usedPrefix}addprem *<@tag>*_
+┣ 👑 _${usedPrefix}delprem *<@tag>*_
+┣ 👑 _${usedPrefix}listprem_
+┣ 👑 _${usedPrefix}listcmd_
+┣ 👑 _${usedPrefix}setppbot *<respondr a image>*_
+┣ 👑 _${usedPrefix}addcmd *<text> <respond a sticker/image>*_
+┣ 📥 _${usedPrefix}instagram *< / link / url>*_
+┣ 📥 _${usedPrefix}mediafire *< LINK >
+┣ 📥 _${usedPrefix}instagram *< link / url>*_
+┣ 📥 _${usedPrefix}gitclone *< link / url>*_
+┣ 📥 _${usedPrefix}gdrive *< link / url>*_
+┣ 📥 _${usedPrefix}tiktok *< link / url>*_
+┣ 📥 _${usedPrefix}xnxxdl *< link / url>*_
+┣ 📥 _${usedPrefix}xvideosdl *<link / url>*_
+┣ 📥 _${usedPrefix}twitter *<  link / url>*_
+┣ 📥 _${usedPrefix}fb *< link / url>*_
+┣ 📥 _${usedPrefix}ytmp3 *< link / url>*_
+┣ 📥 _${usedPrefix}ytmp4 *< link / url>*_
+┣ 📥 _${usedPrefix}ytmp3doc *< link / url>*_
+┣ 📥 _${usedPrefix}ytmp4doc *< link / url>*_
+┣ 📥 _${usedPrefix}play *<text>*_
+┣ 📥 _${usedPrefix}spotify *<text>*_
+┣ 🔞 _${usedPrefix}videoxxx_
+┣ 🔞 _${usedPrefix}videolesbixxx_
+┣ 🔞 _${usedPrefix}tit_
+┣ 🔞 _${usedPrefix}booty_
+┣ 🔞 _${usedPrefix}ecchi_
+┣ 🔞 _${usedPrefix}furro_
+┣ 🔞 _${usedPrefix}lesbians_
+┣ 🔞 _${usedPrefix}panties_
+┣ 🔞 _${usedPrefix}penis_
+┣ 🔞 _${usedPrefix}porn_
+┣ 🔞 _${usedPrefix}randomxxx_
+┣ 🔞 _${usedPrefix}pechos_
+┣ 🔞 _${usedPrefix}yaoi_
+┣ 🔞 _${usedPrefix}yaoi2_
+┣ 🔞 _${usedPrefix}yuri_
+┣ 🔞 _${usedPrefix}yuri2_
+┣ 🔞 _${usedPrefix}trap_
+┣ 🔞 _${usedPrefix}hentai_
+┣ 🔞 _${usedPrefix}hloli_
+┣ 🔞 _${usedPrefix}orgy_
+┣ 🔞 _${usedPrefix}foot_
+┣ 🔞 _${usedPrefix}hass_
+┣ 🔞 _${usedPrefix}bdsm_
+┣ 🔞 _${usedPrefix}cum_
+┣ 🔞 _${usedPrefix}ero_
+┣ 🔞 _${usedPrefix}femdom_
+┣ 🔞 _${usedPrefix}glass_
+┣ 🔞 _${usedPrefix}hentai*_
+┣ 🔞 _${usedPrefix}underwear_
+┣ 🔞 _${usedPrefix}spussy_
+┣ 🔞 _${usedPrefix}bunnygirl_
+┣ 🔞 _${usedPrefix}bunnyear_
+┣ 🔞 _${usedPrefix}sswimsuit_
+┣ 🔞 _${usedPrefix}chain_
+┣ 🔞 _${usedPrefix}genshin_
+┣ 🔞 _${usedPrefix}white_
+┣ 🔞 _${usedPrefix}barefoot_
+┣ 🔞 _${usedPrefix}whitehair_
+┣ 🔞 _${usedPrefix}touhou_
+┣ 🔞 _${usedPrefix}holo_
+┣ 🔞 _${usedPrefix}gamecg_
+┣ 🔞 _${usedPrefix}uncensored_
+┣ 🔞 _${usedPrefix}sunglass_
+┣ 🔞 _${usedPrefix}glass_
+┣ 🔞 _${usedPrefix}demon_
+┣ 🔞 _${usedPrefix}bondage_
+┣ 🔞 _${usedPrefix}torn cloth_
+┣ 🔞 _${usedPrefix}fingering_
+┣ 🔞 _${usedPrefix}gun_
+┣ 🔞 _${usedPrefix}vampire_
+┣ 🔞 _${usedPrefix}idol_
+┣ 🔞 _${usedPrefix}beach_
+┣ 🔞 _${usedPrefix}bra_
+┣ 🔞 _${usedPrefix}topless_
+┣ 🔞 _${usedPrefix}stokings_
+┣ 🔞 _${usedPrefix}shorts_
+┣ 🔞 _${usedPrefix}anus_
+┣ 🔞 _${usedPrefix}tie_
+┣ 🔞 _${usedPrefix}headphone_
+┣ 🔞 _${usedPrefix}pantypull_
+┣ 🔞 _${usedPrefix}wet_
+┣ 🔞 _${usedPrefix}breast_
+┣ 🔞 _${usedPrefix}twintail_
+┣ 🔞 _${usedPrefix}sex_
+┣ 🔞 _${usedPrefix}sex2_
+┣ 🔞 _${usedPrefix}sex3_
+┣ 🔞 _${usedPrefix}skirt_
+┣ 🔞 _${usedPrefix}uniform_
+┣ 🔞 _${usedPrefix}foxgirl_
+┣ 🔞 _${usedPrefix}ponytail_
+┣ 🔞 _${usedPrefix}nude_
+┣ 🔞 _${usedPrefix}bed_
+┣ 🔞 _${usedPrefix}pinkhair_
+┣ 🔞 _${usedPrefix}bikini_
+┣ 🔞 _${usedPrefix}nobra_
+┣ 🔞 _${usedPrefix}maid_
+┣ 🧧 _${usedPrefix}toimg *<sticker>*_
+┣ 🧧 _${usedPrefix}tomp3 *<video>*_
+┣ 🧧 _${usedPrefix}tovideo *<sticker>*_
+┣ 🧧 _${usedPrefix}tourl *<video / image / audio>*_
+┣ 🧧 _${usedPrefix}tts en *<text>*_
+┣ 👾 _${usedPrefix}cristianoronaldo_
+┣ 👾 _${usedPrefix}messi_
+┣ 👾 _${usedPrefix}meme_
+┣ 👾 _${usedPrefix}itzy_
+┣ 👾 _${usedPrefix}blackpink_
+┣ 👾 _${usedPrefix}lolivid_
+┣ 👾 _${usedPrefix}loli_
+┣ 👾 _${usedPrefix}navidad_
+┣ 👾 _${usedPrefix}ppcouple_
+┣ 👾 _${usedPrefix}wpmontaña_
+┣ 👾 _${usedPrefix}pubg_
+┣ 👾 _${usedPrefix}wpgaming_
+┣ 👾 _${usedPrefix}wpaesthetic_
+┣ 👾 _${usedPrefix}wpaesthetic2_
+┣ 👾 _${usedPrefix}wprandom_
+┣ 👾 _${usedPrefix}wallhp_
+┣ 👾 _${usedPrefix}wpvehiculo_
+┣ 👾 _${usedPrefix}wpmoto_
+┣ 👾 _${usedPrefix}coffee_
+┣ 👾 _${usedPrefix}pentol_
+┣ 👾 _${usedPrefix}caricatura_
+┣ 👾 _${usedPrefix}ciberespacio_
+┣ 👾 _${usedPrefix}technology_
+┣ 👾 _${usedPrefix}doraemon_
+┣ 👾 _${usedPrefix}hacker_
+┣ 👾 _${usedPrefix}planeta_
+┣ 👾 _${usedPrefix}randomprofile_
+┣ 👾 _${usedPrefix}neko_
+┣ 👾 _${usedPrefix}waifu_
+┣ 👾 _${usedPrefix}akira_
+┣ 👾 _${usedPrefix}akiyama_
+┣ 👾 _${usedPrefix}anna_
+┣ 👾 _${usedPrefix}asuna_
+┣ 👾 _${usedPrefix}ayuzawa_
+┣ 👾 _${usedPrefix}boruto_
+┣ 👾 _${usedPrefix}chiho_
+┣ 👾 _${usedPrefix}chitoge_
+┣ 👾 _${usedPrefix}deidara_
+┣ 👾 _${usedPrefix}erza_
+┣ 👾 _${usedPrefix}elaina_
+┣ 👾 _${usedPrefix}eba_
+┣ 👾 _${usedPrefix}emilia_
+┣ 👾 _${usedPrefix}hestia_
+┣ 👾 _${usedPrefix}hinata_
+┣ 👾 _${usedPrefix}inori_
+┣ 👾 _${usedPrefix}isuzu_
+┣ 👾 _${usedPrefix}itachi_
+┣ 👾 _${usedPrefix}itori_
+┣ 👾 _${usedPrefix}kaga_
+┣ 👾 _${usedPrefix}kagura_
+┣ 👾 _${usedPrefix}kaori_
+┣ 👾 _${usedPrefix}keneki_
+┣ 👾 _${usedPrefix}kotori_
+┣ 👾 _${usedPrefix}kurumi_
+┣ 👾 _${usedPrefix}madara_
+┣ 👾 _${usedPrefix}mikasa_
+┣ 👾 _${usedPrefix}miku_
+┣ 👾 _${usedPrefix}minato_
+┣ 👾 _${usedPrefix}naruto_
+┣ 👾 _${usedPrefix}nezuko_
+┣ 👾 _${usedPrefix}sagiri_
+┣ 👾 _${usedPrefix}sasuke_
+┣ 👾 _${usedPrefix}sakura_
+┣ 👾 _${usedPrefix}cosplay_
+┣ 🛠️ _${usedPrefix}chatgpt *<text>*_
+┣ 🛠️ _${usedPrefix}dall-e *<text>*_
+┣ 🛠️ _${usedPrefix}styletext *<text>*_
+┣ 👽 _${usedPrefix}sticker *<respond a image video>*_
+┣ 👽 _${usedPrefix}sticker *<link / url>*_
+┣ 👽 _${usedPrefix}s *<respond a image video>*_
+┣ 👽 _${usedPrefix}s *<link / url>*_
+┣ 👽 _${usedPrefix}sfull *<imagen o video>*_
+┣ 👽 _${usedPrefix}emojimix *<emoji 1>&<emoji 2>*_
+┣ 👽 _${usedPrefix}scircle *<image>*_
+┣ 👽 _${usedPrefix}sremovebg *<image>*_
+┣ 👽 _${usedPrefix}semoji *<tip> <emoji>*_
+┣ 👽 _${usedPrefix}attp *<text>*_
+┣ 👽 _${usedPrefix}attp2 *<text>*_
+┣ 👽 _${usedPrefix}attp3 *<text>*_
+┣ 👽 _${usedPrefix}ttp *<text>*_
+┣ 👽 _${usedPrefix}ttp2 *<text>*_
+┣ 👽 _${usedPrefix}ttp3 *<text>*_
+┣ 👽 _${usedPrefix}ttp4 *<text>*_
+┣ 👽 _${usedPrefix}ttp5 *<text>*_
+┣ 👽 _${usedPrefix}pat *<@tag>*_
+┣ 👽 _${usedPrefix}slap *<@tag>*_
+┣ 👽 _${usedPrefix}kiss *<@tag>*_
+┣ 👽 _${usedPrefix}wm *<packname> <author>*_
 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 `.trim())
 handler.help = ['allmenu']
 handler.tags = ['main']
-handler.command = ['list', 'all menu'] 
+handler.command = ['list', 'all menu','tmlist','tm menu'] 
 
 export default handler
