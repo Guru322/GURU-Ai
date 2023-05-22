b6ed3eb010831cc9cf474f59d9033a0105d46f8b
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-22 18:58:18 +0530

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -5,7 +5,7 @@ let handler = async function (m, { conn, __dirname }) {
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
   
 m.reply(`
-type 'usedPrefix'menu for command list )
+type /menu for command list )
     
 }
 
