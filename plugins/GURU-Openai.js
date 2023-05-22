217e4c2df29788b6d7a80bfb2fd18d77bdb1fdc7
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-22 11:11:47 +0530

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -9,6 +9,6 @@ m.reply(`${hasil.result}`.trim())
 } catch {
 throw `*𝙴𝚁𝚁𝙾𝚁*`
 }}
-handler.command = ['bro', 'chatgpt', 'ai', 'siri']
+handler.command = ['bro', 'chatgpt', 'ai', 'siri' 'vivek']
 handler.diamond = false
 export default handler
