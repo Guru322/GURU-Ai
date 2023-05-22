7087460650e4e41fe25f36ca1214c404e4ca8e46
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-22 17:19:41 +0530

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Time Active ${muptime}\n\n ┃ 💎  By GURU`
+		let bio = `\n🟢 Time Active ${muptime}\n\n ┃ 💎  By vivek`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
