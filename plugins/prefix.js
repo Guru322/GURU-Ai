706da2daa54e314d43ee1aaf2924f0fc25607646
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-23 18:02:08 +0530

    Update prefix.js

--- plugins/prefix.js
+++ plugins/prefix.js
@@ -1,6 +1,6 @@
 let handler = async(m, { conn, text }) => {
   if (!text) throw `No symbol detected ...`
-  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '/').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
   await m.reply(`the prefix has been changed to  *${text}*`)
 }
 handler.help = ['setprefix'].map(v => v + ' [prefix]')
