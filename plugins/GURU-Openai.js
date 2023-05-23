367a788753c16236e3410028401dca4dd4558d81
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-23 17:04:58 +0530

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -2,12 +2,12 @@ import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
 if (!text) throw `*enter a request or an order to use the chatgpt*\n\n*—◉ 𝙴xample*\n*◉ ${usedPrefix + command}series 2022  netflix*\n*◉ ${usedPrefix + command} write a js code*`
 try {
-m.reply(`*wait sometime*`)
+m.reply(`*🤖connecting server*`)
 let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
 let hasil = await tiores.json()
 m.reply(`${hasil.result}`.trim())
 } catch {
-throw `*𝙴𝚁𝚁𝙾𝚁*`
+throw `*😭😭😭Server down*`
 }}
 handler.command = ['tmgpt']
 handler.diamond = false
