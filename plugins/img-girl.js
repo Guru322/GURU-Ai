df22d96a985aa6cf27698bf552714ecab514b8af
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-23 21:20:42 +0530

    Update img-girl.js

--- plugins/img-girl.js
+++ plugins/img-girl.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 	//let girl = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/girl.json`)).data
 	let girl = await conn.getFile(global.API('fgmods', '/api/girl', { }, 'apikey'))
 	let img = girl.data
-   await conn.sendButton(m.chat, '✅ Result 🤭', igfg, img, [['▷▷ NEXT', `${usedPrefix + command}`]],m)
+   await (m.chat, '✅ Result 🤭', igfg, img, m)
 }
 handler.help = ['girl']
 handler.tags = ['img']
