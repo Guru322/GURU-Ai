ec82b2107b132181bfdc841671203bd4df118843
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-23 17:01:15 +0530

    Update GURU-Davinci.js

--- plugins/GURU-Davinci.js
+++ plugins/GURU-Davinci.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `*This command generates image from texts*\n\n*—◉ 𝙴xample usage*\n*◉ ${usedPrefix + command} troublemaker dancing*\n*◉ ${usedPrefix + command} vivek sleeping*`
 try {
-m.reply('*Processing image *')
+m.reply('*Processing image *😀')
 let tiores = await conn.getFile(`https://api.lolhuman.xyz/api/dall-e?apikey=${lolkeysapi}&text=${text}`)
 await conn.sendFile(m.chat, tiores.data, null, null, m)
 } catch {
-throw `*INTERNAL ERROR*`
+throw `*🚮ERROR*`
 }}
 handler.command = ['tmai' ]
 export default handler
