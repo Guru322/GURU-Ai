f8fbac6e543a3d61b10d9fa3726348a11be49c4a
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-27 17:27:54 +0530

    Update rmbgvivek.js

--- plugins/rmbgvivek.js
+++ plugins/rmbgvivek.js
@@ -6,7 +6,7 @@ let mime = (q.msg || q).mimetype || ''
 let img = await q.download()
 let url = await uploadImage(img)
 let sremovebg = global.API(`https://api.lolhuman.xyz/api/removebg?apikey=${lolkeysapi}&img=${url}`) 
-await conn.sendFile(m.chat, sremovebg.data, null, null, m)
+await conn.sendFile(m.chat, sremovebg)
 } catch (e) {
 m.reply('*SORRY AN ERROR OCCURRED*')
 }}
