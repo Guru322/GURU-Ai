3dc41f4c981beecea883d4599cc7da8544d0bbbf
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-05-27 15:03:48 +0530

    Update sticker-ttp_attp.js

--- plugins/sticker-ttp_attp.js
+++ plugins/sticker-ttp_attp.js
@@ -16,9 +16,11 @@ conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp?apikey=${lolkeysapi}&te
 if (command == 'attp3') {
 conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp2?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
 
-if (command == 'ttp5') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp6?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-    
+    if (command == 'ttp5') {
+let a1 = await (await fetch(`https://api.lolhuman.xyz/api/ttp6?apikey=${lolkeysapi}&text=${teks`)).buffer()
+let a2 = await createSticker(a1, false, global.packname, global.author)
+conn.sendFile(m.chat, a2, 'sticker.webp', '', m, { asSticker: true })}
+
 if (command == 'ttp4') {
 conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp5?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
     
