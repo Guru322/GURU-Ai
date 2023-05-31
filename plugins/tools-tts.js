975b5e47c377b6494dc4c2b06cf71a1eb6b179fc
GURU <106463398+Guru322@users.noreply.github.com>
2023-05-31 03:26:27 +0000

    Fix

--- plugins/tools-tts.js
+++ plugins/tools-tts.js
@@ -2,7 +2,7 @@ import gtts from 'node-gtts'
 import { readFileSync, unlinkSync } from 'fs'
 import { join } from 'path'
 
-const defaultLang = 'en-en'
+const defaultLang = 'en'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
   let lang = args[0]
