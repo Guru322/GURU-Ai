d841e5a1736fb465e6babca05eac5e60759e1694
GURU <106463398+Guru322@users.noreply.github.com>
2023-05-31 20:57:29 +0530

    Update GURU-nowa.js

--- plugins/GURU-nowa.js
+++ plugins/GURU-nowa.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     } else {
     array.push({ exists: false, jid: result })
     }}
-    let txt = '*Not registered*\n\n' + array.filter(v => !v.exists).map(v => v.jid.split('@')[0]).join('\n')
+    let txt = 'Registered\n\n' + array.filter(v => v.exists).map(v => `• Link: wa.me/${v.jid.split('@')[0]}\n*• Bio:* ${v.status || 'descripiion'}\n*• set on:* ${formatDate(v.setAt)}`).join('\n\n') + '\n\n*Not registered*\n\n' + array.filter(v => !v.exists).map(v => v.jid.split('@')[0]).join('\n')
     m.reply(txt)
     }
     handler.command = /^nowa$/i
