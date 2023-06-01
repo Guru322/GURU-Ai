0472b7441f49207815c66492eef2113e846ff386
GURU <106463398+Guru322@users.noreply.github.com>
2023-06-01 20:29:41 +0530

    sorry my bad

--- plugins/cmd-del.js
+++ plugins/cmd-del.js
@@ -5,7 +5,7 @@ let handler = async (m, { text }) => {
     if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
     if (!hash) throw `✳️ Enter the command name`
     let sticker = global.db.data.sticker
-    if (sticker[hash] && sticker[hash].locked) throw '✳️ You can't delete this command'
+    if (sticker[hash] && sticker[hash].locked) throw `✳️ You can not delete this command`
     delete sticker[hash]
     m.reply(`✅ Command eliminated`)
 }
