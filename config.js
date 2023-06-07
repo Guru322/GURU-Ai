a9dde8eb9830a8e767e6295bcf2bb691ef6978c9
Trippie33 <133229315+Trippie33@users.noreply.github.com>
2023-06-07 02:42:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['2349037546246', 'GURU', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
