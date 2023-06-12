48a990101321fe9e39143ff0560ab8073cfbac4f
GURU <sahoogurucharan00@gmail.com>
2023-06-12 08:49:46 +0530

    Update fun-gay.js

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let name = conn.getName(who)
-  conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/overlay/gay', {
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/gay', {
     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png'), 
   }), 'gay.png', `🏳️‍🌈  *Gay :* ${name}\n\nWho wants to rape this gay☠️? `, m)
 }
