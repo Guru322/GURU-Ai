5d598597aa209dabf4aad9cf7fc25edcb1794dcb
GURU <sahoogurucharan00@gmail.com>
2023-06-12 08:52:25 +0530

    Update maker-blur.js

--- plugins/maker-blur.js
+++ plugins/maker-blur.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedprefix }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/blur', {
+    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/blur', {
     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
     }), 'hornycard.png', '*[ ✔ ]*', m)
     }
