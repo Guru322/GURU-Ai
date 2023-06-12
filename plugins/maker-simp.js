bb8bc2b9f567a64f27f067b2d357be669b9c9671
GURU <sahoogurucharan00@gmail.com>
2023-06-12 08:54:59 +0530

    Update maker-simp.js

--- plugins/maker-simp.js
+++ plugins/maker-simp.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/simpcard', {
+    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/simpcard', {
     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
     }), 'error.png', '*your religion is simping*', m)
     }
