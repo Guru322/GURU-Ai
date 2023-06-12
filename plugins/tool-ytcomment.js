af3a0f1e6b07175ac772d7815057c78a6af80166
GURU <sahoogurucharan00@gmail.com>
2023-06-12 09:01:21 +0530

    Update tool-ytcomment.js

--- plugins/tool-ytcomment.js
+++ plugins/tool-ytcomment.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text }) => {
     if (!text) throw 'No Text'
-    conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/youtube-comment', {
+    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/youtube-comment', {
     avatar: await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
     comment: text,
     username: conn.getName(m.sender)
