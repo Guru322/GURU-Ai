b42545d5a9b5cd3f8762a6dd2bdafabeae7b5920
GURU <sahoogurucharan00@gmail.com>
2023-06-13 13:39:03 +0530

    Update gc-setpp.js

--- plugins/gc-setpp.js
+++ plugins/gc-setpp.js
@@ -1,42 +1,14 @@
-const fs = require('fs')
-const Jimp = require('jimp') 
-let handler = async (m, { conn }) => {
-var image = m.quoted ? m.quoted : m
-var mime = (q.msg || q).mimetype || q.mediaType || ''
-var media = await image.download()
-            const group = m.chat
-            var { img } = await generateProfilePicture(media)
-            await conn.query({
-            tag: 'iq',
-            attrs: {
-            to: group, 
-            type:'set',
-            xmlns: 'w:profile:picture'
-            },
-            content: [
-            {
-            tag: 'picture',
-            attrs: { type: 'image' },
-            content: img
-            }
-            ]
-            })
-            m.reply('Update Profile Group ✅')
-}
-handler.help = ['setppgc']
-handler.tags = ['group']
-handler.command = /^(setppgc|setppgrup|setppgroup)$/i
 
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-module.exports = handler
-
-async function generateProfilePicture(buffer) {
- const jimp_1 = await Jimp.read(buffer);
- const minz = jimp_1.getWidth() > jimp_1.getHeight() ? jimp_1.resize(720, Jimp.AUTO) : jimp_1.resize(Jimp.AUTO, 720)
- const jimp_2 = await Jimp.read(await minz.getBufferAsync(Jimp.MIME_JPEG));
- return {
-   img: await minz.getBufferAsync(Jimp.MIME_JPEG)
- }
-}
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+    if (/image/.test(mime)) {
+    let img = await q.download()
+    if (!img) throw '*REPLY TO AN IMAGE.*'
+    await conn.updateProfilePicture(m.chat, img).then(_ => m.reply('IMAGE SUCCESSFULLY SET AS PP._*'))
+    } else throw '*REPLY TO AN IMAGE.*'}
+    handler.command = /^setpp(group|grup|gc)?$/i
+    handler.group = true
+    handler.admin = true
+    handler.botAdmin = true
+    export default handler
