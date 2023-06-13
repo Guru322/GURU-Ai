11b3542c0a4b5a4a92a42b48a64d2654194ee013
GURU <sahoogurucharan00@gmail.com>
2023-06-13 17:04:21 +0530

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -232,6 +232,6 @@ m.reply(`
 }
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['nable']
-handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff)|[01])$/i
+handler.command = /^((en|dis)able|(turn)?o(n|ff)|[01])$/i
 
 export default handler
