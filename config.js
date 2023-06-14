b71b23626acda16282ac349d70a6bec9b85ee6f0
Mutazalahwal <86023503+Mutazalahwal@users.noreply.github.com>
2023-06-14 09:59:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 global.owner = [
   ['917605902011', 'GURU', true],
   ['6281255369012', 'Ash', true], 
-  [''] 
+  ['962791762549', 'SZYP', true], 
 ] //Number of owners
 
 global.mods = ['917605902011','32460248586'] 
@@ -47,15 +47,15 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'THE SZYP-ʙᴏᴛ'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
+global.packname = '3MK┃SzYP' 
+global.author = '@sz_yp' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/sz_yp\n' 
+global.dygp = '.'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.fgyt = '.'
+global.fgpyp = '.'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 
 
