5c6ca572ae73744c21c79972ac084335e7b864cb
GURU <sahoogurucharan00@gmail.com>
2023-06-14 07:44:27 +0530

    Update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -1,16 +1,30 @@
+import fetch from 'node-fetch';
 
-import fetch from 'node-fetch'
+const BRAINSHOP_BID = '176001';
+const BRAINSHOP_KEY = 'M4fzqfe99b3THOYi';
 
 export async function before(m, { conn }) {
-if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let user = global.db.data.users[m.sender]
-    
-      if (!user.chatbot)
-        return !0
-        let api = await fetch(`https://api.simsimi.net/v2/?text=${m.text}&lc=en`)
-        let res = await api.json()
-        m.reply(res.success.replace('simsimi', 'GURU').replace('Simsimi', 'GURU').replace('sim simi', 'GURU'))
-    
+  if (m.isBaileys && m.fromMe) {
+    return true;
+  }
+  
+  if (!m.isGroup) {
+    return false;
+  }
+  
+  const user = global.db.data.users[m.sender];
+  
+  if (!user.chatbot) {
+    return true;
+  }
+  
+  const uid = encodeURIComponent(m.sender);
+  const msg = encodeURIComponent(m.text);
+  
+  const response = await fetch(`http://api.brainshop.ai/get?bid=${BRAINSHOP_BID}&key=${BRAINSHOP_KEY}&uid=${uid}&msg=${msg}`);
+  const data = await response.json();
+  
+  const reply = data.cnt;
+  
+  m.reply(reply);
 }
