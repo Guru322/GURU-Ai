602955a3bb860679641d0b58844993296e0fe055
Prince <112704679+Princegds786@users.noreply.github.com>
2023-06-15 12:30:04 +0500

    Update main-list.js

--- plugins/main-list.js
+++ plugins/main-list.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix, command}) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let pp = './Guru.jpg'
+let pp = './Princejpg.jpg'
 let more = String.fromCharCode(8206)
 let readMore = more.repeat(850) 
 let lkr = `❀° ┄──•••───╮
