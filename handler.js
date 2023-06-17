7e3b4bd016360420f0ca42213b19e95c4453441c
GURU <107632293+guru65476@users.noreply.github.com>
2023-06-17 22:27:22 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -76,7 +76,7 @@ export async function handler(chatUpdate) {
                 if (!('role' in user))
                     user.role = 'Beginner'
                 if (!('autolevelup' in user))
-                    user.autolevelup = true
+                    user.autolevelup = false
                 if (!isNumber(user.money))
                     user.money = 0
                 if (!isNumber(user.atm))
@@ -215,7 +215,7 @@ export async function handler(chatUpdate) {
                     warn: 0,
                     level: 0,                    
                     role: 'Beginner',
-                    autolevelup: true,
+                    autolevelup: false,
                     money: 0,
                     bank: 0,
                     atm: 0,
