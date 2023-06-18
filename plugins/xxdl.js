d94f8d77fb351e5ab530a399a13d62f407c6bb3a
GURU <107632293+guru65476@users.noreply.github.com>
2023-06-18 07:30:58 +0530

    Update xxdl.js

--- plugins/xxdl.js
+++ plugins/xxdl.js
@@ -1,38 +1,39 @@
 import fetch from 'node-fetch';
 import fg from 'api-dylux';
 
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+const allowedHosts = ['xnxx.com'];
 
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   let chat = global.db.data.chats[m.chat];
-  if (!chat.nsfw) throw `🚫 this group does not support nsfw \n\nto turn on  \n*${usedPrefix}enable* nsfw`;
+  if (!chat.nsfw) throw `🚫 This group does not support NSFW content.\n\nTo turn it on, use: *${usedPrefix}enable* nsfw`;
   let user = global.db.data.users[m.sender].age;
-  if (user < 17) throw `❎ age must be 18 to use this feature`;
-  if (!text) throw `✳️ what to search?\n📌 Use : *${usedPrefix + command} <search>*\n\nExample:- Hot desi bhabi or you can use link also\nExample .xnxx link *`;
+  if (user < 18) throw `❎ You must be 18 years or older to use this feature.`;
+  if (!text) throw `✳️ What do you want to search?\n📌 Usage: *${usedPrefix + command} <search>*\n\nExample: Hot desi bhabi or you can use a link as well\nExample: .xnxx link *`;
     
-  m.react(rwait);
+  m.react('⌛');
 
   // Validate if the input is a URL
   let url;
   try {
     url = new URL(text);
-  } catch (_) {
-    url = null;  
+    if (!allowedHosts.includes(url.hostname)) throw new Error('Invalid URL');
+  } catch (error) {
+    url = null;
   }
 
-  if (url && (url.protocol === "http:" || url.protocol === "https:")) {
-    if (!url.hostname.includes('xnxx.com')) return m.reply(`❎ Invalid link  *xnxx.com*`);
+  if (url) {
     try {
-      let xn = await (await fetch(global.API('fgmods', '/api/dowloader/xnxxdl', { url: text }, 'apikey'))).json()
+      let xn = await (await fetch(global.API('fgmods', '/api/dowloader/xnxxdl', { url: text }, 'apikey'))).json();
       conn.sendFile(m.chat, xn.result.files.high, xn.result.title + '.mp4', `
-  ≡  *XNXX DL*
-            
-  ▢ *📌Title*: ${xn.result.title}
-  ▢ *⌚Duration:* ${xn.result.duration}
-  ▢ *🎞️Quality:* ${xn.result.quality}
-  `.trim(), m, false, { asDocument: chat.useDocument });
-      m.react(done);
+        ≡  *XNXX DL*
+        
+        ▢ *📌Title*: ${xn.result.title}
+        ▢ *⌚Duration*: ${xn.result.duration}
+        ▢ *🎞️Quality*: ${xn.result.quality}
+      `.trim(), m, false, { asDocument: chat.useDocument });
+      m.react('✅');
     } catch (e) {
-      m.reply(`🔴 Error : we are trying hard to fix`);
+      m.reply(`🔴 Error: We encountered a problem while processing the request.`);
     }
   } else {
     try {
@@ -41,13 +42,13 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
       let listSections = [];
       Object.values(json.result).map((v, index) => {
         listSections.push([`${index}┃ ${v.title}`, [
-              ['🎥 MP4', `${usedPrefix}xnxxdl ${v.link}`, `▢ 📌 *Título* : ${v.title}`]
-            ]]);
+          ['🎥 MP4', `${usedPrefix}xnxxdl ${v.link}`, `▢ 📌 *Title*: ${v.title}`]
+        ]]);
       });
-      let ff = json.result.map((v, i) => `${i + 1}┃ *Title* : ${v.title}\n*Link:* ${v.link}\n`).join('\n'); 
+      let ff = json.result.map((v, i) => `${i + 1}┃ *Title*: ${v.title}\n*Link*: ${v.link}\n`).join('\n');
       if (json.status) m.reply(ff);
     } catch (e) {
-      m.reply(`🔴 Error: we are trying hard to fix it`);
+      m.reply(`🔴 Error: We encountered a problem while processing the request.`);
     }
   }
 };
