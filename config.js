23a4420104f9f9733369c74b7b59d0ea61207cfe
GURU <sahoogurucharan00@gmail.com>
2023-06-22 19:57:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c7
 global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
 global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
 global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
-global.lolkeysapi = ['BrunoSobrino']
+global.lolkeysapi = ['SGWN']
 
 global.APIs = { // API Prefix
   // name: 'https://website'
