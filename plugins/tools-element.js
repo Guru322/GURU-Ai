f7e0cb802388f8d54c4ece25a082b4c1ad5014b2
GURU <sahoogurucharan00@gmail.com>
2023-06-25 23:02:07 +0530

    Fix with some new commands added (#51)
    authored-by: GURU

---
+++ plugins/tools-element.js
@@ -0,0 +1,40 @@
+import fetch from 'node-fetch';
+
+let elementHandler = async (m, { conn, text }) => {
+  if (!text) throw 'Please provide an element symbol or name';
+
+  try {
+    let res = await fetch(`https://api.popcat.xyz/periodic-table?element=${text}`);
+
+    if (!res.ok) {
+      throw new Error(`API request failed with status ${res.status}`);
+    }
+
+    let buffer = await res.arrayBuffer();
+    let json = JSON.parse(Buffer.from(buffer).toString());
+
+    console.log('JSON response:', json);
+
+    let elementInfo = 
+    `*Element Information:*\n
+     • *Name:* ${json.name}\n
+     • *Symbol:* ${json.symbol}\n
+     • *Atomic Number:* ${json.atomic_number}\n
+     • *Atomic Mass:* ${json.atomic_mass}\n
+     • *Period:* ${json.period}\n
+     • *Phase:* ${json.phase}\n
+     • *Discovered By:* ${json.discovered_by}\n
+     • *Summary:* ${json.summary}`;
+
+    conn.sendFile(m.chat, json.image, 'element.jpg', elementInfo, m);
+  } catch (error) {
+    console.error(error);
+    // Handle the error appropriately
+  }
+};
+
+elementHandler.help = ['element'];
+elementHandler.tags = ['tools'];
+elementHandler.command = /^(element|ele)$/i;
+
+export default elementHandler;
---
+++ plugins/tools-movie.js
@@ -0,0 +1,57 @@
+import fetch from 'node-fetch';
+
+let imdbHandler = async (m, { conn, text }) => {
+  if (!text) throw 'Please provide a movie title';
+
+  try {
+    let res = await fetch(`https://api.popcat.xyz/imdb?q=${encodeURIComponent(text)}`);
+
+    if (!res.ok) {
+      throw new Error(`API request failed with status ${res.status}`);
+    }
+
+    let json = await res.json();
+
+    console.log('JSON response:', json);
+
+    let ratings = json.ratings.map(rating => `• *${rating.source}:* ${rating.value}`).join('\n');
+
+    let movieInfo = 
+    `*Movie Information:*\n
+     • *Title:* ${json.title}\n
+     • *Year:* ${json.year}\n
+     • *Rated:* ${json.rated}\n
+     • *Released:* ${json.released}\n
+     • *Runtime:* ${json.runtime}\n
+     • *Genres:* ${json.genres}\n
+     • *Director:* ${json.director}\n
+     • *Writer:* ${json.writer}\n
+     • *Actors:* ${json.actors}\n
+     • *Plot:* ${json.plot}\n
+     • *Languages:* ${json.languages}\n
+     • *Country:* ${json.country}\n
+     • *Awards:* ${json.awards}\n
+     • *Metascore:* ${json.metascore}\n
+     • *Rating:* ${json.rating}\n
+     • *Votes:* ${json.votes}\n
+     • *IMDB ID:* ${json.imdbid}\n
+     • *Type:* ${json.type}\n
+     • *DVD:* ${json.dvd}\n
+     • *Box Office:* ${json.boxoffice}\n
+     • *Production:* ${json.production}\n
+     • *Website:* ${json.website}\n\n
+     *Ratings:*\n${ratings}`;
+
+    // send the movie poster along with the movie information as caption
+    await conn.sendFile(m.chat, json.poster, 'poster.jpg', movieInfo, m);
+  } catch (error) {
+    console.error(error);
+    // Handle the error appropriately
+  }
+};
+
+imdbHandler.help = ['imdb'];
+imdbHandler.tags = ['information'];
+imdbHandler.command = /^(imdb|movie)$/i;
+
+export default imdbHandler;
