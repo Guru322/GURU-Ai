6db84ed0b6b8156897994d8fa9da4cd374fe4244
GURU <sahoogurucharan00@gmail.com>
2023-06-25 05:56:52 +0000

    FIX

--- plugins/tools-wa.js
+++ plugins/tools-wa.js
@@ -1,22 +1,12 @@
-import PhoneNumber from 'awesome-phonenumber';
+let handler = async (m, { conn, usedPrefix, text, command }) => {
+  if (!text) throw `Please provide a phone number\n\n📌 Example: *${usedPrefix + command}* 1234567890`;
 
-let handler = async (m, { conn, usedPrefix, command}) => {
+  const waLink = `https://wa.me/${text}`;
+  const message = `*WhatsApp Link:*\n${waLink}`;
 
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-    let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
-    let user = global.db.data.users[who]
-    let username = conn.getName(who)
-    let { name,registered } = global.db.data.users[who]
+  conn.sendMessage(m.chat, { text: message, quoted: m, contextInfo: { mentionedJid: [m.sender] } });
 
-  let message = `
-  
-   *Name:*  ${username} ${registered ? '\n   • ' + name + ' ': ''}
-   *Number:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-   *Link:* wa.me/+${who.split`@`[0]}`;
-
-  conn.sendFile(m.chat, pp, 'pp.jpg', message, m, false, { mentions: [who] })
-    m.react(done)
+  m.react('✅');
 };
 
 handler.help = ['wa'];
@@ -24,3 +14,5 @@ handler.tags = ['tools'];
 handler.command = ['wa'];
 
 export default handler;
+
+
