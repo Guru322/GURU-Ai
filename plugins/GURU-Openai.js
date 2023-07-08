16761b18c63f06960f7041e95689d89c8760ce86
GURU <sahoogurucharan00@gmail.com>
2023-07-08 12:25:10 +0530

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -1,13 +1,18 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
+import { ChatGpt } from '../lib/scraper.js';
 let handler = async (m, { text, usedPrefix, command }) => {
-if (!text) throw `*enter a request or an order to use the chatgpt*\n\n*—◉ 𝙴xample*\n*◉ ${usedPrefix + command}series 2022  netflix*\n*◉ ${usedPrefix + command} write a js code*`
-try {
-let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
-let hasil = await tiores.json()
-m.reply(`${hasil.result}`.trim())
-} catch {
-throw `*𝙴𝚁𝚁𝙾𝚁*`
-}}
-handler.command = ['bro', 'chatgpt', 'ai', 'siri']
-handler.diamond = false
-export default handler
+    if (!text) throw `*Enter a request or an order to use ChatGpt*\n\n*Example*\n* ${usedPrefix + command} series 2022 Netflix*\n* ${usedPrefix + command} write a JS code*`;
+    try {
+      const systemMessage = 'Analyzing your request...';
+      const response = await ChatGpt(text, systemMessage);
+      m.reply(`${response.result}`.trim());
+    } catch (error) {
+      console.error(error);
+      throw `*ERROR*`;
+    }
+  };
+
+handler.command = ['bro', 'chatgpt', 'ai', 'siri'];
+handler.diamond = false;
+
+export default handler;
