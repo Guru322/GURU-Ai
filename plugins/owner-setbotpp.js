0cf38403132bb4ffe5384ab5e6b59a6c65d0de7e
GURU <sahoogurucharan00@gmail.com>
2023-07-08 12:40:09 +0530

    Update owner-setbotpp.js

--- plugins/owner-setbotpp.js
+++ plugins/owner-setbotpp.js
@@ -1,13 +1,59 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
-    let bot = conn.user.jid
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (/image/.test(mime)) {
-    let img = await q.download()
-    if (!img) throw `*IMAGE NOT FOUND, PLEASE REPLY TO AN IMAGE USING THE COMMAND ${usedPrefix + command}*`
-    await conn.updateProfilePicture(bot, img)
-    conn.reply(m.chat, '*SUCCESSFULLY CHANGED THE PROFILE PICTURE OF THE BOT NUMBER*', m)
-    } else throw `*IMAGE NOT FOUND, PLEASE REPLY TO AN IMAGE USING THE COMMAND ${usedPrefix + command}*`}
-    handler.command = /^setppbot$/i
-    handler.owner = true
-    export default handler
+import fs from 'fs'
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const jimp_1 = require('jimp')
+
+let handler = async (m, { conn, command, usedPrefix }) => {
+ let q = m.quoted ? m.quoted : m
+ let mime = (q.msg ? q.msg : q).mimetype ? q.mimetype : q.mediaType || ''
+ if (/image/g.test(mime) && !/webp/g.test(mime)) {
+  try {
+   let media = await q.download()
+   let botNumber = await conn.user.jid
+   let { img } = await pepe(media)
+   await conn.query({
+    tag: 'iq',
+    attrs: {
+     to: botNumber,
+     type:'set',
+     xmlns: 'w:profile:picture'
+    },
+    content: [
+     {
+      tag: 'picture',
+      attrs: { type: 'image' },
+      content: img
+     }
+    ]
+   })
+   m.reply('Successfully changed Bot PP')
+  } catch (e) {
+   console.log(e)
+   m.reply('An error occurred, try again later.')
+  }
+ } else {
+  m.reply(`Send image with caption ${usedPrefix + command} or tag image that has been sent`)
+ }
+}
+
+handler.help = ['setppbotfull']
+handler.tags = ['owner']
+handler.command = /^(setbotpp)$/i
+
+handler.owner = true
+
+export default handler
+
+async function pepe(media) {
+    const jimp = require('jimp');
+    const image = await jimp.read(media)
+    const min = image.getWidth()
+    const max = image.getHeight()
+    const cropped = image.crop(0, 0, min, max)
+    return {
+     img: await cropped.scaleToFit(720, 720).getBufferAsync(jimp.MIME_JPEG),
+     preview: await cropped.normalize().getBufferAsync(jimp.MIME_JPEG)
+    }
+   }
+   
