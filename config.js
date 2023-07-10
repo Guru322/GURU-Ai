69bf77a26b9e184602b67a185a94337e2c7fdd56
Anibot01 <138889645+Anibot01@users.noreply.github.com>
2023-07-10 23:19:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ import axios from 'axios'
 
 global.owner = [
   ['917000263087', 'ANISH', true],
-  ['6281255369012', 'Ash', true], 
+  ['916267716334', 'Akeno-chan', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['917000263087','32460248586'] 
-global.prems = ['917000263087', '32460248586', '919398758484']
+global.mods = ['917000263087','916267716334'] 
+global.prems = ['917000263087', '916267716334', '919398758484']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
