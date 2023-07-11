9115f3df350158b44fb8d25745cd64cd26b2efcd
GURU <sahoogurucharan00@gmail.com>
2023-07-11 20:43:09 +0530

    Fix Ai

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -1,16 +1,21 @@
 import fetch from 'node-fetch';
-import { ChatGpt } from '../lib/scraper.js';
+
 let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `*Enter a request or an order to use ChatGpt*\n\n*Example*\n* ${usedPrefix + command} series 2022 Netflix*\n* ${usedPrefix + command} write a JS code*`;
-    try {
-      const systemMessage = 'Analyzing your request...';
-      const response = await ChatGpt(text, systemMessage);
-      m.reply(`${response.result}`.trim());
-    } catch (error) {
-      console.error(error);
-      throw `*ERROR*`;
-    }
-  };
+  if (!text) throw `*Enter a request or an order to use ChatGpt*\n\n*Example*\n* ${usedPrefix + command} Latest Netflix series*\n* ${usedPrefix + command} write a JS code*`;
+
+  try {
+    const response = await fetch(`https://guru-scrapper.cyclic.app/api/chatgpt?query=${encodeURIComponent(text)}`);
+    const data = await response.json();
+    const { text: result } = data.data || {};
+    const model = data.data?.detail?.model;
+    const creator = data.creator || '';
+    const fullResult = `${result}\n\nModel: ${model}\nCreator: ${creator}`;
+    m.reply(fullResult.trim());
+  } catch (error) {
+    console.error('Error:', error); // Log the error
+    throw `*ERROR*`;
+  }
+};
 
 handler.command = ['bro', 'chatgpt', 'ai', 'siri'];
 handler.diamond = false;
