ac1e763ae285470f5f507465d8c23596b1d8d756
GURU <sahoogurucharan00@gmail.com>
2023-07-23 11:52:39 +0530

    Fixed a bug[it now takes only one single symbol as prefix]

--- plugins/owner-setprefix.js
+++ plugins/owner-setprefix.js
@@ -1,9 +1,18 @@
 let handler = async(m, { conn, text }) => {
   if (!text) throw `No symbol detected ...`
-  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
-  await m.reply(`the prefix has been changed to  *${text}*`)
+
+  // Regular expression to check if the input contains exactly one symbol
+  const symbolRegex = /^[^\w\s]{1}$/
+
+  if (!symbolRegex.test(text)) {
+    throw `Invalid symbol input. Please provide exactly one symbol as a prefix.`
+  }
+
+  // If the input is valid (contains exactly one symbol), update the prefix
+  global.prefix = new RegExp('^[' + text.replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+  await m.reply(`the prefix has been changed to *${text}*`)
 }
-handler.help = ['setprefix'].map(v => v + ' [prefix]')
+handler.help = ['.setprefix ', '.setprefix [symbol]']
 handler.tags = ['owner']
 handler.command = /^(setprefix)$/i
 handler.rowner = true
