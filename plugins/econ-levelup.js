e21560dac8bb87debfdfa87f20b444418c31b3d2
GURU <sahoogurucharan00@gmail.com>
2023-07-25 18:04:04 +0530

    Fix

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -1,11 +1,13 @@
+import { canLevelUp, xpRange } from '../lib/levelling.js';
 
-import { canLevelUp, xpRange } from '../lib/levelling.js'
 let handler = async (m, { conn }) => {
-	  let name = conn.getName(m.sender)
-    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
-    let user = global.db.data.users[m.sender]
+    let name = conn.getName(m.sender);
+    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg');
+    let user = global.db.data.users[m.sender];
+    let background = 'https://i.ibb.co/4YBNyvP/images-76.jpg'; // Fixed background URL
+
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
-        let { min, xp, max } = xpRange(user.level, global.multiplier)
+        let { min, xp, max } = xpRange(user.level, global.multiplier);
         let txt = `
 ┌───⊷ *LEVEL*
 ▢ Number : *${name}*
@@ -14,50 +16,38 @@ let handler = async (m, { conn }) => {
 ▢ Role : *${user.role}*
 └──────────────
 
-You lack *${max - user.exp}* of *XP* to level up
-`.trim()
-try {
-  let imgg = API('fgmods', '/api/rank', {
-    username: name,
-    xp: user.exp - min,
-    exp: xp,
-    avatar: pp,
-    level: user.level,
-    background: 'https://i.ibb.co/CsNgBYw/qiyana.jpg'
-}, 'apikey')
+Hey there, ${name}! You're not ready to level up just yet. It seems like you need to munch up *${max - user.exp}* more XP to level up and reach new heights! Keep going, and the bots will be singing your praises soon! 🚀
+`.trim();
 
-    conn.sendFile(m.chat, imgg, 'level.jpg', txt, m)
-} catch (e) {
-    m.reply(txt)
-}
-    }
-    let before = user.level * 1
-    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-    if (before !== user.level) {
-    	user.role = global.rpg.role(user.level).name
-
+        try {
+            let imgg = `https://wecomeapi.onrender.com/rankup-image?username=${encodeURIComponent(name)}&currxp=${user.exp - min}&needxp=${xp}&level=${user.level}&rank=${encodeURIComponent(pp)}&avatar=${encodeURIComponent(pp)}&background=${encodeURIComponent(background)}`;
+            conn.sendFile(m.chat, imgg, 'level.jpg', txt, m);
+        } catch (e) {
+            m.reply(txt);
+        }
+    } else {
         let str = `
 ┌─⊷ *LEVEL UP*
-▢ Previous level : *${before}*
-▢ current level : *${user.level}*
+▢ Previous level : *${user.level - 1}*
+▢ Current level : *${user.level}*
 ▢ Role : *${user.role}*
 └──────────────
 
-*_The more you interact with the bots, the higher your level will be_*
-`.trim()
+Woo-hoo, ${name}! You've soared to new heights and reached level ${user.level}! 🎉 Time to celebrate! 🎊
+Your newfound power will strike fear into the hearts of trolls, and the bots will bow before your command! Keep up the incredible work, and who knows what epic adventures await you next! 🌟
+`.trim();
+
         try {
-            let img = API('fgmods', '/api/levelup', { 
-                avatar: pp 
-             }, 'apikey')
-      conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
+            let img = `https://wecomeapi.onrender.com/levelup-image?avatar=${encodeURIComponent(pp)}`;
+            conn.sendFile(m.chat, img, 'levelup.jpg', str, m);
         } catch (e) {
-            m.reply(str)
+            m.reply(str);
         }
     }
 }
 
-handler.help = ['levelup']
-handler.tags = ['econ']
-handler.command = ['nivel', 'lvl', 'levelup', 'level'] 
+handler.help = ['levelup'];
+handler.tags = ['econ'];
+handler.command = ['nivel', 'lvl', 'levelup', 'level'];
 
 export default handler
