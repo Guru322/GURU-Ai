4931aa09728868aa56faf2a5d866c28378fd92d8
GURU <sahoogurucharan00@gmail.com>
2023-07-29 18:49:37 +0530

    let's hope it works

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,39 +1,31 @@
+import { tiktokdl } from '@bochilteam/scraper';
 
-import fg from 'api-dylux' 
-import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  if (!args[0]) throw `✳️ Enter a TikTok link\n\n📌 Example: ${usedPrefix + command} https://vm.tiktok.com/ZMYG92bUh/`;
+  if (!args[0].match(/tiktok/gi)) throw `❎ Verify that the link is from TikTok`;
 
-let handler = async (m, { conn, text, args, usedPrefix, command}) => {
-if (!args[0]) throw `✳️ Enter a Tiktok link\n\n 📌 Example : ${usedPrefix + command} https://vm.tiktok.com/ZMNqyusVD/?k=1`
-if (!args[0].match(/tiktok/gi)) throw `❎ verify that the link is from tiktok`
-m.react(rwait)
+  try {
+    const { author: { nickname }, video, description } = await tiktokdl(args[0]);
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw;
+    if (!url) throw '❎ Error downloading the video';
 
-try {
-    let p = await fg.tiktok(args[0]) 
-    let te = `
+   
+
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `
 ┌─⊷ TIKTOK
-▢ *Username:* ${p.unique_id}
-▢ *Description:* ${p.title}
-▢ *Duration:* ${p.duration}
-└───────────`
-   conn.sendFile(m.chat, p.play, 'tiktok.mp4', te, m)
-    m.react(done)
-    } catch {  	
-	const { author: { nickname }, video, description } = await tiktokdl(args[0])
-         .catch(async _ => await tiktokdlv2(args[0]))
-         .catch(async _ => await tiktokdlv3(args[0]))
-    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-    if (!url) throw '❎ Error downloading the video'
-     conn.sendFile(m.chat, url, 'fb.mp4', `
-┌─⊷ *TIKTOK DL*
-▢ *Username:* ${nickname} ${description ? `\n▢ *Description:* ${description}` : ''}
-└───────────`, m)
-m.react(done)
-} 
-    
-}  
-handler.help = ['tiktok']
-handler.tags = ['dl']
-handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
-handler.diamond = true
+▢ *Nickname:* ${nickname} ${description ? `\n▢ *Description:* ${description}` : ''}
+└───────────`, m);
+  } catch (error) {
+    // Log the error
+    console.error('Error downloading TikTok video:', args[0], error);
 
-export default handler
+    m.reply(`❎ Error downloading the video`);
+  }
+};
+
+handler.help = ['tiktok'];
+handler.tags = ['dl'];
+handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i;
+
+
+export default handler;
