d798e7ebb5de7aad697eb73bb43223c639dce683
GURU <sahoogurucharan00@gmail.com>
2023-07-29 18:57:17 +0530

    Update img-wallpaper.js

--- plugins/img-wallpaper.js
+++ plugins/img-wallpaper.js
@@ -1,11 +1,25 @@
-import { wallpaper, wallpaperv2 } from '@bochilteam/scraper'
+import { wallpaper } from '@bochilteam/scraper';
+import fetch from 'node-fetch';
+
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*EXAMPLE USAGE ${usedPrefix + command} Minecraft*`
-const res = await (/2/.test(command) ? wallpaperv2 : wallpaper)(text)
-const img = res[Math.floor(Math.random() * res.length)]
-conn.sendFile(m.chat, img, 'error.jpg', `*𝚁𝙴𝚂𝚄𝙻𝚃 ${text}*`, m)
-}
-handler.help = ['', '2'].map(v => 'wallpaper' + v + ' <query>')
-handler.tags = ['downloader']
-handler.command = /^(wallpaper2?)$/i
+  if (!text) throw `*EXAMPLE USAGE ${usedPrefix + command} Minecraft*`;
+
+
+  const res = await wallpaper(text);
+  const url = res[Math.floor(Math.random() * res.length)];
+
+  // Use fetch to fetch the image using the URL
+  const response = await fetch(url);
+
+  // Use 'arrayBuffer()' instead of 'buffer()'
+  const buffer = await response.arrayBuffer();
+
+ 
+  conn.sendFile(m.chat, buffer, 'wallpaper.jpg', `*${text}*`, m);
+};
+
+handler.help = [''].map(v => 'wallpaper' + v + ' <query>');
+handler.tags = ['downloader'];
+handler.command = /^(wallpaper)$/i;
+
 export default handler
