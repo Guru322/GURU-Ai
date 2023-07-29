ee41189706c5b1470b4fbcfabc10857b46179294
GURU <sahoogurucharan00@gmail.com>
2023-07-29 18:39:11 +0530

    improved

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -4,15 +4,12 @@ let handler = async (m, { text, usedPrefix, command }) => {
   if (!text) throw `*Enter a request or an order to use ChatGpt*\n\n*Example*\n* ${usedPrefix + command} Latest Netflix series*\n* ${usedPrefix + command} write a JS code*`;
 
   try {
-    const response = await fetch(`https://guru-scrapper.cyclic.app/api/chatgpt?query=${encodeURIComponent(text)}`);
+    const response = await fetch(`https://gurugpt4-85987f3ed9b3.herokuapp.com/api/gpt4?query=${encodeURIComponent(text)}`);
     const data = await response.json();
-    const { text: result } = data.data || {};
-    const model = data.data?.detail?.model;
-    const creator = data.creator || '';
-    const fullResult = `${result}\n\nModel: ${model}\nCreator: ${creator}`;
-    m.reply(fullResult.trim());
+    const { response: result } = data; 
+    m.reply(result.trim()); 
   } catch (error) {
-    console.error('Error:', error); // Log the error
+    console.error('Error:', error); 
     throw `*ERROR*`;
   }
 };
