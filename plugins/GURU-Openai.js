5744b4296aa45f23e4d06d59f4aa1f3ecf52419a
LBlaze <69427966+IndraTensei@users.noreply.github.com>
2023-07-30 00:35:48 +0530

    Update GURU-Openai.js
    added quotedmsg

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -1,24 +1,21 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { text, usedPrefix, command }) => {
-  
-  if (!text && !(m.quoted && m.quoted.text)) {
-    throw `Please provide some text or quote a message to get a response.`;
-  }
-
- 
-  if (!text && m.quoted && m.quoted.text) {
-    text = m.quoted.text;
-  }
-
   try {
-    const response = await fetch(`https://gurugpt4-85987f3ed9b3.herokuapp.com/api/gpt4?query=${encodeURIComponent(text)}`);
+    let quotedMsg = m.quoted ? m.quoted : null;
+    let userQuery = quotedMsg ? quotedMsg.text : text;
+
+    if (!userQuery) {
+      throw new Error(`Please provide some text or quote a message to get a response.`);
+    }
+
+    const response = await fetch(`https://gurugpt4-85987f3ed9b3.herokuapp.com/api/gpt4?query=${encodeURIComponent(userQuery)}`);
     const data = await response.json();
-    const { response: result } = data; 
-    m.reply(result.trim()); 
+    const { response: result } = data;
+    m.reply(result.trim());
   } catch (error) {
-    console.error('Error:', error); 
-    throw `*ERROR*`;
+    console.error('Error:', error);
+    m.reply(`*ERROR*`);
   }
 };
 
