d4331910ddffa8c9ed6ddfcb8a06e07c9da5a9e6
LBlaze <69427966+IndraTensei@users.noreply.github.com>
2023-07-30 15:52:04 +0530

    Update GURU-Openai.js
    quoted+user

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -1,23 +1,26 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { text, usedPrefix, command }) => {
-  
   if (!text && !(m.quoted && m.quoted.text)) {
     throw `Please provide some text or quote a message to get a response.`;
   }
 
- 
   if (!text && m.quoted && m.quoted.text) {
     text = m.quoted.text;
+  } else if (text && m.quoted && m.quoted.text) {
+    text = `${text} ${m.quoted.text}`;
+    if (m.quoted.text.includes('.ai')) {
+      text = text.replace('.ai', ''); // Remove ".ai" from the quoted message if detected
+    }
   }
 
   try {
     const response = await fetch(`https://gurugpt4-85987f3ed9b3.herokuapp.com/api/gpt4?query=${encodeURIComponent(text)}`);
     const data = await response.json();
-    const { response: result } = data; 
-    m.reply(result.trim()); 
+    const { response: result } = data;
+    m.reply(result.trim());
   } catch (error) {
-    console.error('Error:', error); 
+    console.error('Error:', error);
     throw `*ERROR*`;
   }
 };
