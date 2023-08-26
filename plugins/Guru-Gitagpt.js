de9f015fbfff3ea579ebc2bcbfa7edfe6c5aeefc
GURU <sahoogurucharan00@gmail.com>
2023-08-26 19:53:22 +0530

    Update Guru-Gitagpt.js

--- plugins/Guru-Gitagpt.js
+++ plugins/Guru-Gitagpt.js
@@ -1,26 +1,32 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
 let gitagptHandler = async (m, { text, usedPrefix, command }) => {
   if (!text && !(m.quoted && m.quoted.text)) {
-    throw `Please provide some text or quote a message to get a response. Keep in mind that GitaGPT is still in testing phase, so it may generate inaccurate responses at times.`
+    throw `Please provide some text or quote a message to get a response. Keep in mind that GitaGPT is still in the testing phase, so it may generate inaccurate responses at times.`;
   }
 
   if (!text && m.quoted && m.quoted.text) {
-    text = m.quoted.text
+    text = m.quoted.text;
   }
 
   try {
-    const response = await fetch(`https://guru-gpt4-prod-gpt4-reverse-o8hyfh.mo1.mogenius.io/api/gitagpt?query=${encodeURIComponent(text)}`)
-    const data = await response.json()
-    const { response: result } = data
-    m.reply(result.trim())
+    conn.sendPresenceUpdate('composing', m.chat);
+    const prompt = encodeURIComponent(text);
+    const model = 'llama';
+    const endpoint = `https://gurugpt.cyclic.app/chat?prompt=${prompt}&model=${model}`;
+
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const result = data.data; // Extracting the "data" field
+
+    m.reply(result);
   } catch (error) {
-    console.error('Error:', error)
-    throw `*ERROR*`
+    console.error('Error:', error);
+    throw `*ERROR*`;
   }
-}
+};
 
-gitagptHandler.command = ['gitagpt']
-gitagptHandler.diamond = false
+gitagptHandler.command = ['gitagpt'];
+gitagptHandler.diamond = false;
 
-export default gitagptHandler
+export default gitagptHandler;
