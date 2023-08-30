523d80f443126fd52767a025fe91df4b7d7d61e5
GURU <sahoogurucharan00@gmail.com>
2023-08-30 10:41:07 +0530

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -1,19 +1,64 @@
-import { promises } from 'fs'
-import { join } from 'path'
+import { promises } from 'fs';
+import { join } from 'path';
+import axios from 'axios';
 
 let handler = async function (m, { conn, __dirname }) {
-let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-  
-m.reply(`
-*≡ SCRIPT*
+  let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({})))  {};
 
-▢ Git : ${_package.homepage}
-`.trim())
-    
-}
+  let repoUrl = _package.repository && _package.repository.url ? _package.repository.url : null;
 
-handler.help = ['script']
-handler.tags = ['main']
-handler.command = ['sc', 'git', 'script'] 
+  let match = repoUrl.match(/github\.com\/(.+?)\/(.+?)(\.git)?$/);
+  if (match) {
+    let owner = match[1];
+    let repoName = match[2];
 
-export default handler
+    try {
+      let response = await axios.get(`https://api.github.com/repos/${owner}/${repoName}`);
+      let repoData = response.data;
+
+      
+      let replyMessage = `
+*≡ REPOSITORY DETAILS *
+
+📚 Repository: ${repoData.full_name}
+📄 Description: ${repoData.description  'No description available'}
+🌟 Stars: ${repoData.stargazers_count}
+🍴 Forks: ${repoData.forks_count}
+🔗 URL: ${repoData.html_url}
+🕒 Created: ${new Date(repoData.created_at).toDateString()}
+🛠 Language: ${repoData.language || 'Unknown'}
+`.trim();
+
+      // Prepare the message object
+      let messageObject = {
+        requestPaymentMessage: {
+          currencyCodeIso4217: 'INR',
+          amount1000: '6900000000',
+          requestFrom: '0@s.whatsapp.net',
+          noteMessage: {
+            extendedTextMessage: {
+              text: replyMessage,
+              contextInfo: {
+                mentionedJid: [m.sender],
+                externalAdReply: {
+                  showAdAttribution: true
+                }
+              }
+            }
+          }
+        }
+      };
+
+   
+      await conn.relayMessage(m.chat, messageObject, {});
+    } catch (error) {
+      console.error('Error fetching repository data:', error);
+    }
+  }
+};
+
+handler.help = ['script'];
+handler.tags = ['main'];
+handler.command = ['sc', 'git', 'script'];
+
+export default handler;
