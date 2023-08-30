180fcfa82d2171480c20f7ba9a9ff7e0a5d23ced
GURU <sahoogurucharan00@gmail.com>
2023-08-30 11:16:40 +0530

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -1,29 +1,28 @@
 import fs from 'fs';
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+const handler = async (m, { conn, args, usedPrefix, command }) => {
   let _muptime;
   if (process.send) {
     process.send('uptime');
     _muptime = await new Promise(resolve => {
       process.once('message', resolve);
       setTimeout(resolve, 1000);
-    }) * 1000;
+    });
   }
-  let muptime = clockString(_muptime);
+  let muptime = clockString(_muptime * 1000);
 
-  const pluginFiles = fs.readdirSync('./plugins'); 
+  const pluginFiles = fs.readdirSync('./plugins');
   let totalPlugins = pluginFiles.length;
 
   let uptim = `Bot Uptime:
-              `🔸 Uptime: ${muptime}\n` +
-              `🔸 Total Plugins: ${totalPlugins}\n`
-             
+              🔸 Uptime: ${muptime}
+              🔸 Total Plugins: ${totalPlugins}`;
 
   conn.sendMessage(m.chat, {
     text: uptim,
     contextInfo: {
       externalAdReply: {
-        title: " 𝐺 𝑈 𝑅 𝑈 𝐵 𝛩 𝑇 𝛧",
+        title: "𝐺 𝑈 𝑅 𝑈 𝐵 𝛩 𝑇 𝛧",
         body: "",
         thumbnailUrl: "https://cdn.wallpapersafari.com/94/61/oLMwsy.jpg",
         sourceUrl: "https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp",
@@ -45,5 +44,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-  return [d, 'd ', h, 'h ', m, 'm ', s, 's '].map(v => v.toString().padStart(2, 0)).join('');
+  return `${d}d ${h}h ${m}m ${s}s`.replace(/(^| )0/g, '');
 }
