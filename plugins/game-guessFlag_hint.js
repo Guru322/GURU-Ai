b6fc939802190b6b1fe5edef03c3498655f3fc8a
GURU <107632293+guru65476@users.noreply.github.com>
2023-09-15 10:54:28 +0530

    Create game-guessFlag_hint.js

---
+++ plugins/game-guessFlag_hint.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    let id = m.chat
+    if (!(id in conn.tebakbendera)) throw false
+    let json = conn.tebakbendera[id][1]
+    conn.reply(m.chat, '```' + json.name.replace(/[AIUEOaiueo]/ig, '_') + '```', m)
+}
+handler.command = /^fhint$/i
+
+
+
+export default handler
