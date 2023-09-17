c4f6a7a1e4e47efaf4e26b8bedc8cbe3bbbca121
GURU <107632293+guru65476@users.noreply.github.com>
2023-09-17 21:38:19 +0530

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,159 +1,244 @@
-import { createHash } from 'crypto'
-import PhoneNumber from 'awesome-phonenumber'
-import { canLevelUp, xpRange } from '../lib/levelling.js'
-import fetch from 'node-fetch'
-import fs from 'fs'
-const { levelling } = '../lib/levelling.js'
-import moment from 'moment-timezone'
-import { promises } from 'fs'
-import { join } from 'path'
-const time = moment.tz('Asia/Kolkata').format('HH')
-let wib = moment.tz('Asia/Kolkata').format('HH:mm:ss')
-//import db from '../lib/database.js'
+import {
+  promises,
+  readFileSync
+ } from "fs"
+ import {
+  join
+ } from "path"
+ import {
+  xpRange
+ } from "../lib/levelling.js"
+ import moment from "moment-timezone"
+ import os from "os"
+ import fs from "fs"
+ import fetch from "node-fetch"
+ 
+ const defaultMenu = {
+  before: `
+  「 ${botname} あ⁩ 」\n
+  *%ucpn*
+ 
+ 乂───『 *U S E R*』───乂
+ ⛥ *Name:* %name
+  ⛥ *Gold:* %credit
+  ⛥ *Role:* %role
+  ⛥ *Level:* %level [ %xp4levelup Xp For Levelup]
+  ⛥ *Xp:* %exp / %maxexp
+  ⛥ *Total Xp:* %totalexp
+  ╰──────────⳹
+ 
+  乂───『 *I N F O*』───乂
+  ⛥ *Bot Name:* %me
+  ⛥ *Mode:* %mode
+  ⛥ *Platform:* %platform
+  ⛥ *Type:* NodeJs
+  ⛥ *Baileys:* Multi Device
+  ⛥ *Prefix:* [ *%_p* ]
+  ⛥ *Uptime:* %muptime
+  ⛥ *Database:* %rtotalreg out of %totalreg
+  ╰──────────⳹
+  
+  乂───『 *I N F O  C M D*』───乂 
+  │ *%totalfeatures* Commands
+  ╰──────────⳹
+ %readmore
+ `.trimStart(),
+ header: "✦ ───『 *%category* 』─── ⚝",
+ body: "◈ %cmd %isPremium %islimit",
+ footer: "╰──────────⳹",
+ after: "\n%me",
+ }
+ let handler = async (m, {
+  conn,
+  usedPrefix: _p,
+  __dirname,
+  args
+ }) => {
+  await conn.sendMessage(m.chat, {
+   react: {
+ text: "⏳",
+ key: m.key,
+   }
+  })
+  
+  let tags = {}
+  
+  try {
+  
+   /* Info Menu */
+   let glb = global.db.data.users
+   let usrs = glb[m.sender]
+   let tag = `@${m.sender.split("@")[0]}`
+   let mode = global.opts["self"] ? "Private" : "Public"
+   let _package = JSON.parse(await promises.readFile(join(__dirname, "../package.json")).catch(_ => ({}))) || {}
+   let {
+ age,
+ exp,
+ limit,
+ level,
+ role,
+ registered,
+ credit
+   } = glb[m.sender]
+   let {
+ min,
+ xp,
+ max
+   } = xpRange(level, global.multiplier)
+   let name = await conn.getName(m.sender)
+   let premium = glb[m.sender].premiumTime
+   let prems = `${premium > 0 ? "Premium": "Free"}`
+   let platform = os.platform()
+ 
+ 
+   let ucpn = `${ucapan()}`
+  
+   let _uptime = process.uptime() * 1000
+   let _muptime
+   if (process.send) {
+ process.send("uptime")
+ _muptime = await new Promise(resolve => {
+  process.once("message", resolve)
+  setTimeout(resolve, 1000)
+ }) * 1000
+   }
+   let muptime = clockString(_muptime)
+   let uptime = clockString(_uptime)
+ 
+   
+   let totalfeatures = Object.values(global.plugins).filter((v) => v.help && v.tags).length;
+   let totalreg = Object.keys(glb).length
+   let rtotalreg = Object.values(glb).filter(user => user.registered == true).length
+   let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
+ return {
+  help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
+  tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+  prefix: "customPrefix" in plugin,
+  limit: plugin.limit,
+  premium: plugin.premium,
+  enabled: !plugin.disabled,
+ }
+   })
+   for (let plugin of help)
+ if (plugin && "tags" in plugin)
+  for (let tag of plugin.tags)
+   if (!(tag in tags) && tag) tags[tag] = tag
+   conn.menu = conn.menu ? conn.menu : {}
+   let before = conn.menu.before || defaultMenu.before
+   let header = conn.menu.header || defaultMenu.header
+   let body = conn.menu.body || defaultMenu.body
+   let footer = conn.menu.footer || defaultMenu.footer
+   let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? "" : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + defaultMenu.after
+   let _text = [
+ before,
+ ...Object.keys(tags).map(tag => {
+  return header.replace(/%category/g, tags[tag]) + "\n" + [
+   ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
+ return menu.help.map(help => {
+  return body.replace(/%cmd/g, menu.prefix ? help : "%_p" + help)
+   .replace(/%islimit/g, menu.limit ? "Ⓛ" : "")
+   .replace(/%isPremium/g, menu.premium ? "🅟" : "")
+   .trim()
+ }).join("\n")
+   }),
+   footer
+  ].join("\n")
+ }),
+ after
+   ].join("\n")
+   let text = typeof conn.menu == "string" ? conn.menu : typeof conn.menu == "object" ? _text : ""
+   let replace = {
+ "%": "%",
+ p: _p,
+ uptime,
+ muptime,
+ me: conn.getName(conn.user.jid),
+ npmname: _package.name,
+ npmdesc: _package.description,
+ version: _package.version,
+ exp: exp - min,
+ maxexp: xp,
+ totalexp: exp,
+ xp4levelup: max - exp,
+ github: _package.homepage ? _package.homepage.url || _package.homepage : "[unknown github url]",
+ tag,
+ ucpn,
+ platform,
+ mode,
+ _p,
+ credit,
+ age,
+ tag,
+ name,
+ prems,
+ level,
+ limit,
+ name,
+ totalreg,
+ totalfeatures,
+ rtotalreg,
+ role,
+ readmore: readMore
+   }
+   text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, "g"), (_, name) => "" + replace[name])
+   const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => "./Guru.jpg")
+   var vid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
+ 
+ let contact = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
+ 
+  conn.sendMessage(m.chat, { video: { url: vid  }, caption: text.trim(),  gifPlayback: true,
+  gifAttribution: 0}, { quoted: contact })
 
-let handler = async (m, { conn, usedPrefix, command}) => {
-    let d = new Date(new Date + 3600000)
-    let locale = 'en'
-    let week = d.toLocaleDateString(locale, { weekday: 'long' })
-    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
-    let _uptime = process.uptime() * 1000
-    let uptime = clockString(_uptime)
-let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let pp = './Guru.jpg'
-let user = global.db.data.users[who]
-let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
-let { min, xp, max } = xpRange(user.level, global.multiplier)
-let username = conn.getName(who)
-let math = max - xp
-let prem = global.prems.includes(who.split`@`[0])
-let sn = createHash('md5').update(who).digest('hex')
-let totaluser = Object.values(global.db.data.users).length 
-let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
-let more = String.fromCharCode(8206)
-let readMore = more.repeat(850) 
-let greeting = ucapan()
-let quote = quotes[Math.floor(Math.random() * quotes.length)];
+  } catch (e) {
+   await conn.reply(m.chat, " error", m)
+   throw e
+  }
+ }
+ handler.command = /^(menu|help|\?)$/i
+ 
 
-let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let str = `
-🚀 *_Buckle up ${name}, ${greeting}! We're going on an adventure!_* 🚀
-
-📜 *_Quote of the day: ${quote}_* 📜
-
-┏━💼 _User Info:_ 💼━┓
-┃ 👾  *User Tag:* ${taguser} 
-┃ 🎩  *Name:* ${name} 
-┃ 🦸  *Master Mind:* ${author} 
-┃ 💎  *Diamonds:* ${diamond} 
-┃ 🏆  *Rank:* ${role}
-┃ 🎮  *XP:* ${exp} 
-┗━━━━━━━━━━━┛
-
-┏━━⏰ _Today's Sauce!_ ⏰━┓
-┃ 📆  *Today's Date:* ${date} 
-┃ ⏲️  *Current Time:* ${wib} 
-┗━━━━━━━━━━━━━┛
-
-┏━━🤖 _BOT STATUS:_🤖━━┓
-┃ 🤡  *Bot Name:* ${botname} 
-┃ 💻  *Platform:* Linux 
-┃ 📣  *Prefix:* ${usedPrefix} 
-┃ 🕓  *Uptime:* ${uptime}
-┃ 💌  *Database:* ${rtotalreg} of ${totaluser} 
-┃ 📚  *Total Users:* ${totaluser} 
-┗━━━━━━━━━━━━━┛
-
-💡 *_Remember, when in doubt, use ${usedPrefix}list or ${usedPrefix}help2. It's like my magic spell book!_* 💡
-`
-
-
-    conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, null, rpyt)
-    m.react(done)
-
-}
-handler.help = ['main']
-handler.tags = ['group']
-handler.command = ['menu', 'help','h','command'] 
-
-export default handler
-function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
-    
-    function ucapan() {
-      const time = moment.tz('Asia/Kolkata').format('HH')
-      let res = "happy early in the day☀️"
-      if (time >= 4) {
-        res = "Good Morning 🌄"
-      }
-      if (time >= 10) {
-        res = "Good Afternoon ☀️"
-      }
-      if (time >= 15) {
-        res = "Good Afternoon 🌇"
-      }
-      if (time >= 18) {
-        res = "Good Night 🌙"
-      }
-      return res
-    }
-    const quotes = [
-      "I'm not lazy, I'm just on my energy saving mode.",
-      "Life is short, smile while you still have teeth.",
-      "I may be a bad influence, but darn I am fun!",
-      "I'm on a whiskey diet. I've lost three days already.",
-      "Why don't some couples go to the gym? Because some relationships don't work out.",
-      "I told my wife she should embrace her mistakes... She gave me a hug.",
-      "I'm great at multitasking. I can waste time, be unproductive, and procrastinate all at once.",
-      "You know you're getting old when you stoop to tie your shoelaces and wonder what else you could do while you're down there.",
-      "I'm so good at sleeping, I can do it with my eyes closed.",
-      "If you think nobody cares if you’re alive, try missing a couple of payments.",
-      "I used to think I was indecisive, but now I'm not so sure.",
-      "If you can't convince them, confuse them.",
-      "I told my wife she was drawing her eyebrows too high. She looked surprised.",
-      "I'm not clumsy, I'm just on a mission to test gravity.",
-      "I told my wife she should do more push-ups. She said, 'I could do a hundred!' So I counted to ten and stopped.",
-      "Life is like a box of chocolates; it doesn't last long if you're hungry.",
-      "I'm not saying I'm Wonder Woman, I'm just saying no one has ever seen me and Wonder Woman in the same room together.",
-      "Why do they call it beauty sleep when you wake up looking like a troll?",
-      "I don't always lose my phone, but when I do, it's always on silent.",
-      "My bed is a magical place where I suddenly remember everything I was supposed to do.",
-      "I love the sound you make when you shut up.",
-      "I'm not arguing, I'm just explaining why I'm right.",
-      "I'm not a complete idiot, some parts are missing.",
-      "When life gives you lemons, squirt someone in the eye.",
-      "I don't need anger management. You just need to stop making me angry.",
-      "I'm not saying I'm Batman. I'm just saying no one has ever seen me and Batman in the same room together.",
-      "I'm not saying I'm Superman. I'm just saying no one has ever seen me and Superman in the same room together.",
-      "I'm not saying I'm Spider-Man. I'm just saying no one has ever seen me and Spider-Man in the same room together.",
-      "I'm not saying I'm a superhero. I'm just saying no one has ever seen me and a superhero in the same room together.",
-      "The early bird can have the worm because worms are gross and mornings are stupid.",
-      "If life gives you lemons, make lemonade. Then find someone whose life has given them vodka and have a party!",
-      "The road to success is always under construction.",
-      "I am so clever that sometimes I don't understand a single word of what I am saying.",
-      "Some people just need a high-five. In the face. With a chair.",
-      "I'm not saying I'm perfect, but I'm pretty close.",
-      "A day without sunshine is like, you know, night.",
-      "The best way to predict the future is to create it.",
-      "If you can't be a good example, then you'll just have to be a horrible warning.",
-      "I don't know why I keep hitting the escape button. I'm just trying to get out of here.",
-      "I'm not lazy. I'm on energy-saving mode.",
-      "I don't need a hairstylist, my pillow gives me a new hairstyle every morning.",
-      "I don't have a bad handwriting, I have my own font.",
-      "I'm not clumsy. It's just the floor hates me, the table and chairs are bullies, and the walls get in my way.",
-      "I'm not saying I'm Batman. I'm just saying no one has ever seen me and Batman in the same room together.",
-      "I'm not saying I'm Wonder Woman. I'm just saying no one has ever seen me and Wonder Woman in the same room together.",
-      "I'm not saying I'm Superman. I'm just saying no one has ever seen me and Superman in the same room together.",
-      "I'm not saying I'm Spider-Man. I'm just saying no one has ever seen me and Spider-Man in the same room together.",
-      "I'm not saying I'm a superhero. I'm just saying no one has ever seen me and a superhero in the same room together.",
-      "वक्त हमे बहुत कुछ सिखा देता है, खासकर तब जब हमारे पास वक्त नहीं होता।",
-      "जिंदगी एक किताब की तरह होती है, हर दिन नया पन्ना बदलता है। कभी हंसते हैं, कभी रोते हैं, पर हर किसी की कहानी अधूरी होती है!",
-      "पढ़ाई करो तो दिल लगता नही, दिल लगाओ तो दिमाग़ लगता नहीं।",
-      "दोस्ती इतनी गहरी करो की दिल में बस जाओ, ऐसे दोस्ती निभाओ की हमे भी तुम्हारे दोस्त होने पर नाज हो।",
-      "मेरे दोस्त तुम बहुत याद आते हो, जब भी भूख लगती है वो समोसे बहुत याद आते है।",
-      "जीवन का असली मज़ा तो तब आता है, जब दूसरे आपकी ज़िंदगी जीने की कोशिश करते हैं।",
-      "कुछ लोग तो इतने फालतू होते हैं, खुद की ज़िंदगी खुद ही नहीं जी पाते और दूसरों की ज़िंदगी में टांग अड़ा देते हैं।"
-];
+ 
+ export default handler
+ 
+ 
+ function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+ }
+ 
+ const more = String.fromCharCode(8206)
+ const readMore = more.repeat(4001)
+ 
+ function clockString(ms) {
+  let h = isNaN(ms) ? "--" : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? "--" : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? "--" : Math.floor(ms / 1000) % 60
+  return [h, " H ", m, " M ", s, " S "].map(v => v.toString().padStart(2, 0)).join("")
+ }
+ 
+ function clockStringP(ms) {
+  let ye = isNaN(ms) ? "--" : Math.floor(ms / 31104000000) % 10
+  let mo = isNaN(ms) ? "--" : Math.floor(ms / 2592000000) % 12
+  let d = isNaN(ms) ? "--" : Math.floor(ms / 86400000) % 30
+  let h = isNaN(ms) ? "--" : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? "--" : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? "--" : Math.floor(ms / 1000) % 60
+  return [ye, " *Years 🗓️*\n", mo, " *Month 🌙*\n", d, " *Days ☀️*\n", h, " *Hours 🕐*\n", m, " *Minute ⏰*\n", s, " *Second ⏱️*"].map(v => v.toString().padStart(2, 0)).join("")
+ }
+ 
+ function ucapan() {
+  const time = moment.tz("Asia/Kolkata").format("HH")
+  let res = "Good morning ☀️"
+  if (time >= 4) {
+   res = "Good Morning 🌄"
+  }
+  if (time >= 10) {
+   res = "Good Afternoon ☀️"
+  }
+  if (time >= 15) {
+   res = "Good Afternoon 🌇"
+  }
+  if (time >= 18) {
+   res = "Good Night 🌙"
+  }
+  return res
+ }
