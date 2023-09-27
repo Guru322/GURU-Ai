035b590544917972deec6a34a344dd5fe8f6231a
GURU <107632293+guru65476@users.noreply.github.com>
2023-09-27 19:40:45 +0530

    new feature

--- plugins/GURU-aisearch.js
+++ plugins/GURU-aisearch.js
@@ -1,8 +1,9 @@
 import fetch from 'node-fetch';
+import displayLoadingScreen from '../lib/loading.js'
 
 const endpoint = 'https://v2-guru-indratensei.cloud.okteto.net/perplexity?query=';
 
-let handler = async (m, { text, usedPrefix, command }) => {
+let handler = async (m, { text, conn, usedPrefix, command }) => {
   try {
     if (!text && !(m.quoted && m.quoted.text)) {
       throw `Please provide some text or quote a message to get a response.`;
@@ -16,8 +17,9 @@ let handler = async (m, { text, usedPrefix, command }) => {
         text = text.replace('.aisearch', ''); // 
       }
     }
-
+    await displayLoadingScreen(conn, m.chat)
     conn.sendPresenceUpdate('composing', m.chat);
+    let emsg = await conn.sendMessage(m.chat, {text: 'Thinking...'})
     const prompt = encodeURIComponent(text);
 
     const response = await fetch(endpoint + prompt);
@@ -28,14 +30,23 @@ let handler = async (m, { text, usedPrefix, command }) => {
 
     const data = await response.json();
     const result = data.response.trim(); 
-    m.reply(result);
+    await conn.relayMessage(m.chat, {
+        protocolMessage: {
+          key: emsg.key,
+          type: 14,
+          editedMessage: {
+            conversation: result 
+          }
+        }
+      }, {})
   } catch (error) {
     console.error('Error:', error);
     m.reply(`An error occurred while processing your request. Please try again later.`);
   }
 };
+handler.help = ['aisearch']
+handler.tags = ['AI']
+handler.command = ['aisearch', 'ai2']; 
 
-handler.command = ['aisearch', 'a2']; 
-handler.diamond = false;
 
 export default handler;
