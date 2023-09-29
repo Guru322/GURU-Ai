a68d5656b2010498a56bc71eae280739350087e8
arsalan12345678 <146023914+arsalan12345678@users.noreply.github.com>
2023-09-29 17:39:09 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923499475748', 'GURU', true],
+  ['923499475748', 'Ash', true], 
   [''] 
 ] //Number of owners
 
