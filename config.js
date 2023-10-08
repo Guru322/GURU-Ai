fcbff46be0e563d7b36c841bd3ad45e786fe48e9
RYUOKIIN <141437937+Not-so-far@users.noreply.github.com>
2023-10-08 19:43:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,12 +9,12 @@ import axios from 'axios'
 global.owner = [
   ['917605902011', 'GURU', true],
   ['6281255369012', 'Ash', true], 
-  [''] 
+  ['918638238336', '𝞒𝙔𝙐𝞗𝞙𝞘𝞘𝞜',true] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
+global.mods = ['917605902011','32460248586', '918638238336'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
 global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
@@ -50,9 +50,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '𝞒𝙔𝙐𝞗𝞙𝞘𝞘𝞜'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
+global.packname = '𝞒𝙔𝙐𝞗𝞙𝞘𝞘𝞜' 
 global.author = '@Asliguru' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
