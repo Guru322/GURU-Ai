5e43c2a8273819c92a7d9047d60e302683a67cd0
RYUOKIIN <141437937+Not-so-far@users.noreply.github.com>
2023-10-08 11:29:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['918638238336', '𝞒𝙔𝙐𝞗𝞙𝞘𝞘𝞜', true],
+  ['918402887115', '𝙂𝞒𝞦𝝯𝞢', true], 
   [''] 
 ] //Number of owners
 
@@ -50,13 +50,13 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'COSMIC-VIBES'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
+global.packname = '𝞒𝙔𝙐𝞗𝞙𝞘𝞘𝞜 × 𝙂𝞒𝞦𝝯𝞢' 
 global.author = '@Asliguru' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/ryuokiin\n' 
+global.dygp = 'https://chat.whatsapp.com/GqnUPQsUusPBcFouhlzssE'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
 global.fgyt = 'https://youtube.com/@Asliguru'
 global.fgpyp = 'https://youtube.com/@Asliguru'
