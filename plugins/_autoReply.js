8b4c184f5ac411819f42b0e9e59ba6cefd4a72e2
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-10-19 19:51:41 +0500

    _autoReply.js

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -1,15 +1,16 @@
 export async function all(m) {
-	
-  // when someone sends a group link to the bot's dm
-  if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('open this link')) && !m.isBaileys && !m.isGroup) {
-   this.sendMessage(m.chat,{text:`Hello @${m.sender.split('@')[0]}\nyou can rent the bot to join a group\n\n_For more info you can DM the owner_\n*Type* \`\`\`.owner\`\`\` *to contact the owner*`.trim()}, {quoted:m});
-   /*this.sendButton(m.chat, `*Invite bot to a group*      
-    Hallo @${m.sender.split('@')[0]} 
-    you can rent the bot to join a group or contact owner 
-    more info click on the button
-  `.trim(), igfg, null, [['Rent', '/buyprem']] , m, { mentions: [m.sender] })*/
-   m.react('💎')
-} 
+  // When someone sends an invite link invitation in private
+  if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Follow this link')) && !m.isBaileys && !m.isGroup) {
+    // Reply to the user who sent the group link to the bot's DM
+    this.sendMessage(m.chat, {
+      text: `Hello @${m.sender.split('@')[0]},\n\nThank you for inviting me! Your request has been received and will be processed once approved. For further infor, contact our representative_.`.trim()
+    }, { quoted: m });
 
- return !0
+    // Ensure the reply is only sent in private chat (DM)
+    if (m.isPrivate) {
+      // Customize the message as needed
+    }
+  }
+
+  return !0;
 }
