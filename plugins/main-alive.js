408ef2798b3c1afdf9c842d7aead9cf6bbc456e9
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-10-19 18:18:29 +0500

    main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,41 +1,22 @@
+let handler = async (m, { conn }) => {
+    let name = "Silver Fox"; //name
 
+    let message = `Greetings, I am ${name}, the Silver Fox. 🦊\n\nI'm alive and ready to assist you! Explore my capabilities. 🤖 And hey, keep smiling! 😄`;
 
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-
-    // Sound
-    let name = m.pushName || conn.getName(m.sender)
-    var vn = "https://raw.githubusercontent.com/Kai0071/A17/master/Assets/audio/🔥.mp3"
-    let url = "https://github.com/Guru322/GURU-BOT"
-    let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
-    let img = "https://cdn.wallpapersafari.com/71/19/7ZfcpT.png"
-    let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
-    let doc = {
-        audio: {
-          url: vn
-        },
-        mimetype: 'audio/mp4',
-        ptt: true,
-        waveform:  [100, 0, 100, 0, 100, 0, 100],
-        fileName: "Guru",
-    
+    await conn.sendMessage(m.chat, message, 'text', { 
+        quoted: m,
         contextInfo: {
-          mentionedJid: [m.sender],
-          externalAdReply: {
-          title: "I AM ALIVE",
-          body: "GURU BOT",
-          thumbnailUrl: img,
-          sourceUrl: 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp',
-          mediaType: 1,
-          renderLargerThumbnail: true
-          }}
-      };
-    
-      await conn.sendMessage(m.chat, doc, { quoted: con });
-    
-    }
-    
-    handler.help = ['alive']
-    handler.tags = ['main']
-    handler.command = /^(alive)$/i 
+            externalAdReply: {
+                title: "I AM ALIVE",
+                body: message,
+                mediaType: 2, // Setting mediaType to 2 for text
+            }
+        }
+    });
+}
 
-    export default handler;
+handler.help = ['alive']
+handler.tags = ['main']
+handler.command = /^(alive)$/i 
+
+export default handler;
