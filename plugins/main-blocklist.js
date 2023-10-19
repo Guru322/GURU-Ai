eb5b0d792b056b3218032156cbff85697b249d5c
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-10-19 19:07:18 +0500

    main-blocklist.js

--- plugins/main-blocklist.js
+++ plugins/main-blocklist.js
@@ -1,17 +1,19 @@
-
 let handler = async (m, { conn }) => {
-	
-	await conn.fetchBlocklist().then(async data => {
-		let txt = `*≡ List *\n\n*Total :* ${data.length}\n\n┌─⊷\n`
-		for (let i of data) {
-			txt += `▢ @${i.split("@")[0]}\n`
-		}
-		txt += "└───────────"
-		return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) })
-	}).catch(err => {
-		console.log(err);
-		throw 'no numbers blocked'
-	})
+  if (m.sender !== global.owner && m.sender !== conn.user.jid) {
+    throw '❌ This command can only be used by our robotic central.'
+  }
+
+  await conn.fetchBlocklist().then(async data => {
+    let txt = `*≡ Blocklist *\n\n*Total :* ${data.length}\n\n┌─⊷\n`
+    for (let i of data) {
+      txt += `▢ @${i.split("@")[0]}\n`
+    }
+    txt += "└───────────"
+    return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) })
+  }).catch(err => {
+    console.log(err);
+    throw '❌ No numbers are blocked.'
+  })
 }
 
 handler.help = ['blocklist']
