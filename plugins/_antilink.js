f7b0fe2dc8021ff4d7844e90f20f25613ff811f0
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-10-19 17:49:45 +0500

    _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,27 +1,24 @@
+const linkRegex = /(https?:\/\/[^\s]+)/i;
 
-const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-
-export async function before(m, {conn, isAdmin, isBotAdmin }) {
+export async function before(m, { isAdmin, isBotAdmin }) {
     if (m.isBaileys && m.fromMe)
         return !0
     if (!m.isGroup) return !1
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
-    const isGroupLink = linkRegex.exec(m.text)
+    const containsLink = linkRegex.test(m.text)
+    let removeParticipant = m.key.participant
+    let messageId = m.key.id
+    
+    if (chat.antiLink && containsLink) {
+        const mentionedUser = `@${m.sender.split('@')[0]}`; // Mention the user who sent the link
+        await this.reply(m.chat, `*Link Detected!* \n\n${mentionedUser} has been removed for sending a link.`, m)
 
-    if (chat.antiLink && isGroupLink && !isAdmin) {
         if (isBotAdmin) {
-            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-            if (m.text.includes(linkThisGroup)) return !0
-        }
-        await conn.reply(m.chat, `*≡ Link Detected*
-            
-We do not allow links from other groups 
-I'm sorry *@${m.sender.split('@')[0]}*  you will be kicked out of the group ${isBotAdmin ? '' : '\n\nIM not an admin so I canT expel you :"v'}`, null, { mentions: [m.sender] } )
-        if (isBotAdmin && chat.antiLink) {
-        	await conn.sendMessage(m.chat, { delete: m.key })
-            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!chat.antiLink) return //m.reply('')
+            // Remove the participant from the group
+            global.db.data.users[m.sender].warn += 1
+            return this.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: messageId, participant: removeParticipant }})
+        } 
     }
     return !0
 }
