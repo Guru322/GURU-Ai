bab6e205de052d3cec01aae04402d7835efa76ff
DarkMan747 <120257712+DarkMan747@users.noreply.github.com>
2023-10-27 15:57:25 +0330

    Create عيون.js

---
+++ عيون.js
@@ -0,0 +1,200 @@
+]
+{
+        "img": "https://telegra.ph/file/6f8efac3b99f11b4e636b.jpg",
+        "name": "كوراما"
+    },
+
+{
+        "img": "https://telegra.ph/file/c634a999326335746b79b.jpg",
+        "name": "كاكاشي"
+    },
+
+{
+        "img": "https://telegra.ph/file/325f5a423a69df5715a5b.jpg",
+        "name": "اوريتشيمارو"
+    },
+
+{
+        "img": "https://telegra.ph/file/bf3addab65fb92bea8905.jpg",
+        "name": "اكازا"
+    },
+
+{
+        "img": "https://telegra.ph/file/333bd919688fd4bfc723e.jpg",
+        "name": "ميهوك"
+    },
+{
+        "img": "https://telegra.ph/file/7514d8e3dc53ef0d797f4.jpg",
+        "name": "ايتاتشي"
+    },
+
+{
+        "img": "https://telegra.ph/file/6092bcea3cb2d2a1b38b1.jpg",
+        "name": "غوكو"
+    },
+
+{
+        "img": "https://telegra.ph/file/37f7277d6d4793ca31109.jpg",
+        "name": "ساسكي"
+    },
+
+{
+        "img": "https://telegra.ph/file/37f7277d6d4793ca31109.jpg",
+        "name": "اوبيتو"
+    },
+
+{
+        "img": "https://telegra.ph/file/3453787dd194a4e5396f7.jpg",
+        "name": "ساكورا"
+    },
+
+{
+        "img": "https://telegra.ph/file/aeead01596a11e324a3c8.jpg",
+        "name": "نيجي"
+    },
+
+{
+        "img": "https://telegra.ph/file/863f8f2c0aa5f6c439ba9.jpg",
+        "name": "تسونادي"
+    },
+
+{
+        "img": "https://telegra.ph/file/68e772b116d66d46cb9e4.jpg",
+        "name": "انيا"
+    },
+
+{
+        "img": "https://telegra.ph/file/d4c1cf42de1d447fd449c.jpg",
+        "name": "هيستوريا"
+    },
+
+{
+        "img": "https://telegra.ph/file/ac12e2b7d1359e4628dfb.jpg",
+        "name": "كاغياما"
+    },
+
+{
+        "img": "https://telegra.ph/file/211fc67e9bc0067dd701d.jpg",
+        "name": "نيزوكو"
+    },
+
+{
+        "img": "https://telegra.ph/file/20c6a7ca758ac31ce355b.jpg",
+        "name": "غوجو"
+    },
+
+{
+        "img": "https://telegra.ph/file/f83fcbb994fa7c18ea028.jpg",
+        "name": "سوكونا"
+    },
+
+{
+        "img": "https://telegra.ph/file/fb6f6b9b16cc32e83d64e.jpg",
+        "name": "ناروتو"
+    },
+
+{
+        "img": "https://telegra.ph/file/da6f102d1b859a7f2284c.jpg",
+        "name": "اليزابيث"
+    },
+
+{
+        "img": "https://telegra.ph/file/cb5bcd83894beb61ea70b.jpg",
+        "name": "هيناتا"
+    },
+
+{
+        "img": "https://telegra.ph/file/33b65cae5c2ff88fdf1c3.jpg",
+        "name": "ميدوريا"
+    },
+
+{
+        "img": "https://telegra.ph/file/b0ebf185e0ed84490953a.jpg",
+        "name": "زابورا"
+    },
+
+{
+        "img": "https://telegra.ph/file/14bfdfb93fa94f23e41d6.jpg",
+        "name": "شانكس"
+    },
+
+{
+        "img": "https://telegra.ph/file/b34490c0203c7a6016e80.jpg",
+        "name": "ميليوداس"
+    },
+
+{
+        "img": "https://telegra.ph/file/e8b6b59acbd96a3f5add9.jpg",
+        "name": "سفيان"
+    },
+
+{
+        "img": "https://telegra.ph/file/24ae2ae5fe63eae4e718c.jpg",
+        "name": "استا"
+    },
+
+{
+        "img": "https://telegra.ph/file/fd527e641823d0023b488.jpg",
+        "name": "اوزوي"
+    },
+
+{
+        "img": "https://telegra.ph/file/f711baf19e30dc32fbf9c.jpg",
+        "name": "موزان"
+    },
+
+{
+        "img": "https://telegra.ph/file/f33544afb9c84569822e4.jpg",
+        "name": "اياتو"
+    },
+
+{
+        "img": "https://telegra.ph/file/0bb8504d44615c063bb60.jpg",
+        "name": "نوبارا"
+    },
+
+{
+        "img": "https://telegra.ph/file/031944cd5950ff9e734b1.jpg",
+        "name": "غون"
+    },
+
+{
+        "img": "https://telegra.ph/file/ebd9ecbb525d4bc7310af.jpg",
+        "name": "دابي"
+    },
+
+{
+        "img": "https://telegra.ph/file/adaf3f6be77a4ceb9e0a9.jpg",
+        "name": "لوفي"
+    },
+
+{
+        "img": "https://telegra.ph/file/6c61b2d1ab3b806e6fe86.jpg",
+        "name": "زورو"
+    },
+
+{
+        "img": "https://telegra.ph/file/81d17ac27a02ef1a6f9c1.jpg",
+        "name": "باين"
+    },
+
+{
+        "img": "https://telegra.ph/file/8630e18b8b59c99179374.jpg",
+        "name": "لايت"
+    },
+
+{
+        "img": "https://telegra.ph/file/f915e217ec1b427f212f0.jpg",
+        "name": "غارا"
+    },
+
+{
+        "img": "https://telegra.ph/file/dc62a0907a7d66457aca8.jpg",
+        "name": "ياماتو"
+    },
+
+{
+        "img": "https://telegra.ph/file/ae98f3af5b6906795f72f.jpg",
+        "name": "اوسوب"
+    },
+]
