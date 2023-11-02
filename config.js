b1ec67afaea378f1b44737967b089c51fb817aea
YobexGavo <118975062+YobexGavo@users.noreply.github.com>
2023-11-02 15:59:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ import axios from 'axios'
 
 global.owner = [
   ['254114140048', 'MYSTIC', true],
-  ['254798577589', 'Ash', true], 
-  [''] 
+  ['', 'Ash', true], 
+  ['254114140048'] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
 global.mods = ['254790540898','254114140048'] 
-global.prems = ['254114140048', '32460248586', '919398758484']
-global.allowed = ['254114140048', '32460248586', '19152999993']
+global.prems = ['', '32460248586', '919398758484']
+global.allowed = ['', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
