27bb6f8bd42d8c02afbff96f643e55e2dcb20273
YobexGavo <118975062+YobexGavo@users.noreply.github.com>
2023-11-02 15:34:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ global.owner = [
 //global.pairingNumber = "" //put your bot number here
 
 global.mods = ['254790540898','254114140048'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.prems = ['254114140048', '32460248586', '919398758484']
+global.allowed = ['254114140048', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
