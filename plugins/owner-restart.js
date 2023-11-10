7dae52f34319396b7ca0bf247f35825097df245a
GURU SENSEI <sahoogurucharan00@gmail.com>
2023-11-10 19:39:11 +0530

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,32 +1,16 @@
-import { spawn } from 'child_process';
-import Heroku from 'heroku-client';
+import { spawn } from 'child_process'
+let handler = async (m, { conn, isROwner, text }) => {
+    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+    if (conn.user.jid == conn.user.jid) {
+    await m.reply('🔄 Restarting Bot...\n Wait a moment')
+    process.send('reset')
+  } else throw 'eh'
+}
 
+handler.help = ['restart']
+handler.tags = ['owner']
+handler.command = ['restart'] 
 
-let handler = async (m, { isROwner }) => {
+handler.rowner = true
 
-    if (isROwner) {
-        await m.reply('🔄 Restarting Bot...\nWait a moment');
-
-        const heroku = new Heroku({ token: process.env.KEY });
-        const appName = process.env.APP;
-
-        try {
-            await heroku.patch(`/apps/${appName}/formation/web`, { body: { quantity: 0 } });
-            await heroku.patch(`/apps/${appName}/formation/web`, { body: { quantity: 1 } });
-        } catch (error) {
-            console.error('Error restarting web dyno:', error.message);
-            throw 'Failed to restart web dyno';
-        }
-    } else {
-        throw 'You are not the owner';
-    }
-};
-
-
-handler.help = ['restart'];
-handler.tags = ['owner'];
-handler.command = ['restart', 'reiniciar'];
-
-handler.rowner = true;
-
-export default handler;
+export default handler
