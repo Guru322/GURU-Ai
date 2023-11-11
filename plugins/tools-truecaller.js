770a2b0cd6fa4faedb6824a1822a6c1edcef056e
GURU SENSEI <sahoogurucharan00@gmail.com>
2023-11-11 20:20:11 +0530

    Update tools-truecaller.js

--- plugins/tools-truecaller.js
+++ plugins/tools-truecaller.js
@@ -1,106 +1,52 @@
-//GURU-BOT
-/*import fetch from 'node-fetch';
+import truesearch from 'truesearch';
 
 let handler = async (m, { conn, text }) => {
-  if (!text) throw 'Please provide a phone number.';
+  let ifone;
 
-  try {
-    let res = await fetch(`https://inrl-web.onrender.com/api/truecaller?number=${text}`);
+  if (!text && m.quoted) {
 
-    if (!res.ok) {
-      throw new Error(`API request failed with status ${res.status}`);
-    }
+    let phoneNumber = m.quoted.sender.replace(/[^0-9]/g, '');
 
-    let json = await res.json();
 
-    if (!json.data || !json.data.length) {
-      throw new Error('No data found in API response.');
-    }
-
-    // Focus on the first object inside the data array.
-    let data = json.data[0];
-
-    // Add custom property.
-    data.creator = 'GURU';
-
-    let response = '';
-
-    // Add details directly to the response
-    response += `𝑁𝑎𝑚𝑒: ${data.name}\n`;
-    response += `𝙶𝚎𝚗𝚍𝚎𝚛: ${data.gender}\n`;
-    response += `𝚂𝚌𝚘𝚛𝚎: ${data.score}\n`;
-    response += `𝚂𝚌𝚘𝚛𝚎: ${data.access}\n`;
-    response += `𝙴𝚗𝚑𝚊𝚗𝚌𝚎𝚍: ${data.enhanced}\n`;
-
-    // Handle phone details
-    if (data.phones && data.phones.length > 0) {
-      let phone = data.phones[0];
-      response += `𝙿𝚑𝚘𝚗𝚎:\n`;
-      response += `  - 𝙚164𝙁𝙤𝙧𝙢𝙖𝙩: ${phone.e164Format}\n`;
-      response += `  - 𝙣𝙪𝙢𝙗𝙚𝙧𝙏𝙮𝙥𝙚: ${phone.numberType}\n`;
-      response += `  - 𝙣𝙖𝙩𝙞𝙤𝙣𝙖𝙡𝙁𝙤𝙧𝙢𝙖𝙩: ${phone.nationalFormat}\n`;
-      response += `  - 𝙙𝙞𝙖𝙡𝙞𝙣𝙜𝘾𝙤𝙙𝙚: ${phone.dialingCode}\n`;
-      response += `  - 𝙘𝙤𝙪𝙣𝙩𝙧𝙮𝘾𝙤𝙙𝙚: ${phone.countryCode}\n`;
-      response += `  - 𝙘𝙖𝙧𝙧𝙞𝙚𝙧: ${phone.carrier}\n`;
-      response += `  - 𝙩𝙮𝙥𝙚: ${phone.type}\n`;
-    }
-
-    // Handle address details
-    if (data.addresses && data.addresses.length > 0) {
-      let address = data.addresses[0];
-      response += `𝘈𝘥𝘥𝘳𝘦𝘴𝘴𝘦𝘴:\n`;
-      response += `  - 𝗮𝗱𝗱𝗿𝗲𝘀𝘀: ${address.address}\n`;
-      response += `  - 𝘀𝘁𝗿𝗲𝗲𝘁: ${address.street}\n`;
-      response += `  - 𝘇𝗶𝗽𝗖𝗼𝗱𝗲: ${address.zipCode}\n`;
-      response += `  - 𝗰𝗶𝘁𝘆: ${address.city}\n`;
-      response += `  - 𝗰𝗼𝘂𝗻𝘁𝗿𝘆𝗖𝗼𝗱𝗲: ${address.countryCode}\n`;
-      response += `  - 𝘁𝗶𝗺𝗲𝗭𝗼𝗻𝗲: ${address.timeZone}\n`;
-      response += `  - 𝘁𝘆𝗽𝗲: ${address.type}\n`;
-    }
-
-    // Add 'creator' property to the response
-    response += `𝗖𝗥𝗘𝗔𝗧𝗢𝗥: ${data.creator}\n`;
-
-    m.reply(response);
-  } catch (error) {
-    console.error(error);
-    m.reply('An error occurred while processing your request. Please try again.');
+    ifone = `+${phoneNumber}`;
+  } else {
+    throw `please give a number in international format or quote a user`;
   }
-};
-
-handler.help = ['true'];
-handler.tags = ['tools'];
-handler.command = /^(true|caller)$/i;
-
-export default handler;*/
-import fetch from 'node-fetch';
-
-let handler = async (m, { conn, text }) => {
-  if (!text) throw 'NUMBER dedo?';
 
   try {
-    let res = await fetch(`https://inrl-web.onrender.com/api/truecaller?number=${text}`);
+    const installationId = 'a1i0D--j3deY0V_k_vQthFibnfb2sS3cf7uttIZnc7UNs9W9JkCGQCwS671R85tI';
 
-    if (!res.ok) {
-      throw new Error(`API request failed with status ${res.status}`);
-    }
+    let json = await truesearch(ifone, installationId);
 
-    let json = await res.json();
+  
 
-    console.log('JSON response:', json);
-
-    
     json.creator = 'GURU';
 
     let milf = '';
     for (let prop in json) {
-      milf += `• *${prop}:* ${json[prop]}\n`;
+      if (prop === 'addresses') {
+        milf += `⚝ *${prop}:*\n`;
+        for (let addressProp in json[prop][0]) {
+          milf += `  ⚝ *${addressProp}:* ${json[prop][0][addressProp]}\n`;
+        }
+      } else if (prop === 'countryDetails') {
+        milf += `⚝ *${prop}:*\n`;
+        for (let countryProp in json[prop]) {
+          if (Array.isArray(json[prop][countryProp])) {
+            milf += `  ⚝ *${countryProp}:* ${json[prop][countryProp].join(', ')}\n`;
+          } else {
+            milf += `  ⚝ *${countryProp}:* ${json[prop][countryProp]}\n`;
+          }
+        }
+      } else {
+        milf += `⚝ *${prop}:* ${json[prop]}\n`;
+      }
     }
 
     m.reply(milf);
   } catch (error) {
     console.error(error);
-    // Handle the error appropriately
+    
   }
 };
 
@@ -109,3 +55,4 @@ handler.tags = ['tools'];
 handler.command = /^(true|caller)$/i;
 
 export default handler;
+
