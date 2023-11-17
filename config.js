d721415439d475c38e3c50d4c0055736b4b04163
Invincibility44 <151100337+Invincibility44@users.noreply.github.com>
2023-11-17 03:31:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['2348032309782', 'GURU', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
