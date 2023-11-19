55b70bce36619608e8d1a19d1d1838111acf1134
wahab0035 <151034460+wahab0035@users.noreply.github.com>
2023-11-19 20:14:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923211910035', 'wahab', true],
+  ['923072167671', 'chuza', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['923211910035','32460248586'] 
+global.prems = ['923211910035', '32460248586', '923072167671']
+global.allowed = ['923211910035', '32460248586', '923072167671']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,16 +50,16 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'itx-wahab-ʙᴏᴛ'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'wahab┃ᴮᴼᵀ' 
+global.author = '@wahabplays' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.igfg = '▢ Follow on Instagram\nhttps://instagram.com/_itx.__.romeo?igshid=MTk0NTkyODZkYg==\n' 
+global.dygp = 'https://chat.whatsapp.com/CFcANSBjVH3GX1qMCSk9UN'
+global.fgsc = 'https://github.com/wahab0035/wahab-BOT' 
+global.fgyt = 'https://youtube.com/@wahab_plays?si=OwurZrPsHo7pqOkG'
+global.fgpyp = 'https://youtube.com/@wahab_plays?si=OwurZrPsHo7pqOkG'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
