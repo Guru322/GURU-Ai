4d1a2fe9921a0dcdd1154da753e4981e09fad304
Shibu8343 <151452306+Shibu8343@users.noreply.github.com>
2023-11-20 10:51:57 +0530

    Shibu

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ import axios from 'axios'
 global.owner = [
   ['917605902011', 'GURU', true],
   ['6281255369012', 'Ash', true], 
-  [''] 
+  [976297181312''] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "916297181312" //put your bot number here
 
 global.mods = ['917605902011','32460248586'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
