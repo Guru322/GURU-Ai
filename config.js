ec4165371c67ddc2140b5cdbc01464c265fc7011
LuciZR <151607296+LuciZR@users.noreply.github.com>
2023-11-24 21:29:58 +0530

    Update config.js
    Signed-off-by: LuciZR <151607296+LuciZR@users.noreply.github.com>

--- config.js
+++ config.js
@@ -7,8 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['916296851422', 'LUCI_😈', true],
   [''] 
 ] //Number of owners
 
