5eb34447036799fec4ca1283b5f3a94c28e296b2
Cpmodzyt <121564783+Cpmodzyt@users.noreply.github.com>
2023-12-04 05:37:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94765665354', 'GURU', true],
+  ['94756770055', 'Ash', true], 
   [''] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+global.pairingNumber = "1(510)500-8674" //put your bot number here
 
 global.mods = ['917605902011','32460248586'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
@@ -50,16 +50,16 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'Itz Cp´s Bot'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
+global.packname = 'I t ᴢ  C p ´ ꜱ ;  B ᴏ ᴛ  />  ☑️' 
+global.author = '@Itz_Cp' 
+global.menuvid = 'https://telegra.ph/file/d79b0afebef5f9cd1263a.jpg'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/Cpmodzyt\n' 
+global.dygp = 'https://chat.whatsapp.com/HPl9gi585raKVZjzEmcvDH'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.fgyt = 'https://youtube.com/@Itz_Cp'
+global.fgpyp = 'https://youtube.com/@Itz_Cp'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
