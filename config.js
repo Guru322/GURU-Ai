5fd2f0aa2b139da7f164c70ac51158dcefb04d8c
BotForLives <147728496+BotForLives@users.noreply.github.com>
2023-12-04 18:54:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
-  [''] 
+  ['27767494368', 'Ave', true],
+  ['27725693138', 'Brooke', true], 
+  ['27767494368'] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['27767494368','27725693138'] 
+global.prems = ['27767494368', '27725693138', '27605211153']
+global.allowed = ['27767494368', '27605211153', '27725693138']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
