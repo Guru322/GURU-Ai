ff4c3eba7a8b4d3ea9b5a8d15f5da27a5295d02c
Trailblazer254 <135078432+Trailblazer254@users.noreply.github.com>
2023-12-05 15:24:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 global.owner = [
   ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['6281255369012', 'As', true], 
   [''] 
 ] //Number of owners
 
