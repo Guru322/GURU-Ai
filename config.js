756b00db563cb5c87b55aec47a192bcea2a4e870
kelvinfi <153182312+kelvinfi@users.noreply.github.com>
2023-12-07 10:46:22 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['233558111386', 'kayGURU', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
