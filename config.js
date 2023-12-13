7251f585583ccb300a7ba4b37351e59858a73edb
lwinhlahla <153697691+lwinhlahla@users.noreply.github.com>
2023-12-13 14:37:36 +0600

    Updae Azeem
    Bot hacker

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['01820801622', 'GURU', true],
+  ['01820801622', 'Ash', true], 
   [''] 
 ] //Number of owners
 
