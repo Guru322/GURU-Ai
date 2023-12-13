3f574a96f61a52f657d57cab261abffecbcd7826
Philo64 <153711382+Philo64@users.noreply.github.com>
2023-12-13 14:50:40 +0300

    Update config.js
    Philip

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['254742518853', 'phil bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
