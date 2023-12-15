a0a70ef7500748b42e6a1cf92f82dbdcb7898ceb
NISAR <145640352+NKMODER786@users.noreply.github.com>
2023-12-15 02:42:41 -0800

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -14,7 +14,7 @@ handler.all = async function (m) {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Time Active ${muptime}\n\n ┃ 💎  By GURU`
+		let bio = `\n🟢 Time Active ${muptime}\n\n ┃ 💎  By NISAR`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
   }
