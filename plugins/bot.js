7e808153610b3f9bc0c8a12639a93667bcd0655b
NISAR <145640352+NKMODER786@users.noreply.github.com>
2023-12-15 22:57:13 +0500

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/mp3/(["Guru"]).mp3`
+let av = `./Assets/mp3/["Guru"].mp3`
 
 m.reply( `Hello ${taguser} THIS BOT CREATED BY 𓆩😍𓆪NISAR𓆩😍𓆪`)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
