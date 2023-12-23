08d789c66f33843648a4e763a28f1890fc382e6a
steezyhackers <153860813+steezyhackers@users.noreply.github.com>
2023-12-23 22:02:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['27655831912', 'steezyhackers', true],
+  ['27655831912', 'steezyhackers', true], 
   [''] 
 ] //Number of owners
 
