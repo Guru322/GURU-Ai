ea63c0b352c38c16c39a30098b8b81fd67ba35d9
YobexGavo <118975062+YobexGavo@users.noreply.github.com>
2023-12-23 17:45:30 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -579,7 +579,7 @@ export async function participantsUpdate({
                         title: "ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ",
                         body: "welcome to Group",
                         thumbnailUrl: welcomeApiUrl,
-                        sourceUrl: 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA',
+                        sourceUrl: 'https://chat.whatsapp.com/CLwJ8Tkx7vy4Y66i06L0sN',
                         mediaType: 1,
                         renderLargerThumbnail: true
                         }}})
@@ -632,7 +632,7 @@ export async function participantsUpdate({
                         title: "ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ",
                         body: "Goodbye from  Group",
                         thumbnailUrl: leaveApiUrl,
-                        sourceUrl: 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA',
+                        sourceUrl: 'https://chat.whatsapp.com/CLwJ8Tkx7vy4Y66i06L0sN',
                         mediaType: 1,
                         renderLargerThumbnail: true
                         }}})
