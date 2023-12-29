ffd36b24f45d3ace36239a1f05254aca24b70ed2
kelvinfi <153182312+kelvinfi@users.noreply.github.com>
2023-12-29 21:03:19 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [233558111386]
+global.owner = [233558111386
   ['233558111386', 'kayGURU', true],
   ['491789316237', 'yah', true], 
   [''] 
