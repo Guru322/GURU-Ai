dee5262406806f1c57fb4d0c0d5d360856717e24
GURU SENSEI <sahoogurucharan00@gmail.com>
2023-12-31 21:24:15 +0530

    fix&improvement (#228)
    * Update GURU-gemini.js
    
    * Update main-runtime.js
    
    * Update _similarity.js
    
    * Create tools-ocr.js

--- plugins/GURU-gemini.js
+++ plugins/GURU-gemini.js
@@ -1,5 +1,5 @@
 import {GoogleGenerativeAI} from '@google/generative-ai'
-
+import displayLoadingScreen from '../lib/loading.js'
 const genAI = new GoogleGenerativeAI('AIzaSyDJC5a882ruaC4XL6ejY1yhgRkN-JNQKg8');
 
 
@@ -7,6 +7,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   try {
     if (!text) throw `uhm.. what do you want to say?`
     m.react('🤖')
+    await displayLoadingScreen(conn, m.chat)
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
     const prompt = text
