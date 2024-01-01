499fe71b2c47e4c5079b8b07ba06f8fc8e0c45da
Itxmanno05 <149866936+Itxmanno05@users.noreply.github.com>
2024-01-01 21:19:14 +0500

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "https://cdn.jsdelivr.net/gh/Guru322/GURU-BOT@main/Assets/mp3/Alive.mp3atest%20Punjabi%20Mashup%202023.mp3"
-    let url = "https://github.com/Guru322/GURU-BOT"
+    let url = "https://github.com/Itxmanno05/GURU-BOT"
     let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
     let img = "https://telegra.ph/file/cf536b0ee6b71234e387e.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
@@ -24,7 +24,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
           title: "*🫀🄼 ˻𝐈ʈͥ᥊ ϻaͣŋͫŋ̑ο̑˼²⁴⁶ ᴡᴀ ʙᴏᴛ ᴛᴇᴀᴍ*",
           body: "GURU BOT",
           thumbnailUrl: img,
-          sourceUrl: 'https://Wa.me//+60142568861 ',
+          sourceUrl: 'https://Wa.me//+60142568861',
           mediaType: 1,
           renderLargerThumbnail: true
           }}
