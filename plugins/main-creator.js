cb092b58a99bf00f9ef78dc58a771ed3fc7b44fb
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-01-03 15:48:53 +0100

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -7,6 +7,6 @@ function handler(m) {
 
 handler.help = ['owner']
 handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño', 'Gowner'] 
+handler.command = ['owner', 'creator', 'botowner', 'dueño', 'yuji'] 
 
 export default handler
