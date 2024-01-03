080ba1d69741f7c93dfbf91675bb9142b2fa7e23
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-01-03 12:45:33 +0100

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { text, conn, usedPrefix, command }) => {
   if (!text && !(m.quoted && m.quoted.text)) {
-    throw `Please provide some text or quote a message to get a response.`;
+    throw `Please provide some text or quote a message or ask any question to get a response.`;
   }
 
   if (!text && m.quoted && m.quoted.text) {
@@ -12,8 +12,8 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
   try {
     m.react(rwait)
     const { key } = await conn.sendMessage(m.chat, {
-      image: { url: 'https://telegra.ph/file/c3f9e4124de1f31c1c6ae.jpg' },
-      caption: 'Thinking....'
+      image: { url: 'https://i.imgur.com/Io4njZR.jpg' },
+      caption: 'asking Kenjaku 🧠....'
     }, {quoted: m})
     conn.sendPresenceUpdate('composing', m.chat);
     const prompt = encodeURIComponent(text);
@@ -27,7 +27,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
 
       if (!result) {
         
-        throw new Error('No valid JSON response from the first API');
+        throw new Error('I got no response from Kenjaku');
       }
 
       await conn.relayMessage(m.chat, {
@@ -41,7 +41,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
       }, {});
       m.react(done);
     } catch (error) {
-      console.error('Error from the first API:', error);
+      console.error('I got no response from Kenjaku:', error);
 
   
       const model = 'llama';
@@ -67,11 +67,11 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
 
   } catch (error) {
     console.error('Error:', error);
-    throw `*ERROR*`;
+    throw `*NO RESPONSE*`;
   }
 };
 handler.help = ['chatgpt']
 handler.tags = ['AI']
-handler.command = ['bro', 'chatgpt', 'ai', 'gpt'];
+handler.command = ['bro', 'chatgpt', 'ai', 'mechamaru'];
 
 export default handler;
