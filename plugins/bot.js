5578e500f25c06b507c23551d29f45f55f63e17e
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-01-03 14:30:03 +0100

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -5,11 +5,11 @@ let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./Assets/mp3/${pickRandom(["Guru", "Guru1", "Guru2", "Guru3", "Guru4"])}.mp3`
 
-m.reply( `Hello ${taguser} Need help?  type /help `)
+m.reply( `🦄Hello ${taguser} Need any help?  type /help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(bot|guru)$/i
+handler.customPrefix = /^(bot|mechamaru)$/i
 handler.command = new RegExp
 
 export default handler
