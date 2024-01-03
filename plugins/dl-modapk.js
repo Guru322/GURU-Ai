7905401118c834baaf38251970211c123f145bed
Itxmanno05 <149866936+Itxmanno05@users.noreply.github.com>
2024-01-03 22:45:19 +0500

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -1,33 +1,79 @@
-import { download } from 'aptoide-scraper';
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
-  try {
-    if (command === 'modapk') {
-      if (!text) throw `*[❗] Please provide the APK Name you want to download.*`;
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+  if (!args[0]) throw 'Ex: ' + usedPrefix + command + ' minecraft';
+  let info = await apkinfo(text);
+  let res = await apk(text);
 
-      await conn.reply(m.chat, global.wait, m);
-      let data = await download(text);
+  m.react(rwait);
 
-      if (data.size.replace(' MB', '') > 200) {
-        return await conn.sendMessage(m.chat, { text: '*[⛔] The file is too large.*' }, { quoted: m });
-      }
+        if (res.size > 113000000) {
+            m.react(error)
+            throw 'The apk file is too large. Maximum download size is 113MB.';
+          }
 
-      if (data.size.includes('GB')) {
-        return await conn.sendMessage(m.chat, { text: '*[⛔] The file is too large.*' }, { quoted: m });
-      }
 
-      await conn.sendMessage(
-        m.chat,
-        { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null },
-        { quoted: m }
-      )
-    }
-  } catch {
-    throw `*[❗] An error occurred. Make sure to provide a valid link.*`;
-  }
+  await conn.sendMessage(m.chat, {
+    image: { url: info.icon },
+    caption: `*Name:* ${info.name}\n*Package:* ${info.packageN}\n*OBB:* ${info.obb_link}`,
+    footer: '_Apk files..._',
+  });
+
+  await conn.sendMessage(m.chat, {
+    text: `Downloading ${info.name}...`,
+  });
+
+  await conn.sendMessage(
+    m.chat,
+    { document: { url: res.download }, mimetype: res.mimetype, fileName: res.fileName },
+    { quoted: m }
+  );
 };
 
-handler.help = ['modapk']
-handler.tags = ['downloader']
-handler.command = /^modapk$/i;
+handler.command = /^(apk)$/i;
+handler.help = ['apk'];
+handler.tags = ['downloader'];
+handler.premium = false
 export default handler;
+
+async function apkinfo(url) {
+  let res = await fetch('http://ws75.aptoide.com/api/7/apps/search?query=' + url + '&limit=1');
+  let $ = await res.json();
+
+  try {
+    let icon = $.datalist.list[0].icon;
+  } catch {
+    throw 'Can\'t download the apk!';
+  }
+
+  let icon = $.datalist.list[0].icon;
+  let name = $.datalist.list[0].name;
+  let packageN = $.datalist.list[0].package;
+  let download = $.datalist.list[0].file.path;
+  let obb_link;
+  let obb;
+
+  try {
+    obb_link = await $.datalist.list[0].obb.main.path;
+    obb = true;
+  } catch {
+    obb_link = '_not available_';
+    obb = false;
+  }
+
+  if (!download) throw 'Can\'t download the apk!';
+  return { obb, obb_link, name, icon, packageN };
+}
+
+async function apk(url) {
+  let res = await fetch('http://ws75.aptoide.com/api/7/apps/search?query=' + encodeURIComponent(url) + '&limit=1');
+  let $ = await res.json();
+  let fileName = $.datalist.list[0].package + '.apk';
+  let download = $.datalist.list[0].file.path;
+  let size = (await fetch(download, { method: 'head' })).headers.get('Content-Length');
+  if (!download) throw 'Can\'t download the apk!';
+  let icon = $.datalist.list[0].icon;
+  let mimetype = (await fetch(download, { method: 'head' })).headers.get('content-type');
+
+  return { fileName, mimetype, download, size };
+}
