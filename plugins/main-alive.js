a074ebd57a9a7bf7771ab2abb0abf2aab2a02f40
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-01-03 16:13:17 +0100

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -4,10 +4,10 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 
     // Sound
     let name = m.pushName || conn.getName(m.sender)
-    var vn = "https://cdn.jsdelivr.net/gh/Guru322/GURU-BOT@main/Assets/mp3/Alive.mp3"
-    let url = "https://github.com/Guru322/GURU-BOT"
-    let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
-    let img = "https://cdn.wallpapersafari.com/71/19/7ZfcpT.png"
+    var vn = "https://cdn.jsdelivr.net/gh/Yuujikun/GURU-BOT@main/Assets/mp3/Alive.mp3"
+    let url = "https://github.com/Yuujikun/GURU-BOT"
+    let murl = "https://whatsapp.com/channel/0029Va8VuWo4o7qRFa9pEF47"
+    let img = "https://i.imgur.com/aL8lUUX.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
@@ -16,15 +16,15 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         mimetype: 'audio/mpeg',
         ptt: true,
         waveform:  [100, 0, 100, 0, 100, 0, 100],
-        fileName: "Guru",
+        fileName: "Mechamaru",
     
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
-          title: "I AM ALIVE",
-          body: "GURU BOT",
+          title: "HI I'M ALIVE",
+          body: "JUJUTSU™ BOT",
           thumbnailUrl: img,
-          sourceUrl: 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp',
+          sourceUrl: 'https://whatsapp.com/channel/0029Va8VuWo4o7qRFa9pEF47',
           mediaType: 1,
           renderLargerThumbnail: true
           }}
