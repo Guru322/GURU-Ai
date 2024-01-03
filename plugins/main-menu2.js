9d86da2c624047c1a34234e555842db4fc913ff1
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-01-03 16:14:27 +0100

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -37,7 +37,7 @@ let quote = quotes[Math.floor(Math.random() * quotes.length)];
 
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let str = `
-🚀 *_Buckle up ${name}, ${greeting}! We're going on an adventure!_* 🚀
+🚀 *_Buckle up ${name}, ${greeting}! We're going on a Shibuya adventure!_* 🚀
 
 📜 *_Quote of the day: ${quote}_* 📜
 
