c86921eecdf189a8a19c39ed9c0aaa3e3ea19697
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-01-03 13:58:21 +0100

    Update _ranks.js

--- plugins/_ranks.js
+++ plugins/_ranks.js
@@ -4,22 +4,22 @@ global.rpg = {
     if (isNaN(level)) return { name: "", level: "" };
 
     const role = [
-      { name: "Tadpole", level: 0 },
-      { name: "Lobster Larva", level: 5 }, //»»————⍟——««\n
-      { name: "Octopus Apprentice", level: 10 },
-      { name: "Siren's Apprentice", level: 15 },
-      { name: "🐬 Dolphin Diplomat", level: 20 },
-      { name: "🥷 Sea Serpent Tamer", level: 25 }, //𐏓・,〔𒁷, 𒆜〢
-      { name: "⚔ Kraken Hunter", level: 30 },
-      { name: "👑 Sea King", level: 35 },
-      { name: "🪼 Neptune's Protege", level: 40 },
-      { name: "🐍 Abyssal Ambassador", level: 45 },
-      { name: "👹 Guardian of the Abyss", level: 50 },
-      { name: "🧙‍♂️ Deep Sea Sage", level: 60 },
-      { name: "🧝‍♂️ Master of Atlantis", level: 70 },
-      { name: "🐲 Legendary Leviathan", level: 80 },
-      { name: "🔮 Ocean Overlord 🔮", level: 90 },
-      { name: "🔱 Emperor of the Seven Seas 🔱", level: 100 },
+      { name: "Curse Spirit Grade 4", level: -1 },
+      { name: "Common human", level: 0 },
+      { name: "New Born Sorcerer", level: 5 }, //»»————⍟——««\n
+      { name: "Grade 4 sorcerer", level: 10 },
+      { name: "Grade 3 sorcerer", level: 15 },
+      { name: "😎 Semi grade 2 sorcerer", level: 20 },
+      { name: "⚔ Grade 2 sorcerer", level: 25 }, //𐏓・,〔𒁷, 𒆜〢
+      { name: "🕴🏻 Semi grade 1 sorcerer", level: 30 },
+      { name: "⚕️ Grade 1 sorcerer", level: 35 },
+      { name: "🐉 Special Grade sorcerer", level: 40 },
+      { name: "🥷🏻 Rouge Special grade sorcerer", level: 45 },
+      { name: "🧙🏻‍♂️ Jujutsu higher up", level: 50 },
+      { name: "😈 Master of the Zen'in clan,wielder of 10 shadow technique", level: 60 },
+      { name: "🤞🏻💫 Master of Satoru clan,wielder of the six eye and limitless", level: 70 },
+      { name: "🔮 Vessel of Abomination", level: 80 },
+      { name: "👑KING OF CURSES AND MASTER OF JUJUTSU👑", level: 100 },
     ];
 
     return role.reverse().find((role) => level >= role.level);
