4f9ab4f23340dbc27e9a02fd9c1c616db1d0b5bc
RanaAwaisDSP <131793425+RanaAwaisDSP@users.noreply.github.com>
2024-01-04 17:38:06 +0500

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 
     // Sound
     let name = m.pushName || conn.getName(m.sender)
-    var vn = "https://cdn.jsdelivr.net/gh/Guru322/GURU-BOT@main/Assets/mp3/Alive.mp3"
+    var vn = "https://cdn.jsdelivr.net/gh/RanaAwaisDSP/GURU-BOT@main/Assets/mp3/Alive.mp3"
     let url = "https://github.com/Guru322/GURU-BOT"
     let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
     let img = "https://cdn.wallpapersafari.com/71/19/7ZfcpT.png"
