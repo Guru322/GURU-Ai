d9fcdaabd6bce776c3fbb57deccc34c2f60d2121
vaishnav00x <155564861+vaishnav00x@users.noreply.github.com>
2024-01-04 16:58:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['919074559638', 'GURU', true],
+  ['994400180666', 'Ash', true], 
   [''] 
 ] //Number of owners
 
@@ -50,12 +50,12 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'ᴠᴀɪꜱʜɴᴀᴠ ʙᴏᴛ-ᴛᴇᴄʜ'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
+global.packname = 'ᴠᴀɪꜱʜʜʜ' 
+global.author = '@v415h_' 
+global.menuvid = 'https://i.imgur.com/kLJ6ULT.jpg'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/v415h_\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
 global.fgyt = 'https://youtube.com/@Asliguru'
