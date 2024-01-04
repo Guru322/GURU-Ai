6e2a9837e07c17a38238acdafeaca99e7fb3b25e
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-01-04 09:09:49 +0100

    Update plugins

---
+++ plugins/hentaivid.js
@@ -0,0 +1,29 @@
+import fetch from 'node-fetch';
+import displayLoadingScreen from '../lib/loading.js';
+
+let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
+  let chat = global.db.data.chats[m.chat];
+m.react("🍑")
+async function hentaivid() {
+    return new Promise((resolve, reject) => {
+        const page = Math.floor(Math.random() * 1153)
+        axios.get('https://sfmcompile.club/page/'+page)
+        .then((data) => {
+            const $ = cheerio.load(data.data)
+            const hasil = []
+            $('#primary > div > div > ul > li > article').each(function (a, b) {
+                hasil.push({
+                    title: $(b).find('header > h2').text(),
+                    link: $(b).find('header > h2 > a').attr('href'),
+                    category: $(b).find('header > div.entry-before-title > span > span').text().replace('in ', ''),
+                    share_count: $(b).find('header > div.entry-after-title > p > span.entry-shares').text(),
+                    views_count: $(b).find('header > div.entry-after-title > p > span.entry-views').text(),
+                    type: $(b).find('source').attr('type') || 'image/jpeg',
+                    video_1: $(b).find('source').attr('src') || $(b).find('img').attr('data-src'),
+                    video_2: $(b).find('video > a').attr('href') || ''
+                })
+            })
+            resolve(hasil)
+        })
+    })
+}
