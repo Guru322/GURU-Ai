1c71e9ec79fe236b9a0d6c738f923f8b6c331243
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-01-06 20:25:47 +0100

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-    let img = "https://i.imgur.com/NXGmR5H.jpg
+    let img = "https://avatars.githubusercontent.com/u/106463398?v=4"
 let info = `*MECHAMARU ACTIVE*`
 await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 256,
       isForwarded: true, externalAdReply: { title: author, body: botname, sourceUrl: fgyt, thumbnail: await conn.getFile(img) }}})
