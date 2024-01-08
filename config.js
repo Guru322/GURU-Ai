a4ca6eb6e489e0371c6bf056315fcc7e618a2dc3
LuciZR <151607296+LuciZR@users.noreply.github.com>
2024-01-08 22:58:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,15 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
-  [''] 
+  ['918293838182', 'LuciZR', true],
+  
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['918293838182','918293838182'] 
+global.prems = ['918293838182', '918293838182', '918293838182']
+global.allowed = ['918293838182', '918293838182', '918293838182']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,17 +49,17 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '🖤⃝🇱 ✮ 🆄🖤⃝🄲🖤⃝ɪ࿐★࿐'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
+global.packname = '🖤⃝🇱 ✮ 🆄🖤⃝🄲🖤⃝ɪ࿐★࿐' 
+global.author = '@LuciZR' 
+global.menuvid = 'https://i.imgur.com/4J1WUUP.mp4'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/i.luci.zr\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
+global.fgsc = 'https://raw.githubusercontent.com/LuciZR/LuciZR/main/IMG-20231208-WA0010.jpg' 
 global.fgyt = 'https://youtube.com/@Asliguru'
 global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.fglog = 'https://raw.githubusercontent.com/LuciZR/LuciZR/main/IMG-20231208-WA0010.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
