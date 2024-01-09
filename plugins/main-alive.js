1c31938b5dd97c4723ca0c5113ae157a68aee81f
Itxmanno05 <149866936+Itxmanno05@users.noreply.github.com>
2024-01-09 12:56:36 +0500

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -7,7 +7,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     var vn = "https://cdn.jsdelivr.net/gh/Itxmanno05/GURU-BOT@main/Assets/mp3/sajjad.m4a"
     let url = "https://github.com/Itxmanno05/GURU-BOT"
     let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
-    let img = "https://telegra.ph/file/bc14b5c7bb76e1c7fcef9.jpg"
+    let img = "https://telegra.ph/file/d52a552a0b817753754e8.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
