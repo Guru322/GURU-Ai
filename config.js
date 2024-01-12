1d5d23d767d0418d46bc7d4ac9d29acd65a7fdf6
911smooves67 <154805652+911smooves67@users.noreply.github.com>
2024-01-13 02:45:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['254112974149', 'viktor', true],
+  ['254112974149', 'smooves+viktor', true], 
   [''] 
 ] //Number of owners
 
