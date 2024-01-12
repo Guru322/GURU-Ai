96428324ac5a91eaaf723ba275abaa2528d333fe
Itxmanno05 <149866936+Itxmanno05@users.noreply.github.com>
2024-01-12 15:14:28 +0500

    Update main-Guru.js

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -1,32 +1,14 @@
 let handler = async m => m.reply(`
 
-≡  *GURU  ┃ ᴮᴼᵀ*   GROUPS
+≡  *MANNO  ┃ ᴮᴼᵀ*   GROUPS
 
 ─────────────
-▢ Join public bot group and support
-https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
-
-▢ Group 2
-https://chat.whatsapp.com/LwLQab5mZVW6WWRbBPnDGC
-
-▢ Group 3
-https://chat.whatsapp.com/Jn9qVerYN6Q1L0vlVFe2XL
-
-─────────────
-≡ Disabled links? enter here! 
-
-▢ Group WhatsApp 
- https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
-─────────────
-▢ *Owner Telegram*
- https://t.me/i_want_to_be_isekaied
-
-▢ *YouTube*
-• https://www.youtube.com/@Asliguru
+▢ Join public post group and support
+https://chat.whatsapp.com/Esn7W3azds7EWBtJcn16Zt
 
 
 `.trim())
-handler.help = ['gpguru']
+handler.help = ['gpmanno']
 handler.tags = ['main']
 handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru'] 
 
