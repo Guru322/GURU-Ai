9f8e65d74f282bc957205f330cf2883a455d95b9
M-cross1 <154022207+M-cross1@users.noreply.github.com>
2024-01-21 18:07:59 +0100

    KING M-ÇRØSS STYLE ABEG

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2349042566473', 'KÌÑG M-ÇRØSS', true],
+  ['2349042566473', 'Still me', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['2349042566473','2349042566473'] 
+global.prems = ['2349042566473', '32460248586', '919398758484']
+global.allowed = ['2349042566473', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,11 +50,11 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '⚠️ᴋ ɪ ɴ ɢ ༒ ᴍ - ᴄ ʀ ᴏ ᴅ s✅'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
+global.packname = 'ᴍ-ᴄʀᴏss sᴘᴇᴄɪᴀʟ ʙᴏᴛ' 
+global.author = '@KÌÑG M-ÇRØSS' 
+global.menuvid = 'https://telegra.ph/file/e43ca7cda79b62de252a2.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
@@ -64,15 +64,15 @@ global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
-global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
+global.wait = '*⌛ _LOADING BRO SO CALM UR ASS DOWN😁..._*\n*▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
-global.dmoji = '🤭'
+global.dmoji = '😏'
 global.done = '✅'
 global.error = '❌' 
 global.xmoji = '🔥' 
 
-global.multiplier = 69 
-global.maxwarn = '3' 
+global.multiplier = 69
+global.maxwarn = '7' 
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
