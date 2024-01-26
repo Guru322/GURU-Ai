c80b8fe9121d07d38b24440dcf363bfdb5cc89a3
DanielRajput <138299021+DanielRajput@users.noreply.github.com>
2024-01-26 22:06:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,14 +9,15 @@ import axios from 'axios'
 global.owner = [
   ['919654934474', 'KING B2K ✓', true],
   ['919654970780', 'KING B2K ✓', true], 
-  [''] 
+  ['918512849649', 'KING B2K ✓', true],
+  ['']
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['919654934474','919654970780'] 
-global.prems = ['919654934474', '919654970780']
-global.allowed = ['919654934474', '919654970780']
+global.mods = ['919654934474','919654970780','918512849649'] 
+global.prems = ['919654934474', '919654970780','918512849649']
+global.allowed = ['919654934474', '919654970780','918512849649']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -64,7 +65,7 @@ global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
-global.wait = '*⌛ Ruko meri jaan...*\n*▰▰▰▱▱▱▱▱*'
+global.wait = '*⌛ Ruko na babu...*\n*▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
