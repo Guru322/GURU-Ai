d716fb1610ca5c4c077c6205f454acafddbfbad1
Romek <149534184+Romeofaiz@users.noreply.github.com>
2024-02-01 08:29:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['919341378016', 'Romek', true],
+  ['919341378016', 'xd', true], 
   [''] 
 ] //Number of owners
 
