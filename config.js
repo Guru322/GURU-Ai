4c7b80742a93bb2616f19c8601d9df16107ab041
Walkdowngangstar <157907781+Walkdowngangstar@users.noreply.github.com>
2024-02-08 19:24:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['260763825649', 'GURU', true],
-  ['260763825649', 'Ash', true], 
+  ['260765626819', 'GURU', true],
+  ['260765626819', 'Ash', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['260763825649','260763825649'] 
-global.prems = ['260763825649', '260763825649', '919398758484']
-global.allowed = ['260763825649', '260763825649', '19152999993']
+global.mods = ['260765626819','260765626819'] 
+global.prems = ['260765626819', '260765626819', '919398758484']
+global.allowed = ['260765626819', '260765626819', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
