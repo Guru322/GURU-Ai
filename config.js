bd3fbfc15136ed0c20c6aa28587d0b591810f11c
Revelateur2 <159134986+Revelateur2@users.noreply.github.com>
2024-02-08 10:13:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['+237658292966', 'Revelateur Isole', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
