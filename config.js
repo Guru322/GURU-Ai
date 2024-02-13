0a4d0607a9fef0602e9e6db9e46816c286d5cedc
akashlakshan <159102027+akashlakshan@users.noreply.github.com>
2024-02-13 18:17:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94762724515', 'AKASH', true],
+  ['94770258268', 'Lakshan', true], 
   [''] 
 ] //Number of owners
 
