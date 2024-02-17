3a07f9a1acab15c83b216ed3f6008381c0f4bceb
DanielRajput <138299021+DanielRajput@users.noreply.github.com>
2024-02-17 09:57:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,17 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['918512849649', 'KING B2K ✓', true],
-  ['919654970780', 'KING B2K ✓', true], 
   ['919654970780', 'KING B2K ✓', true],
+  ['918512849649', 'KING B2K ✓', true], 
   ['']
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['919654934474','919654970780','918512849649'] 
-global.prems = ['919654934474', '919654970780','918512849649']
-global.allowed = ['919654934474', '919654970780','918512849649']
+global.mods = ['919654970780','918512849649'] 
+global.prems = ['919654970780','918512849649']
+global.allowed = ['919654970780','918512849649']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
