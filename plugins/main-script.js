679ab6a531e89e9524d3f4286bb7a95507dd3711
DanielRajput <138299021+DanielRajput@users.noreply.github.com>
2024-02-17 10:21:38 +0530

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import { join } from 'path';
 import axios from 'axios'; 
 
 let handler = async function (m, { conn, __dirname }) {
-  const githubRepoURL = 'https://github.com/Guru322/GURU-BOT';
+  const githubRepoURL = 'https://github.com/DANIELRAJPUT/GURU-BOT';
 
   try {
   
