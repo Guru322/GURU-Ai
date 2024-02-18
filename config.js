401bdeda3d039aac7cc6de7c1d16da11fc234144
ERNESTSOKO0075 <159727983+ERNESTSOKO0075@users.noreply.github.com>
2024-02-18 15:10:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['260952230719', 'ERNEST', true],
+  ['260960262603', 'ERNEST', true],
   ['0960262603', 'NESTO', true], 
-  [''] 
+  [''0960262603',] 
 ] //Number of owners
 
 //global.pairingNumber = "0952230719xx" //put your bot number here
 
-global.mods = ['260952230719','0960262603'] 
-global.prems = ['',260952230729 '', '919398758484']
-global.allowed = ['260952230719', '32460248586', '19152999993']
+global.mods = 260960262603','0960262603'] 
+global.prems = ['',260960262603 '', '919398758484']
+global.allowed = ['260960262603', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
