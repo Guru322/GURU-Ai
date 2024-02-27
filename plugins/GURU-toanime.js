1dd2ec26997e0d920a714ce42b0012b8251f7a0b
CYBER WARRIOR <134825125+CYBER-WARRIIOR@users.noreply.github.com>
2024-02-27 17:32:14 +0500

    𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹

--- plugins/GURU-toanime.js
+++ plugins/GURU-toanime.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ""
 if (!/image/g.test(mime)) throw '*Respond to a image*'
-m.reply('*This command can turn your photo into anime*')    
+m.reply('*This command can turn your photo into anime photo*')    
 let data = await q.download?.()
 let image = await uploadImage(data)
 try {
