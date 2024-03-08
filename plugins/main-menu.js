756f84bcfbcc4e634dcef66ef83e90764af46f90
CYBER WARRIOR <134825125+CYBER-WARRIIOR@users.noreply.github.com>
2024-03-08 14:16:24 +0500

    𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -537,7 +537,6 @@ import {
     global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     const infoText = `
     ╭━༻𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹༺━╮
-
     
     *${ucpn}* 
    
@@ -547,14 +546,13 @@ import {
     ⛥ *Platform:* ${platform}
     ⛥ *Prefix:* [ *${usedPrefix}* ]
     ⛥ *Uptime:* ${muptime}
-    ⛥ *Database:*  ${totalreg}
     ╰━━━━━━━━━━━━━···▸▸
-    
    
     乂───『 *INFO*』───乂 
   │*Reply with the number*
   │ to get respected Menu*
   ╰━━━━━━━━━━━━━···▸▸
+  
   ╭━⊱Wᴇʟᴄᴏᴍᴇ ᴛᴏ ᴄᴍᴅ ᴍᴇɴᴜ⊱━╮
   │ *1.* Bot Menu
   │ *2.* Owner Menu
