daa3f86ff91f52bc1f116e191ff2aa6a44b258e3
DanielRajput <138299021+DanielRajput@users.noreply.github.com>
2024-03-10 23:50:09 +0530

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -12,7 +12,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
   try {
     m.react(rwait)
     const { key } = await conn.sendMessage(m.chat, {
-      image: { url: 'https://telegra.ph/file/c3f9e4124de1f31c1c6ae.jpg' },
+      image: { url: 'https://telegra.ph/file/fbbc8527df936651da471.jpg' },
       caption: 'Thinking....'
     }, {quoted: m})
     conn.sendPresenceUpdate('composing', m.chat);
