b34524182b59e55dbcb9b2422e5a076998be6219
Dazai31643 <163316742+Dazai31643@users.noreply.github.com>
2024-03-19 11:55:55 +0200

    Create طلاق.js

---
+++ plugins/طلاق.js
@@ -0,0 +1,16 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+m.reply(`*${toM(a)},  وش رأيك تطلقها 😂*
+*${toM(b)},*لاتزعلي 🤭\n\*نزوجك دازاي وش رأيك*`, null, {
+mentions: [a, b]
+})}
+handler.help = ['formarpareja']
+handler.tags = ['main', 'fun']
+handler.command = ['الطلاق','طلاق']
+handler.group = true
+export default handler
