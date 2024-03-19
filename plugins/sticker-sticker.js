691de2c4e48a8f5cccdb170fa3b3337c417c1e3d
Dazai31643 <163316742+Dazai31643@users.noreply.github.com>
2024-03-19 12:30:45 +0200

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -26,7 +26,7 @@ stiker = '*huh*'
 m.reply(stiker)}}
 handler.help = ['sfull']
 handler.tags = ['sticker']
-handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
+handler.command = /^ملصق(tic?ker)?(gif)?(wm)?$/i
 export default handler
 const isUrl = (text) => text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
 async function createSticker(img, url, packName, authorName, quality) {
