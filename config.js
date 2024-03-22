87ae97e911acc3363d6396a3624fa84d5f0f92b3
Makuo0 <139632870+Makuo0@users.noreply.github.com>
2024-03-22 17:20:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['07055805736', 'marchisio', true],
+  ['07055805736', 'marchisio', true], 
   [''] 
 ] //Number of owners
 
