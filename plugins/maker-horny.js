a4440ea39e48a8fd7769d77f119ea64bfd739da7
DanielRajput <138299021+DanielRajput@users.noreply.github.com>
2024-03-27 13:19:35 +0530

    Update maker-horny.js

--- plugins/maker-horny.js
+++ plugins/maker-horny.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/horny', {
     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-    }), 'hornycard.png', '*so horny 🥵🔥*', m)
+    }), 'hornycard.png', '*so horny par KING B2K se kam 🥵🔥*', m)
     }
     handler.help = ['hornycard', 'hornylicense']
     handler.tags = ['maker'] 
