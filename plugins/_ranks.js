5428559195ddbd78b1ecc1560e817a82998bf4f0
DanielRajput <138299021+DanielRajput@users.noreply.github.com>
2024-03-28 11:06:44 +0530

    Update _ranks.js

--- plugins/_ranks.js
+++ plugins/_ranks.js
@@ -19,7 +19,7 @@ global.rpg = {
       { name: "🧝‍♂️ Master of Atlantis", level: 70 },
       { name: "🐲 Legendary Leviathan", level: 80 },
       { name: "🔮 Ocean Overlord 🔮", level: 90 },
-      { name: "🔱 Emperor of the Seven Seas 🔱", level: 100 },
+      { name: "🔱 Left hand of KING B2K ✓ Congratulations! 🔱", level: 100 },
     ];
 
     return role.reverse().find((role) => level >= role.level);
