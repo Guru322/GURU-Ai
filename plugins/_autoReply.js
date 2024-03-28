357001f4a238febe274f185472cc9193668f1cc8
DanielRajput <138299021+DanielRajput@users.noreply.github.com>
2024-03-28 11:03:40 +0530

    Update _autoReply.js

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -2,7 +2,7 @@ export async function all(m) {
 	
   // when someone sends a group link to the bot's dm
   if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('open this link')) && !m.isBaileys && !m.isGroup) {
-   this.sendMessage(m.chat,{text:`Hello @${m.sender.split('@')[0]}\nyou can rent the bot to join a group\n\n_For more info you can DM the owner_\n*Type* \`\`\`.owner\`\`\` *to contact the owner*`.trim()}, {quoted:m});
+   this.sendMessage(m.chat,{text:`Hello @${m.sender.split('@')[0]}\nBot chahiye group ke liye??\n\n_KING B2K ✓ ko Admin bana or use bol add krne ke liye._\n*Type* \`\`\`.owner\`\`\` *to contact the owner*`.trim()}, {quoted:m});
    /*this.sendButton(m.chat, `*Invite bot to a group*      
     Hallo @${m.sender.split('@')[0]} 
     you can rent the bot to join a group or contact owner 
