37f0268da67e4b6e7c060dba8e5c4e7aa00a4e01
DanielRajput <138299021+DanielRajput@users.noreply.github.com>
2024-03-28 10:19:53 +0530

    Update GURU-qrmaker.js

--- plugins/GURU-qrmaker.js
+++ plugins/GURU-qrmaker.js
@@ -1,7 +1,7 @@
 import { toDataURL } from 'qrcode'
 let handler = async (m, { text, conn }) => {
-if (!text) throw `*Give a text to convert*`
-conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', 'Here u go', m)
+if (!text) throw `*Koi Text to de Babu!*`
+conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', 'Lo Babu!', m)
 }
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <text>')
 handler.tags = ['tools']
