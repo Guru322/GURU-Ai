9ac5c8321bfe9cc590eae1abf9f1e11e320429fc
DanielRajput <138299021+DanielRajput@users.noreply.github.com>
2024-03-28 10:20:27 +0530

    Update GURU-readqr.js

--- plugins/GURU-readqr.js
+++ plugins/GURU-readqr.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '*Respond to a qr*'
+if (!mime) throw '*Koi QR code pe respond karo babu*'
 let img = await q.download?.()
 let url = await uploadImage(img)
 let anu = await fetch(`https://api.lolhuman.xyz/api/read-qr?apikey=${lolkeysapi}&img=${url}`)
