5159ee48a1d835ca5f1cc8da28e65e54e557de92
Itx_manno_05 <149866936+Itxmanno05@users.noreply.github.com>
2024-04-01 01:42:51 +0500

    Update hdr.js

--- plugins/hdr.js
+++ plugins/hdr.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 				let error;
 				try {
 					const This = await processing(img, "dehaze");
-					conn.sendFile(m.chat, This, "", "Nih...", m);
+					conn.sendFile(m.chat, This, "", "*🄼 GRANTED BY MANNO-MD*", m);
 				} catch (er) {
 					error = true;
 				} finally {
@@ -44,7 +44,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 				let error;
 				try {
 					const This = await processing(img, "recolor");
-					conn.sendFile(m.chat, This, "", "Nih...", m);
+					conn.sendFile(m.chat, This, "", "*🄼 GRANTED BY MANNO-MD*", m);
 				} catch (er) {
 					error = true;
 				} finally {
@@ -70,7 +70,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 				let error;
 				try {
 					const This = await processing(img, "enhance");
-					conn.sendFile(m.chat, This, "", "Nih...", m);
+					conn.sendFile(m.chat, This, "", "*🄼 GRANTED BY MANNO-MD*", m);
 				} catch (er) {
 					error = true;
 				} finally {
