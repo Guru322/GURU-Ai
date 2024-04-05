c777119ad6473e4cc8027ef9c734f59352d34f0b
Itx_manno_05 <149866936+Itxmanno05@users.noreply.github.com>
2024-04-06 04:44:23 +0500

    Update gp-groupInfo.js

--- plugins/gp-groupInfo.js
+++ plugins/gp-groupInfo.js
@@ -7,31 +7,31 @@ let handler = async (m, { conn, participants, groupMetadata }) => {
     const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
     const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
     let text = `
-┌──「 *INFO GROUP* 」
-▢ *♻️ID:*
+┌──「 *𝙄𝙉𝙁𝙊 𝙂𝙍𝙊𝙐𝙋* 」
+▢ *♻️𝙄𝘿:*
    • ${groupMetadata.id}
-▢ *🔖NAME* : 
+▢ *🔖𝙉𝘼𝙈𝙀* : 
 • ${groupMetadata.subject}
-▢ *👥Members* :
+▢ *👥𝙈𝙀𝙈𝘽𝙀𝙍𝙎* :
 • ${participants.length}
-▢ *🤿Group Owner:*
+▢ *🤿𝙂𝙍𝙊𝙐𝙋 𝙊𝙒𝙉𝙀𝙍:*
 • @${owner.split('@')[0]}
-▢ *🕵🏻‍♂️Admins:*
+▢ *🕵🏻‍♂️𝘼𝘿𝙈𝙄𝙉:*
  ${listAdmin}
-▢ *🪢 group configuration:*
+▢ *🪢 𝙂𝙍𝙊𝙐𝙋 𝘾𝙊𝙉𝙁𝙄𝙂𝙐𝙍𝘼𝙏𝙄𝙊𝙉:*
 • ${isBanned ? '✅' : '❎'} Banned
 • ${welcome ? '✅' : '❎'} Welcome
 • ${detect ? '✅' : '❎'} Detector
 • ${del ? '❎' : '✅'} Anti Delete
 • ${antiLink ? '✅' : '❎'} Anti Link WhatsApp
 
-*▢  📬 message settings:*
+*▢  📬 𝙈𝙀𝙎𝙎𝙀𝙂𝙀 𝙎𝙀𝙏𝙏𝙀𝙄𝙉𝙂𝙎:*
 • Welcome: ${sWelcome}
 • Farewell: ${sBye}
 • Promoted: ${sPromote}
 • Degraded: ${sDemote}
 
-▢ *📌Description* :
+▢ *📌𝘿𝙀𝙎𝘾𝙍𝙀𝙋𝙏𝙄𝙊𝙉* :
    • ${groupMetadata.desc?.toString() || 'unknown'}
 `.trim()
     conn.sendFile(m.chat, pp, 'pp.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
