98aac4a9db6275ea8c4cc5733134892127b2324c
Kingsantos <jamesaddai2011@gmail.com>
2024-05-27 12:12:20 -0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ if (!ownervb) {
 
 const ownerlist = ownervb.split(';');
 
-global.owner = [];
+global.owner = [233549709686];
 for (let i = 0; i < ownerlist.length; i += 2) {
     const owner = [
         ownerlist[i],            
