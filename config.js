29bbda3c4e8cae8d880ee951c55ed1306764cfdd
OfficialOnePesewa1990 <166985763+OfficialOnePesewa1990@users.noreply.github.com>
2024-05-31 22:15:22 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "917605902011;GURU;6281255369012;Ash"
+const ownervb = process.env.OWNERS || "233544482494; Official OnePesewa;233544482494;Official OnePesewa"
 
 
 const ownerlist = ownervb.split(';');
