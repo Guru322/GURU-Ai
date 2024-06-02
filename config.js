60448819b578f3d3238089fc924d15b7dd0c7217
tanay312 <tanaypasari3@gmail.com>
2024-06-03 02:01:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,9 +23,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011', '32460248586']
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['919641777234', '32460248586']
+global.prems = ['919641777234', '32460248586', '919641777234']
+global.allowed = ['919641777234', '32460248586', '919641777234']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
