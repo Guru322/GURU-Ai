14c6730618e5eaffe6819aa72d49c74e6b803a78
GURU SENSEI <owner@guruapi.tech>
2024-09-10 22:59:54 +0530

    should work

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -6,28 +6,28 @@ let handler = async (m, { conn, usedPrefix, args, command, text }) => {
 
   let res
   try {
-    res = await fetch(`${gurubot}/igdlv1?url=${text}`)
+    res = await fetch(`https://api.guruapi.tech/insta/v1/igdl?url=${text}`)
   } catch (error) {
     throw `An error occurred: ${error.message}`
   }
 
   let api_response = await res.json()
 
-  if (!api_response || !api_response.data) {
+  if (!api_response || !api_response.media) {
     throw `No video or image found or Invalid response from API.`
   }
 
-  const mediaArray = api_response.data
+  const mediaArray = api_response.media
 
   for (const mediaData of mediaArray) {
     const mediaType = mediaData.type
-    const mediaURL = mediaData.url_download
+    const mediaURL = mediaData.url
 
     let cap = `HERE IS THE ${mediaType.toUpperCase()} >,<`
 
     if (mediaType === 'video') {
       conn.sendFile(m.chat, mediaURL, 'instagram.mp4', cap, m)
-    } else if (mediaType === 'image') {
+    } else if (mediaType === 'photo') {
       conn.sendFile(m.chat, mediaURL, 'instagram.jpg', cap, m)
     }
   }
